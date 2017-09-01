cde197acc14ecb39938fd8127f722c032a5cc59d
Aaron Brown <aaronwbrown@msn.com>
2017-09-01 11:44:20 -0700

    Update server.py
    adding dbw_enable status change from the simulator's manual control button

--- ros/src/styx/server.py
+++ ros/src/styx/server.py
@@ -14,11 +14,12 @@ app = Flask(__name__)
 bridge = Bridge(conf)
 msgs = []
 
+dbw_enable = False
 
 @sio.on('connect')
 def connect(sid, environ):
     print("connect ", sid)
-    bridge.publish_dbw_status(True)
+    bridge.publish_dbw_status(dbw_enable)
 
 def send(topic, data):
     s = 1
@@ -29,6 +30,10 @@ bridge.register_server(send)
 
 @sio.on('telemetry')
 def telemetry(sid, data):
+    global dbw_enable
+    if(data["dbw_enable"] != dbw_enable):
+        dbw_enable = data["dbw_enable"]
+        bridge.publish_dbw_status(dbw_enable)
     bridge.publish_odometry(data)
     for i in range(len(msgs)):
         topic, data = msgs.pop(0)
