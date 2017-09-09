dace55ce6299fd6962d530bb859ab0179edc8880
Celien Nanson <cesliens@gmail.com>
2017-09-09 11:45:27 +0200

    Update twist_controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -8,17 +8,19 @@ from pid import PID
 
 
 class Controller(object):
-    def __init__(self, throttle, brake, steering):
-    	"""
-    	:param throttle - PID controller applied to the throttle
-    	:param brake - PID controller applied to the brake
-    	:param steering - PID controller applied to the steering   
-    	"""
-        self.throttle = throttle
-        self.brake = brake 
-        self.steering = steering
+	def __init__(self, throttle, brake, steering):
+		"""
+		:param throttle - PID controller applied to the throttle
+		:param brake - PID controller applied to the brake
+		:param steering - PID controller applied to the steering   
+		"""
+		self.throttle = throttle
+		self.brake = brake 
+		self.steering = steering
 
-    def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
-        return 1., 0., 0.
+	def control(self, *args, **kwargs):
+		throttle = 0
+		brake = 0
+		steering = 0
+
+		return throttle, brake. steering
