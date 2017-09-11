64a3d96eb5b5a3e6ed03b2ea4619dee8dd433c33
Celien Nanson <cesliens@gmail.com>
2017-09-11 11:31:36 +0200

    Update code regarding commit here: https://github.com/jdleesmiller/CarND-Capstone/commit/33dae9248a73feab9b577dd135116b6575e85788

--- ros/src/styx/server.py
+++ ros/src/styx/server.py
@@ -21,8 +21,9 @@ def connect(sid, environ):
     print("connect ", sid)
 
 def send(topic, data):
-    s = 1
-    msgs.append((topic, data))
+    msgs[topic] = data
+    #s = 1
+    #msgs.append((topic, data))
     #sio.emit(topic, data=json.dumps(data), skip_sid=True)
 
 bridge.register_server(send)
@@ -35,7 +36,8 @@ def telemetry(sid, data):
         bridge.publish_dbw_status(dbw_enable)
     bridge.publish_odometry(data)
     for i in range(len(msgs)):
-        topic, data = msgs.pop(0)
+        topic, data = msgs.popitem()
+        #topic, data = msgs.pop(0)
         sio.emit(topic, data=data, skip_sid=True)
 
 @sio.on('control')
--- ros/src/waypoint_follower/src/pure_pursuit.cpp
+++ ros/src/waypoint_follower/src/pure_pursuit.cpp
@@ -31,7 +31,7 @@
 #include "pure_pursuit_core.h"
 
 constexpr int LOOP_RATE = 30; //processing frequency
-
+const int PUB_SUB_QUEUE_SIZE = 1;
 
 int main(int argc, char **argv)
 {
@@ -51,16 +51,16 @@ int main(int argc, char **argv)
 
   ROS_INFO("set publisher...");
   // publish topic
-  ros::Publisher cmd_velocity_publisher = nh.advertise<geometry_msgs::TwistStamped>("twist_cmd", 10);
+  ros::Publisher cmd_velocity_publisher = nh.advertise<geometry_msgs::TwistStamped>("twist_cmd", PUB_SUB_QUEUE_SIZE);
 
   ROS_INFO("set subscriber...");
   // subscribe topic
   ros::Subscriber waypoint_subscriber =
-      nh.subscribe("final_waypoints", 10, &waypoint_follower::PurePursuit::callbackFromWayPoints, &pp);
+      nh.subscribe("final_waypoints", PUB_SUB_QUEUE_SIZE, &waypoint_follower::PurePursuit::callbackFromWayPoints, &pp);
   ros::Subscriber ndt_subscriber =
-      nh.subscribe("current_pose", 10, &waypoint_follower::PurePursuit::callbackFromCurrentPose, &pp);
+      nh.subscribe("current_pose", PUB_SUB_QUEUE_SIZE, &waypoint_follower::PurePursuit::callbackFromCurrentPose, &pp);
   ros::Subscriber est_twist_subscriber =
-      nh.subscribe("current_velocity", 10, &waypoint_follower::PurePursuit::callbackFromCurrentVelocity, &pp);
+      nh.subscribe("current_velocity", PUB_SUB_QUEUE_SIZE, &waypoint_follower::PurePursuit::callbackFromCurrentVelocity, &pp);
 
   ROS_INFO("pure pursuit start");
   ros::Rate loop_rate(LOOP_RATE);
