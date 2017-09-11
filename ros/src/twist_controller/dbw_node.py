cb2590113f9304995800a1212638764d20edd5ff
shernshiou <shernshiou@gmail.com>
2017-09-11 17:52:46 +0200

    Draft dbw with controllers

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -89,17 +89,20 @@ class DBWNode(object):
         rate = rospy.Rate(50) # 50Hz
         while not rospy.is_shutdown():
             if (self.waypoints is not None) and (self.pose is not None) and (self.velocity is not None):
-                pass
-                # TODO: Get predicted throttle, brake, and steering using `twist_controller`
-                # You should only publish the control commands if dbw is enabled
-                # throttle, brake, steering = self.controller.control(<proposed linear velocity>,
-                #                                                     <proposed angular velocity>,
-                #                                                     <current linear velocity>,
-                #                                                     <dbw status>,
-                #                                                     <any other argument you need>)
-
-                # if self.dbw_enabled is True:
-                #     self.publish(throttle, brake, steer)
+                if self.dbw_enabled is True:
+                    throttle, brake, steering = self.controller.control(
+                        pose=self.pose,
+                        waypoints=self.waypoints,
+                        velocity=self.velocity,
+                        target_velocity=15
+                    )
+                    yaw_steering = self.yaw_controller.get_steering(
+                        self.twist.linear.x, self.twist.angular.z, velocity
+                    )
+                    # TODO: Incorporate steering with yaw_steering
+                    self.publish(throttle, brake, steering)
+                else:
+                    self.controller.reset()
             rate.sleep()
 
     def publish(self, throttle, brake, steer):
@@ -135,9 +138,6 @@ class DBWNode(object):
 
     def dbw_enabled_cb(self, msg):
         self.dbw_enabled = bool(msg.data)
-
-    def cte(self):
-        pass
 
 if __name__ == '__main__':
     DBWNode()
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,4 +1,6 @@
+import numpy as np
 import rospy
+import tf
 from pid import PID
 
 GAS_DENSITY = 2.858
@@ -16,9 +18,59 @@ class Controller(object):
         self.throttle_pid = PID(kp=0.0, ki=0.0, kd=0.0, mn=self.decel_limit, mx=self.accel_limit)
         self.steer_pid = PID(kp=0.0, ki=0.0, kd=0.0, mn=-self.max_steer_angle, mx=self.max_steer_angle)
 
-        self.timestamp = rospy.get_time()
+        self.cte = None
+        self.diff_velocity = 0
+
+        self.timestamp = None
 
     def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
-        return 1., 0., 0.
+        pose = kwargs.get('pose')
+        waypoints = kwargs.get('waypoints')
+        velocity = kwargs.get('velocity')
+        target_velocity = kwargs.get('target_velocity')
+
+        if self.timestamp is None:
+            self.timestamp = rospy.get_time()
+
+        current_timestamp = rospy.get_time()
+        self.timestamp = current_timestamp
+        sample_time = current_timestamp - self.timestamp
+
+        cte = self.get_cte(pose, waypoints)
+        steer = self.steer_pid.step(cte, sample_time)
+        # TODO: Implement filters
+        throttle = self.throttle_pid.step(target_velocity-velocity, sample_time)
+        brake = 0
+
+        return throttle, brake, steer
+
+    def reset(self):
+        """Reset the PIDs
+
+        Reset all the settings when user changes from auto to manual driving
+        """
+        self.timestamp = None
+        self.throttle_pid.reset()
+        self.steer_pid.reset()
+
+    def get_cte(self, pose, waypoints):
+        x_coords, y_coords = []
+        # Current car pose and head (used as waypoint 0)
+        roll, pitch, yaw = tf.transformations.euler_from_quaternion(
+            [pose.orientation.x, pose.orientation.y,
+             pose.orientation.z, pose.orientation.w])
+        heading_x = pose.position.x
+        heading_y = pose.position.y
+
+        for waypoint in waypoints:
+            # Calculate waypoint wrt current car position
+            waypoint_x = waypoint.pose.pose.position.x - heading_x
+            waypoint_y = waypoint.pose.pose.position.y - heading_y
+            x = waypoint_x * math.cos(0 - yaw) - waypoint_y * math.sin(0 - yaw)
+            y = waypoint_y * math.sin(0 - yaw) - waypoint_x * math.cos(0 - yaw)
+
+            x_coords.append(x)
+            y_coords.append(y)
+
+            coeffs = np.polyfit(x_coords, y_coords, 3)
+            self.cte = coeffs[0]
