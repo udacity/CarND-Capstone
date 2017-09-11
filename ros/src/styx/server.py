e3027177e6535e1005b2e239946e036b0ac84e2e
Celien Nanson <cesliens@gmail.com>
2017-09-11 11:44:46 +0200

    Vehicule is able to drive with simple command

--- ros/src/styx/server.py
+++ ros/src/styx/server.py
@@ -21,9 +21,8 @@ def connect(sid, environ):
     print("connect ", sid)
 
 def send(topic, data):
-    msgs[topic] = data
-    #s = 1
-    #msgs.append((topic, data))
+    s = 1
+    msgs.append((topic, data))
     #sio.emit(topic, data=json.dumps(data), skip_sid=True)
 
 bridge.register_server(send)
@@ -36,8 +35,7 @@ def telemetry(sid, data):
         bridge.publish_dbw_status(dbw_enable)
     bridge.publish_odometry(data)
     for i in range(len(msgs)):
-        topic, data = msgs.popitem()
-        #topic, data = msgs.pop(0)
+        topic, data = msgs.pop(0)
         sio.emit(topic, data=data, skip_sid=True)
 
 @sio.on('control')
