29aa7cc5214e450d8742d0fe59ccb4eb1d7f88de
Isaac Haw <isaac.haw@gmail.com>
2017-09-14 22:46:45 +0800

    partial waypoint_updater

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 import math
 import rospy
+
 import tf
+from std_msgs.msg import Int32
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
+
+from collections import namedtuple
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -22,6 +26,8 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
 
+Rotations = namedtuple('Rotations', ['roll', 'pitch', 'yaw'])
+Vector = namedtuple('Vector', ['x', 'y', 'z'])
 
 class WaypointUpdater(object):
     def __init__(self):
@@ -31,30 +37,23 @@ class WaypointUpdater(object):
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
+        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
+        rospy.Subscriber('/obstacle_waypoint', PoseStamped, self.obstacle_cb)
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
-        self.base_waypoints = None
-        self.pose = None
+        self.current_pose = None
+        self.waypoints = None
 
-        self.loop()
-
-    def loop(self):
-        """
-        So final_waypoint publish rate is independent of simulator rate
-        """
-        rate = rospy.Rate(10)
-        while not rospy.is_shutdown():
-            self.publish()
-            rate.sleep()
+        self.publish()
+        rospy.spin()
 
     def pose_cb(self, msg):
-        self.pose = msg.pose
+        self.current_pose = msg.pose
 
     def waypoints_cb(self, msg):
-        self.base_waypoints = msg.waypoints
+        self.waypoints = msg.waypoints
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
@@ -63,50 +62,6 @@ class WaypointUpdater(object):
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
         pass
-
-    def publish(self):
-        if self.base_waypoints is not None:
-            closest_waypoint_idx = self.get_closest_waypoint()
-
-            final_waypoints = []
-            if closest_waypoint_idx + LOOKAHEAD_WPS <= len(self.base_waypoints):
-                m = closest_waypoint_idx + LOOKAHEAD_WPS
-                final_waypoints = self.base_waypoints[closest_waypoint_idx:m]
-            else
-                m = closest_waypoint_idx + LOOKAHEAD_WPS - len(self.base_waypoints)
-                final_waypoints = self.base_waypoints[closest_waypoint_idx:] +
-                    self.base_waypoints[:m]
-
-            published_lane = Lane()
-            published_lane.header.stamp = rospy.Time.now()
-            published_lane.waypoints = final_waypoints
-            self.final_waypoints_pub.publish(published_lane)
-
-    def get_closest_waypoint(self):
-        pose = self.pose
-        current_position = pose.position
-        closest_gap = float('inf')
-        closest_gap_idx = 0
-
-        for idx, waypoint in enumerate(self.base_waypoints):
-            waypoint_position = waypoint.pose.pose.position
-            dx = current_position.x - waypoint_position.x
-            dy = current_position.y - waypoint_position.y
-            gap = dx*dx + dy*dy
-
-            if gap < closest_gap:
-                roll, pitch, yaw = tf.transformations.euler_from_quaternion(
-                    [pose.orientation.x, pose.orientation.y,
-                     pose.orientation.z, pose.orientation.w])
-                heading_x = pose.position.x
-                heading_y = pose.position.y
-                gap_x = waypoint.pose.pose.position.x - heading_x
-                gap_y = waypoint.pose.pose.position.y - heading_y
-                x = gap_x * math.cos(0 - yaw) - gap_y * math.sin(0 - yaw)
-                if x > 0:
-                    closest_gap = gap
-                    closest_gap_idx = idx
-        return closest_gap_idx
 
     def get_waypoint_velocity(self, waypoint):
         return waypoint.twist.twist.linear.x
@@ -122,6 +77,127 @@ class WaypointUpdater(object):
             dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
             wp1 = i
         return dist
+
+    def rotations_from_quaternion(self, q):
+        """ Get rotations on roll, pitch and yaw axes (global) from quaternion """
+        roll, pitch, yaw = tf.transformations.euler_from_quaternion([q.x, q.y, q.z, q.w])[:3]
+        return Rotations(roll, pitch, yaw)
+
+    def rotations_from_vector(self, v):
+        """ Get rotations on roll, pitch and yaw axes (global) from vector """
+        roll = 0
+        pitch = math.atan2(v.z, math.sqrt(v.x ** 2 + v.y ** 2))
+        yaw = math.atan2(v.y, v.x)
+        return Rotations(roll, pitch, yaw)
+
+    def get_distance(self, a, b):
+        """ Get distance between position A and position B """
+        return math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2  + (a.z - b.z) ** 2)
+
+    def get_vector(self, a, b):
+        """ Get vector from position A to position B """
+        return Vector(b.x - a.x, b.y - a.y, b.z - a.z)
+
+    def angles_delta(self, angle1, angle2, unit='radian'):
+        """ Absolute difference between 2 angles """
+        if unit == 'radian':
+            return math.pi - math.fabs(math.fabs(angle1 - angle2) - math.pi)
+        else:
+            return 180 - math.fabs(math.fabs(angle1 - angle2) - 180)
+
+    def get_closest_waypoint(self, position, waypoints):
+        """ Get the waypoint closest to the pose """
+        dl = lambda wp: self.get_distance(wp.pose.pose.position, position)
+        waypoint_i = waypoints.index(min(waypoints, key=dl))
+        return waypoint_i
+
+    def get_fwd_bwd_vectors(self, waypoint_i, waypoints):
+        """ Compute vectors for forward and backward direction on the waypoint """
+        get_position = lambda wp: wp.pose.pose.position
+        vector_fwd = self.get_vector(get_position(waypoints[waypoint_i]), get_position(waypoints[(waypoint_i + 1) % len(waypoints)]))
+        vector_bwd = self.get_vector(get_position(waypoints[waypoint_i]), get_position(waypoints[(waypoint_i - 1) % len(waypoints)]))
+        return vector_fwd, vector_bwd
+
+    def look_ahead_waypoints(self, pose, waypoints, count=LOOKAHEAD_WPS):
+        """ Look ahead waypoints in the direction based on the vehicle current heading """
+
+        waypoint_i = self.get_closest_waypoint(pose.position, waypoints)
+
+        vector_fwd, vector_bwd = self.get_fwd_bwd_vectors(waypoint_i, waypoints)
+
+        fwd_yaw = self.rotations_from_vector(vector_fwd).yaw
+        bwd_yaw = self.rotations_from_vector(vector_bwd).yaw
+        current_yaw = self.rotations_from_quaternion(pose.orientation).yaw
+
+        fwd_yaw_diff = self.angles_delta(fwd_yaw, current_yaw)
+        bwd_yaw_diff = self.angles_delta(bwd_yaw, current_yaw)
+
+        fwd = True
+        if bwd_yaw_diff < fwd_yaw_diff:
+            waypoints = waypoints[::-1]
+            waypoint_i = len(waypoints) - waypoint_i - 1
+            fwd = False
+
+        # Loop to start if waypoints are depleted
+        waypoints_ahead = waypoints[waypoint_i:waypoint_i + count]
+        while len(waypoints_ahead) < count:
+            waypoints_ahead.extend(waypoints[:count - len(waypoints_ahead)])
+
+        rospy.logout('waypoint_i: %d, yaw %f, fwd_yaw %f, bwd_yaw %f, %s, len(waypoints): %d',
+                waypoint_i, current_yaw, fwd_yaw, bwd_yaw, 'Forward'  if fwd else 'Backward', len(waypoints_ahead))
+
+        return waypoints_ahead
+
+    def look_ahead_waypoints2(self, pose, waypoints, count=LOOKAHEAD_WPS):
+        """ Only look ahead waypoints in the forward direction """
+
+        waypoint_i = self.get_closest_waypoint(pose.position, waypoints)
+
+        current_yaw = self.rotations_from_quaternion(pose.orientation).yaw
+
+        vector = self.get_vector(pose.position, waypoints[waypoint_i].pose.pose.position)
+        new_rotations = self.rotations_from_vector(vector)
+        new_yaw = new_rotations.yaw
+
+        delta = self.angles_delta(current_yaw, new_yaw)
+
+        vector_fwd = self.get_vector(waypoints[waypoint_i].pose.pose.position, waypoints[(waypoint_i + 1) % len(waypoints)].pose.pose.position)
+        proposed_yaw = self.rotations_from_vector(vector_fwd).yaw
+
+        if delta > math.pi / 4:
+            waypoint_i = (waypoint_i + 1) % len(waypoints)
+
+        # Loop to start if waypoints are depleted
+        waypoints_ahead = waypoints[waypoint_i:waypoint_i + count]
+        while len(waypoints_ahead) < count:
+            waypoints_ahead.extend(waypoints[:count - len(waypoints_ahead)])
+
+        rospy.logout('waypoint_i: %d, proposed_yaw %f, len(waypoints): %d', waypoint_i, proposed_yaw, len(waypoints_ahead))
+
+        return waypoints_ahead
+
+    def get_next_waypoints(self):
+        if not self.current_pose or not self.waypoints:
+            return None
+
+        #waypoints_ahead = self.look_ahead_waypoints(self.current_pose, self.waypoints)
+        waypoints_ahead = self.look_ahead_waypoints2(self.current_pose, self.waypoints)
+
+        next_waypoints = waypoints_ahead
+
+        return next_waypoints
+
+    def publish(self):
+        rate = rospy.Rate(40)
+        while not rospy.is_shutdown():
+            next_waypoints = self.get_next_waypoints()
+            if next_waypoints:
+                lane = Lane()
+                lane.header.frame_id = '/world'
+                lane.header.stamp = rospy.Time.now()
+                lane.waypoints = next_waypoints
+                self.final_waypoints_pub.publish(lane)
+            rate.sleep()
 
 
 if __name__ == '__main__':
