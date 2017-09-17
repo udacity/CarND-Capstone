f2077014871bf796761a896e1466512423479578
Shahid <shahid.case2013@gmail.com>
2017-09-17 00:41:57 -0700

    Modified twist controller

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -56,7 +56,7 @@ class DBWNode(object):
                                          BrakeCmd, queue_size=1)
 
         # TODO: Create `TwistController` object
-        self.controller = Controller(wheel_base, steer_ratio, max_lat_accel, max_steer_angle)
+        self.controller = Controller(vehicle_mass, fuel_capacity, brake_deadband, decel_limit, accel_limit, wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle)
 
         # TODO: Subscribe to all the topics you need to
 	rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_enabled_cb)
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -2,29 +2,38 @@
 
 from pid import PID
 from yaw_controller import YawController
+from lowpass import LowPassFilter
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
 class Controller(object):
     def __init__(self, *args, **kwargs):
-        # TODO: Implement
+        	
+	self.vehicle_mass = args[0]
+	self.fuel_capacity = args[1]
+	self.brake_deadband = args[2]
+	self.decel_limit = args[3]
+	self.accel_limit = args[4]
+	self.wheel_radius = args[5]
+	self.wheel_base = args[6]
+	self.steer_ratio = args[7]	
+	self.max_lat_accel = args[8]
+	self.max_steer_angle = args[9]
 
-        self.speed_controller = PID(0.5, 0.02, 0.2)
-	self.steering_controller = PID(5, 0.05, 1, -0.5, 0.5)
-	
-	self.wheel_base = args[0]
-	self.steer_ratio = args[1]
 	self.min_speed = 0
-	self.max_lat_accel = args[2]
-	self.max_steer_angle = args[3]
+
+	self.speed_controller = PID(0.5, 0.02, 0.2, 							self.decel_limit,self.accel_limit)
+	self.steering_controller = PID(5, 0.05, 1, -0.5, 0.5)
+
+	self.tau = 0.2
+	self.ts = 0.1
+	self.lpf = LowPassFilter(self.tau, self.ts)
 	
 	self.yaw_controller = YawController(self.wheel_base, self.steer_ratio, self.min_speed, self.max_lat_accel, self.max_steer_angle)
 
     def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
-
+ 
 	target_linear_vel = args[0]
 	target_angular_vel = args[1]
 	current_linear_vel = args[2]
@@ -33,30 +42,32 @@ class Controller(object):
 	dt = args[5]
 
 	linear_vel_cte = (target_linear_vel - current_linear_vel)
-	
-	#angular_vel_cte = target_angular_vel - current_angular_vel
 	angular_vel_cte = target_angular_vel
-	# $rostopic echo /twist_cmd says target linear velocity is fixed to 11.
-	# but target angular velocity is changing based on vehicle's orientation.
 
 	if dbw_en is False: #PID must not accumulate error under manual control
 	    self.speed_controller.reset()
 	    self.steering_controller.reset()
 
 	linear_vel = self.speed_controller.step(linear_vel_cte, dt)
-	
-	corrective_steer = self.steering_controller.step(angular_vel_cte, dt)
-	predictive_steer = self.yaw_controller.get_steering(target_linear_vel, target_angular_vel, current_linear_vel)
-	# predictive steering helps at higher speeds.
-
-	steer = corrective_steer + predictive_steer
+	linear_vel = self.lpf.filt(linear_vel)
 	
 	throttle = 0
 	brake = 0
 	
-	if linear_vel > 0:
+	if target_linear_vel < 0.01:
+	    brake = (self.vehicle_mass + self.fuel_capacity * GAS_DENSITY) * 					self.wheel_radius * self.decel_limit
+	    throttle = 0
+
+	elif linear_vel > 0:
 	    throttle = linear_vel
+	    brake = 0
 	else:
-	    brake = abs(linear_vel)*20000
+	    
+	    brake = (self.vehicle_mass + self.fuel_capacity * GAS_DENSITY) * 					self.wheel_radius * abs(linear_vel)
+	    throttle = 0
+  	
+	corrective_steer = self.steering_controller.step(angular_vel_cte, dt)
+	predictive_steer = self.yaw_controller.get_steering(target_linear_vel, 					target_angular_vel, current_linear_vel)
+	steer = corrective_steer + predictive_steer
 
         return throttle, brake, steer
