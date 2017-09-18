6076b340baa22b85488d203341addee5979b643f
student <cesliens@gmail.com>
2017-09-18 03:32:36 -0700

    Fix variable initialisation

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -29,7 +29,7 @@ class Controller(object):
 		brake = 0.0
 
 		# Compute difference between target and current velocity as CTE for throttle. 
-		diff_velocity = target_linear_velocity - current_linear_velocity
+		diff_velocity = target_velocity_linear_x - current_velocity_linear_x
 
 		current_time = rospy.get_time()
 		dt = 0
@@ -37,7 +37,9 @@ class Controller(object):
 			dt = current_time - self.prev_time
 		self.prev_time = current_time
 		
-		velocity_controller = self.throttle_pid.step(diff_velocity, dt)
+		velocity_controller = 0
+		if dt > 0:
+			velocity_controller = self.throttle_pid.step(diff_velocity, dt)
 		if velocity_controller > 0:
 			throttle = velocity_controller
 		elif velocity_controller < 0:
@@ -55,38 +57,3 @@ class Controller(object):
 		#steering = math.degrees(steering)
 
 		return throttle, brake, steering
-
-
-"""
-def control(self, twist_cmd, current_velocity, dbw_enabled):
-
-        if not dbw_enabled:
-            self.pid_controller.reset()
-            return 0.0, 0.0, 0.0
-
-        target_linear_velocity = twist_cmd.twist.linear.x
-        target_angular_velocity = twist_cmd.twist.angular.z
-        current_linear_velocity = current_velocity.twist.linear.x
-        current_agular_velocity = current_velocity.twist.angular.z
-
-        #rospy.loginfo("tl = %f, cl = %f, ta = %f", target_linear_velocity, current_linear_velocity, target_angular_velocity)
-
-        linear_velocity_error = target_linear_velocity - current_linear_velocity
-        time = rospy.get_time()
-        delta_t = time - self.prev_time if self.prev_time is not None else 0
-        self.prev_time = time
-        linear_control = self.pid_controller.step(linear_velocity_error, delta_t)
-
-        throttle = 0.0
-        if linear_control > 0.0:
-            throttle = linear_control
-
-        brake = 0.0
-        if linear_control < 0.0:
-            rospy.loginfo("BRAKE = %f", linear_control)
-            brake = -linear_control
-
-        steer = self.yaw_controller.get_steering(target_linear_velocity, target_angular_velocity, current_linear_velocity)
-
-        return throttle, brake, steer
-"""
