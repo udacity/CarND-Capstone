c9f815615f298562a350b346024281632e57ae5d
Nick Hortovanyi <nick.hortovanyi@gmail.com>
2017-09-20 19:18:10 +1000

    change per Celien

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -11,7 +11,7 @@ import math
 
 class Controller(object):
 	def __init__(self, *args, **kwargs):
-		self.yaw_controller = YawController(kwargs['wheel_base'], kwargs['steer_ratio'] * 8,
+		self.yaw_controller = YawController(kwargs['wheel_base'], kwargs['steer_ratio'],
 											kwargs['min_speed'] + ONE_MPH, kwargs['max_lat_accel'],
 											kwargs['max_steer_angle'])
 		self.throttle_pid = PID(kp=0.1, ki=0.015, kd=0.15, mn=kwargs['decel_limit'], mx=kwargs['accel_limit'])
@@ -27,19 +27,19 @@ class Controller(object):
 		throttle = 0.0
 		brake = 0.0
 
-		if not dbw_enabled: 
+		if not dbw_enabled:
 			self.throttle.reset()
 			return 0, 0, 0
 
-		# Compute difference between target and current velocity as CTE for throttle. 
+		# Compute difference between target and current velocity as CTE for throttle.
 		diff_velocity = target_velocity_linear_x - current_velocity_linear_x
 
 		current_time = rospy.get_time()
 		dt = 0
-		if self.prev_time is not None: 
+		if self.prev_time is not None:
 			dt = current_time - self.prev_time
 		self.prev_time = current_time
-		
+
 		velocity_controller = 0
 		if dt > 0:
 			velocity_controller = self.throttle_pid.step(diff_velocity, dt)
@@ -47,7 +47,7 @@ class Controller(object):
 			throttle = velocity_controller
 		elif velocity_controller < 0:
 			brake = -velocity_controller
-		
+
 		steering = self.yaw_controller.get_steering(target_velocity_linear_x, target_velocity_angular_z, current_velocity_linear_x)
 
 		return throttle, brake, steering
