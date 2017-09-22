f9d1803077b0b70f6e4d0abc0b8e1a53fa938335
Khalid <khalid.ashmawy@gmail.com>
2017-09-21 20:14:50 -0700

    Added model for light detection

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,17 @@
 from styx_msgs.msg import TrafficLight
+from keras.preprocessing.image import img_to_array
+import numpy as np
+import keras.backend as K
+from squeezenet import SqueezeNet
+from consts import IMAGE_WIDTH, IMAGE_HEIGHT
+import rospy
+import cv2
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        K.set_image_dim_ordering('tf')
+        self.model = SqueezeNet(3, (IMAGE_HEIGHT, IMAGE_WIDTH, 3))
+        self.model.load_weights("trained_model/challenge1.weights")
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -15,5 +23,19 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        image = cv2.resize(image, (224, 224))
+        image = img_to_array(image)
+        image /= 255.0
+        image = np.expand_dims(image, axis=0)
+        preds = self.model.predict(image)[0]
+        prediction_result = np.argmax(preds)
+
+        if prediction_result == 0:
+            rospy.loginfo('tl_classifier: No traffic light detected.')
+            return TrafficLight.UNKNOWN
+        elif prediction_result == 1:
+            rospy.loginfo('tl_classifier: Red traffic light detected.')
+            return TrafficLight.RED
+        else:
+            rospy.loginfo('tl_classifier: Green traffic light detected')
+            return TrafficLight.GREEN
