22ce99c9408347108edb7f8cf9abd3e2fb123152
Kegan Gan <kegan@map72.com>
2017-09-27 23:44:15 +0800

    Housekeeping tl_classifier.py.

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -10,7 +10,6 @@ class TLClassifier(object):
     def __init__(self, path='light_classifier_model.h5'):
         self.model = load_model(path)
         self.graph = tf.get_default_graph()
-        # self.processing = False
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -22,12 +21,7 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        # if self.processing:
-        #     return TrafficLight.UNKNOWN
-
-        # self.processing = True
-        original = cv2.resize(image, (800, 600), interpolation=cv2.INTER_CUBIC)
-        processed = Helper.preprocess(original)
+        processed = Helper.preprocess(image)
 
         # ----- Filter contour.
         width, height = processed.shape
@@ -41,8 +35,6 @@ class TLClassifier(object):
             ok_size = (w > 5) and (h > 5)
 
             if ok_ratio and ok_size:
-                # cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-
                 # Height expanded.
                 h = h * 7
                 y = y - h*0.25
@@ -51,28 +43,52 @@ class TLClassifier(object):
                 w = h/2
                 # Crop
                 x, y, w, h = int(max(0,x)), int(max(0,y)), int(w), int(h)
-                image = original[y:(y+h), x:(x+w)]
-                with self.graph.as_default():
-                    has_red = (self.predict(image) == TrafficLight.RED) or has_red
+                target = image[y:(y+h), x:(x+w)]
+                # Model classification.
+                has_red = (self.predict(target) == TrafficLight.RED) or has_red
 
-                # cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,255),1)
-                # cv2.putText(img, RESULT[predict(image)], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,255,255))
+        return TrafficLight.RED if has_red else TrafficLight.UNKNOWN
 
-        prediction = TrafficLight.RED if has_red else TrafficLight.UNKNOWN
-        # self.processing = False
-        print(prediction)
-        return prediction
+    def preprocess(input_img):
+        img = cv2.resize(input_img, (800, 600), interpolation=cv2.INTER_CUBIC)
+        out = cv2.bilateralFilter(input_img,11,75,75)
+        # ----- RGB
+        rgb = cv2.cvtColor(out, cv2.COLOR_BGR2RGB)
+
+        # Filter BLUE color
+        rgb[ rgb[:,:,2] > 180 ] = 0
+        # Filter YELLOW color
+        rgb[ (rgb[:,:,1] < 230) & (rgb[:,:,2] > 150) ] = 0
+        # Filter RED < color
+        rgb[ rgb[:,:,0] < rgb[:,:,1] ] = 0
+        rgb[ rgb[:,:,0] < rgb[:,:,2] ] = 0
+        # Filter EQUAL channel color
+        mask1 = (rgb[:,:,0]-rgb[:,:,1] < 20) | (rgb[:,:,1]-rgb[:,:,0] < 20)
+        mask2 = (rgb[:,:,0]-rgb[:,:,2] < 20) | (rgb[:,:,2]-rgb[:,:,0] < 20)
+        mask3 = (rgb[:,:,1]-rgb[:,:,2] < 20) | (rgb[:,:,2]-rgb[:,:,1] < 20)
+        rgb[ mask1 & mask2 & mask3 ] = 0
+        rgb[ mask2 ] = 0
+
+        # ----- Combine channel
+        image = rgb[:,:,0]
+        _, image = cv2.threshold(image,235,255,cv2.THRESH_BINARY)
+
+        return image
 
     def predict(self, image):
-        if type(image) == np.ndarray:
+        if image is not None:
             image_array = cv2.resize(image, (64, 64), interpolation=cv2.INTER_CUBIC)
             image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
-        else:
-            image_array = img_to_array(image.resize((64, 64), PIL.Image.ANTIALIAS))
-        prediction = self.model.predict(image_array[None, :])
-        if prediction[0][0] == 1:
-          return TrafficLight.GREEN
-        elif prediction[0][1] == 1:
-          return TrafficLight.RED
-        else:
-          return TrafficLight.UNKNOWN
+
+            # Make prediction from model.
+            with self.graph.as_default():
+                prediction = self.model.predict(image_array[None, :])
+
+            # Process prediction.
+            if prediction[0][0] == 1:
+              return TrafficLight.GREEN
+            elif prediction[0][1] == 1:
+              return TrafficLight.RED
+            else:
+              return TrafficLight.UNKNOWN
+        return TrafficLight.UNKNOWN
