046e60d087fc8a7d3fdd8c7192d58ea09357d605
steffens21 <steffens21@gmail.com>
2017-09-27 11:39:37 -0700

    Changes throttle PID params and use only steering PID instead of yaw controller
    I noticed that the yaw_controller does not work great if current
    velocity and target velocity don't agree.
    
    The throttle PID might also need more attention.  Right now it takes
    the velocity difference (between current and target) as input.  It
    might be better to take the needed acceleration??  It is still
    unclear to me what a thorttle value of X meas for the accelleration
    of the car

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -20,7 +20,7 @@ class Controller(object):
             max_lat_accel = params['max_lat_accel'],
             max_steer_angle = params['max_steer_angle'])
 
-        self.yaw_filter = LowPassFilter(0.5, 0.1)   
+        self.yaw_filter = LowPassFilter(0.5, 0.1)
         self.filter_yaw = False
 
         self.vehicle_mass = params['vehicle_mass']
@@ -29,8 +29,8 @@ class Controller(object):
         self.wheel_radius = params['wheel_radius']
         self.sample_rate = params['sample_rate']
         self.throttle_pid = PID(
-            1.0,
-            .02,
+            .2,
+            .0,
             .2,
             0,
             1
@@ -65,17 +65,18 @@ class Controller(object):
             if velocity_diff > 0:
                 throttle = self.throttle_pid.step(velocity_diff, time_interval)
             else:
-                brake = -velocity_diff * self.total_mass * self.wheel_radius / time_interval  
+                brake = -velocity_diff * self.total_mass * self.wheel_radius / time_interval
                 self.throttle_pid.reset()
 
 
-            steer = self.yaw_controller.get_steering(
-                linear_velocity,
-                angular_velocity,
-                current_velocity
-            )
             if USE_STEER_PID:
-                steer = self.steer_pid.step(steer, time_interval)
+                steer = self.steer_pid.step(angular_velocity, time_interval)
+            else:
+                steer = self.yaw_controller.get_steering(
+                    linear_velocity,
+                    angular_velocity,
+                    current_velocity
+                )
 
         else:
             self.throttle_pid.reset()
