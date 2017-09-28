9a99d1790d8baf46ccb171c92c75de525defc508
Yury Melnikov <Yury.Melnikov@gmail.com>
2017-09-27 21:36:02 -0400

    Initial implementation of Traffic Light classifier
    The classifier is using the Tensorflow model Faster R-CNN + ResNet101 trained on the COCO dataset.
    http://download.tensorflow.org/models/object_detection/faster_rcnn_resnet101_coco_11_06_2017.tar.gz
    
    The model is able to detect traffic signals, but the classification is done this way:
    1) Threshold luma of each third of the signal image resulting in a binary image.
    2) Add binary of the red color detected in the HSV color space of the first third of the signal
       image.
    3) Add binary of the yellow color of the second third.
    4) Add binary of the green color of the last third.
    5) Pick the light whose average weight is higher than 0.15 and higher than the second greatest
       weight by at least 0.15.
    
    The solution has been tested on simulated and real-world pictures with a good reliability, but
    pretty slow inference rate - 12 seconds per frame on Intel i7 with 8 GiB RAM.

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,52 @@
 from styx_msgs.msg import TrafficLight
+import cv2
+import numpy as np
+import os.path
+import rospy
+import six.moves.urllib as urllib
+import tarfile
+import tensorflow as tf
+
+MODEL_NAME = 'faster_rcnn_resnet101_coco_11_06_2017'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+GRAPH_FILE = 'frozen_inference_graph.pb'
+PATH_TO_CKPT = MODEL_NAME + '/' + GRAPH_FILE
+TRAFFIC_SIGNAL_CLASS = 10
+LUMA_THRESHOLD = (200, 255)
+MIN_LIGHT_WEIGHT = .15
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        if not os.path.isfile(PATH_TO_CKPT):
+            rospy.loginfo("Downloading Tensorflow model")
+            # Download Model.
+            opener = urllib.request.URLopener()
+            opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+            tar_file = tarfile.open(MODEL_FILE)
+            for file in tar_file.getmembers():
+                file_name = os.path.basename(file.name)
+                if GRAPH_FILE in file_name:
+                    tar_file.extract(file, os.getcwd())
+        rospy.loginfo("Loading Tensorflow model into memory")
+        # Load a (frozen) Tensorflow model into memory.
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+            # Definite input and output Tensors for detection_graph.
+            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+            # Each box represents a part of the image where a particular object was detected.
+            self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+            # Each score represent how level of confidence for each of the objects.
+            # Score is shown on the result image, together with the class label.
+            self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+            self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+            self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+        rospy.loginfo("Classifier has been initialized")
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -15,5 +58,106 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        begin_time = rospy.get_rostime()
+
+        signal_detected = False
+        with self.detection_graph.as_default():
+            with tf.Session(graph=self.detection_graph) as sess:
+                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+                image_expanded = np.expand_dims(image, axis=0)
+                # Actual detection.
+                (boxes, scores, classes, num) = sess.run(
+                    [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
+                    feed_dict={self.image_tensor: image_expanded})
+                highest_score = 0.
+                for n in range(num):
+                    c = np.squeeze(classes)[n]
+                    s = np.squeeze(scores)[n]
+                    if c == TRAFFIC_SIGNAL_CLASS and s > highest_score:
+                        signal_box = np.squeeze(boxes)[n]
+                        highest_score = s
+                        signal_detected = True
+                        print("%d: score %f, class %d, box %s" % (n, s, c, signal_box))
+        end_time = rospy.get_rostime()
+        rospy.loginfo("Inference duration %.3f s", end_time.to_sec() - begin_time.to_sec())
+
+        if not signal_detected:
+            return TrafficLight.UNKNOWN
+
+        # Get coords of the traffic signal in px.
+        y_min = int(image.shape[0] * signal_box[0])
+        x_min = int(image.shape[1] * signal_box[1])
+        y_max = int(image.shape[0] * signal_box[2])
+        x_max = int(image.shape[1] * signal_box[3])
+        signal_img = image[y_min:y_max,x_min:x_max]
+        #cv2.imwrite("signal.png", signal_img)
+
+        # Convert signal image to luma-only and HSV.
+        signal_hsv = cv2.cvtColor(signal_img, cv2.COLOR_BGR2HSV)
+        signal_luma = cv2.cvtColor(signal_img, cv2.COLOR_BGR2GRAY)
+
+        # Get coords of the lights in px.
+        offset_y = int(signal_img.shape[0] / 15)
+        offset_x = int(signal_img.shape[1] / 5)
+        one_third = int(signal_img.shape[0] / 3)
+        two_thirds = int(signal_img.shape[0] * 2 / 3)
+        y_min_red = offset_y
+        y_max_red = one_third - offset_y
+        y_min_yellow = one_third + offset_y
+        y_max_yellow = two_thirds - offset_y
+        y_min_green = two_thirds + offset_y
+        y_max_green = signal_img.shape[0] - offset_y
+        x_min_sig = offset_x
+        x_max_sig = signal_img.shape[1] - offset_x
+
+        # Compute weight of the red light.
+        red_luma = signal_luma[y_min_red:y_max_red, x_min_sig:x_max_sig]
+        #cv2.imwrite("red_luma.png", cv2.cvtColor(red_luma, cv2.COLOR_GRAY2RGB))
+        red_binary = np.zeros(red_luma.shape, dtype=np.uint8)
+        red_binary[(red_luma >= LUMA_THRESHOLD[0]) & (red_luma <= LUMA_THRESHOLD[1])] = 1
+        red_hsv = signal_hsv[y_min_red:y_max_red, x_min_sig:x_max_sig]
+        red_mask1 = cv2.inRange(red_hsv, np.array([0, 100, 100]), np.array([10, 255, 255]))
+        red_mask2 = cv2.inRange(red_hsv, np.array([170, 100, 100]), np.array([180, 255, 255]))
+        red_mask = np.clip(red_mask1.astype(np.uint16) + red_mask2.astype(np.uint16), 0, 255).astype(np.uint8)
+        #cv2.imwrite("red_mask.png", cv2.cvtColor(red_mask, cv2.COLOR_GRAY2RGB))
+        red_binary += np.clip(red_mask, 0, 1)
+        red_binary = np.clip(red_binary, 0, 1)
+        #cv2.imwrite("red_binary.png", cv2.cvtColor(red_binary * 255, cv2.COLOR_GRAY2RGB))
+        red_weight = np.average(red_binary)
+
+        # Compute weight of the yellow light.
+        yellow_luma = signal_luma[y_min_yellow:y_max_yellow, x_min_sig:x_max_sig]
+        #cv2.imwrite("yellow_luma.png", cv2.cvtColor(yellow_luma, cv2.COLOR_GRAY2RGB))
+        yellow_binary = np.zeros(yellow_luma.shape, dtype=np.uint8)
+        yellow_binary[(yellow_luma >= LUMA_THRESHOLD[0]) & (yellow_luma <= LUMA_THRESHOLD[1])] = 1
+        yellow_hsv = signal_hsv[y_min_yellow:y_max_yellow, x_min_sig:x_max_sig]
+        yellow_mask = cv2.inRange(yellow_hsv, np.array([20, 100, 100]), np.array([30, 255, 255]))
+        #cv2.imwrite("yellow_mask.png", cv2.cvtColor(yellow_mask, cv2.COLOR_GRAY2RGB))
+        yellow_binary += np.clip(yellow_mask, 0, 1)
+        yellow_binary = np.clip(yellow_binary, 0, 1)
+        #cv2.imwrite("yellow_binary.png", cv2.cvtColor(yellow_binary * 255, cv2.COLOR_GRAY2RGB))
+        yellow_weight = np.average(yellow_binary)
+
+        # Compute weight of the green light.
+        green_luma = signal_luma[y_min_green:y_max_green, x_min_sig:x_max_sig]
+        #cv2.imwrite("green_luma.png", cv2.cvtColor(green_luma, cv2.COLOR_GRAY2RGB))
+        green_binary = np.zeros(green_luma.shape, dtype=np.uint8)
+        green_binary[(green_luma >= LUMA_THRESHOLD[0]) & (green_luma <= LUMA_THRESHOLD[1])] = 1
+        green_hsv = signal_hsv[y_min_green:y_max_green, x_min_sig:x_max_sig]
+        green_mask = cv2.inRange(green_hsv, np.array([50, 100, 100]), np.array([70, 255, 255]))
+        #cv2.imwrite("green_mask.png", cv2.cvtColor(green_mask, cv2.COLOR_GRAY2RGB))
+        green_binary += np.clip(green_mask, 0, 1)
+        green_binary = np.clip(green_binary, 0, 1)
+        #cv2.imwrite("green_binary.png", cv2.cvtColor(green_binary * 255, cv2.COLOR_GRAY2RGB))
+        green_weight = np.average(green_binary)
+
+        # Classify lights.
+        lights = [(TrafficLight.RED, red_weight),
+                  (TrafficLight.YELLOW, yellow_weight),
+                  (TrafficLight.GREEN, green_weight)]
+        lights.sort(key=lambda tup: tup[1], reverse=True)
+        rospy.loginfo("Lights %s", lights)
+        if lights[0][1] < MIN_LIGHT_WEIGHT or lights[0][1] - lights[1][1] < MIN_LIGHT_WEIGHT:
+            return TrafficLight.UNKNOWN
+
+        return lights[0][0]
