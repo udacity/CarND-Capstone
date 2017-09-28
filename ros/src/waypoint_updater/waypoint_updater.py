6f7bb8201bb192b2a3936f1756a865b05ce16328
Milutin Nikolic <ajsmilutin@gmail.com>
2017-09-29 01:30:20 +0200

    Initiall commit

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,7 +3,9 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
-
+from std_msgs.msg import Int32
+from itertools import chain
+import tf
 import math
 
 '''
@@ -22,7 +24,7 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
-
+PUBLISH_PERIOD = 0.5
 
 class WaypointUpdater(object):
     def __init__(self):
@@ -32,21 +34,64 @@ class WaypointUpdater(object):
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
+        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
+        rospy.Subscriber('/obstacle_waypoint', Int32, self.obstacle_cb)
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
+        self.last_pose_time = 0
 
+        self.waypoints_received = False
+        self.all_waypoints = []
+        self.last_waypoint_idx = 0
         # TODO: Add other member variables you need below
 
         rospy.spin()
 
     def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+
+        current_time = msg.header.stamp.secs + 1e-9 * msg.header.stamp.nsecs
+        if not self.waypoints_received:
+            return
+        if (current_time > self.last_pose_time + PUBLISH_PERIOD):
+            self.last_pose_time = current_time
+
+            concatenated = chain(range(self.last_waypoint_idx, len(self.all_waypoints)),
+                                 range(self.last_waypoint_idx))
+            rospy.logwarn(len(self.all_waypoints))
+            x = msg.pose.position.x
+            y = msg.pose.position.y
+            quaternion = (
+            msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w)
+            euler = tf.transformations.euler_from_quaternion(quaternion)
+            # direction unit vector
+            ux = math.cos(euler[2])
+            uy = math.sin(euler[2])
+
+
+            for idx in concatenated:
+                pos = self.all_waypoints[idx].pose.pose
+                wx = pos.position.x - x
+                wy = pos.position.y - y
+                intensity = math.sqrt(wx * wx + wy * wy)
+                wx = wx / intensity
+                wy = wy / intensity
+                # if dot producti is > sqrt(2)/2 which corresponds to 45 degrees
+                if (ux * wx + uy * wy) > math.sqrt(2) / 2:
+                    next_points = Lane()
+                    next_points.header.stamp = rospy.Time.now()
+                    next_points.waypoints = [self.all_waypoints[i] for i in
+                                             range(idx, (idx + LOOKAHEAD_WPS) % len(self.all_waypoints))]
+                    self.final_waypoints_pub.publish(next_points)
+                    self.last_waypoint_idx = idx
+                    return
+
 
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+        rospy.loginfo("Waypoints received")
+        self.all_waypoints = waypoints.waypoints
+        self.waypoints_received = True
+
+
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
