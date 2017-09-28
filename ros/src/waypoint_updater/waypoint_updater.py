79a6d5c6a7cae968fae0f69342b0e3128738536a
Kevin James <kjames4@gmail.com>
2017-09-27 18:26:55 -0700

    Use distance in case of horizontal track

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -6,7 +6,8 @@ from styx_msgs.msg import Lane, Waypoint
 from std_msgs.msg import Int32
 
 import math
-import tf
+import tf.transformations
+import angles
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -43,6 +44,8 @@ class WaypointUpdater(object):
         self.base_lane = None
         self.traffic_wp = None
         self.obstacle_wp = None
+        self.seqnum = 0
+        self.wp_start_q = -1
 
         rospy.spin()
 
@@ -53,82 +56,63 @@ class WaypointUpdater(object):
             quaternion = (msg.pose.orientation.x, msg.pose.orientation.y,
                           msg.pose.orientation.z, msg.pose.orientation.w)
             roll, pitch, yaw = tf.transformations.euler_from_quaternion(quaternion)
-            yaw = yaw % (2.0 * math.pi)
+            veh_yaw = yaw % (2.0 * math.pi)
+            veh_x = msg.pose.position.x
+            veh_y = msg.pose.position.y
             
             # the course hits an inflection point at x=2339 yaw=90+ at which point x starts to decrease
             # next inflection at x=155 yaw=270+ at which point x starts to increase
             # in front of the car = increasing x for yaw between 270 and 90 and decreasing x from 90 to 270
+
+            #veh_fwd = True
+            #if veh_yaw > (math.pi/2) and veh_yaw <= (3*math.pi/2):  # 90 to 270 degrees
+            #    veh_fwd = False
+
             pub_waypoints = []
-            wp_cnt = 0
-            veh_fwd = True
-            if yaw > (math.pi/2) and yaw <= (3*math.pi/2):  # 90 to 270 degrees
-                veh_fwd = False
-            wp_first = -1
-            if veh_fwd:
-                wp_x_cmp = 1e9
-            else:
-                wp_x_cmp = -1.0
-            max_decreasing = -1.0
-            min_increasing = 1e9
-            max_dec_idx = -1
-            min_inc_idx = -1
+            wp_start = -1
+            min_dist = 1e9
             if len(self.base_lane.waypoints) > 1:
                 # find the first waypoint in front of the vehicle,
                 #   then take a sequence of LOOKAHEAD_WPS waypoints
-                wp_q_x = self.base_lane.waypoints[0].pose.pose.position.x
-                wp_fwd_q = self.base_lane.waypoints[1].pose.pose.position.x > wp_q_x
-                for wp_cnt in range(len(self.base_lane.waypoints)):
-                    wp_x = self.base_lane.waypoints[wp_cnt].pose.pose.position.x
-                    if wp_q_x == wp_x:
-                        wp_fwd = wp_fwd_q
-                    else:
-                        wp_fwd = wp_x > wp_q_x
-                    if wp_fwd:
-                        if wp_x < min_increasing or min_inc_idx < 0:
-                            min_increasing = wp_x
-                            min_inc_idx = wp_cnt
-                    else:
-                        if wp_x > max_decreasing or max_dec_idx < 0:
-                            max_decreasing = wp_x
-                            max_dec_idx = wp_cnt
-                    if veh_fwd:
-                        # find the smallest increasing value greater than position
-                        if wp_fwd and wp_x > msg.pose.position.x and wp_x < wp_x_cmp:
-                            wp_x_cmp = wp_x
-                            wp_first = wp_cnt
-                    else:
-                        # find the largest decreasing value less than position
-                        if not wp_fwd and wp_x < msg.pose.position.x and wp_x > wp_x_cmp:
-                            wp_x_cmp = wp_x
-                            wp_first = wp_cnt
-                    
-                    wp_fwd_q = wp_fwd
-                    wp_q_x = self.base_lane.waypoints[wp_cnt].pose.pose.position.x
-                    wp_cnt = 0
-            if wp_first < 0:
-                if veh_fwd:
-                    # inflection: take the max decreasing waypoint since there was no larger point found
-                    wp_first = max_dec_idx
-                else:
-                    # inflection: take the min increasing waypoint since there was no smaller point found
-                    wp_first = min_inc_idx
-            if wp_first >= 0:
+                for i in range(len(self.base_lane.waypoints)):
+                    wp_idx = i
+                    if self.wp_start_q >= 0:
+                        wp_idx = (self.wp_start_q + i) % len(self.base_lane.waypoints)                    
+                    wp = self.base_lane.waypoints[wp_idx]
+                    wp_x = wp.pose.pose.position.x
+                    wp_y = wp.pose.pose.position.y
+                    veh_to_wp_dist = math.sqrt((wp_x - veh_x)**2 + (wp_y - veh_y)**2)
+                    if veh_to_wp_dist < min_dist:
+                        theta = math.atan2(wp_y - veh_y, wp_x - veh_x)
+                        # make sure the waypoint is in front of the car
+                        if abs(angles.shortest_angular_distance(theta, veh_yaw)) < math.pi/4.0:
+                            rospy.loginfo("wp_x %f wp_y %f veh_x %f veh_y %f dist %f theta %f yaw %f",
+                                          wp_x, wp_y, veh_x, veh_y, veh_to_wp_dist, theta, veh_yaw)
+                            min_dist = veh_to_wp_dist
+                            wp_start = wp_idx
+                            # we should be able to stop iterating because the next waypoint should be in front
+                            #   of the previous waypoint...for now, starting where we left off should at least
+                            #   eliminate a lot of atan2 calls
+
+            if wp_start >= 0:
                 for wp_idx in range(LOOKAHEAD_WPS):
-                    idx = (wp_first + wp_idx) % len(self.base_lane.waypoints)
+                    idx = (wp_start + wp_idx) % len(self.base_lane.waypoints)
                     pub_waypoints.append(self.base_lane.waypoints[idx])
-                    wp_cnt = wp_cnt + 1
+                self.wp_start_q = wp_start
                 # Add velocity command to waypoints
                 # TESTING
                 for wp_cnt in range(len(pub_waypoints)):
                     self.set_waypoint_velocity(pub_waypoints, wp_cnt, 20.0)
                 l = Lane()
-                l.header = msg.header
+                l.header.seq = self.seqnum
+                self.seqnum = self.seqnum + 1
+                l.header.stamp = rospy.get_rostime()
                 l.waypoints = pub_waypoints
                 self.final_waypoints_pub.publish(l)
                             
-            rospy.loginfo("wp_first %d x %f pose.x %f yaw %f fwd %s wp_cnt %d",
-                          wp_first, self.base_lane.waypoints[wp_first].pose.pose.position.x,
-                          msg.pose.position.x, yaw, veh_fwd, wp_cnt)
+            rospy.loginfo("wp_start %d min_dist %f wp.x %f pose.x %f veh_yaw %f wp_cnt %d",
+                          wp_start, min_dist, self.base_lane.waypoints[wp_start].pose.pose.position.x,
+                          msg.pose.position.x, veh_yaw, wp_cnt)
 
         pass
 
