ae34a4a6c6b856120a461bfebf719c9fab58a04f
jfjensen <jesfinkjensen@gmail.com>
2017-09-28 09:35:42 -0700

    fix: further improvement of PID params

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -80,7 +80,9 @@ class DBWNode(object):
         # self.controller = Controller(kp=0.9, ki=0.0, kd=0.0, vc=vc) # good
         # self.controller = Controller(kp=1.0, ki=0.0, kd=0.0, vc=vc) #
         # self.controller = Controller(kp=1.6, ki=0.0, kd=0.01, vc=vc) # good
-        self.controller = Controller(kp=1.6, ki=0.0, kd=0.04, vc=vc) # good
+        # self.controller = Controller(kp=1.6, ki=0.0, kd=0.04, vc=vc) # good ++++++++++++++
+        # self.controller = Controller(kp=1.6, ki=0.00005, kd=0.04, vc=vc) # good ++++++++++++
+        self.controller = Controller(kp=1.6, ki=0.00001, kd=0.04, vc=vc) # good
 
         
 
