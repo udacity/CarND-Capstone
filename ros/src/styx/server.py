f6b137f4878ac1325adbf7ddf8676de42a94bbc7
Marc Reichelt <mcreichelt@gmail.com>
2017-09-29 13:25:26 +0200

    fix bug: do not use monkey_patch and async mode

--- ros/src/styx/server.py
+++ ros/src/styx/server.py
@@ -9,9 +9,7 @@ from flask import Flask, render_template
 from bridge import Bridge
 from conf import conf
 
-eventlet.monkey_patch()
-
-sio = socketio.Server(async_mode='eventlet')
+sio = socketio.Server()
 app = Flask(__name__)
 msgs = []
 
