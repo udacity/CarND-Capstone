33f5343f3937cb4ff0595c6440459ead9cfbc153
rvarun7777 <rvarun7777@gmail.com>
2017-09-30 09:50:31 +0200

    tweaks to pid
    tweaks to pid

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,22 +1,60 @@
+from yaw_controller import YawController
+from lowpass import LowPassFilter
 from pid import PID
-
+import time
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
+MAX_SPEED = 40.0
 
 
 class Controller(object):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         # TODO: Implement
-        kp = 0.5
-        ki = 0
-        kd = 0
-        self.speed_pid = PID(kp, ki, kd)
-        pass
+        self.throttle_pid = PID(kwargs['throttle_gains'])
+        self.yaw_control = YawController(kwargs['wheel_base'], kwargs['steer_ratio'],
+                                         kwargs['min_speed'], kwargs['max_lat_accel'],
+                                         kwargs['max_steer_angle'],
+                                         kwargs['steering_gains']
+                                         )
+        self.last_t = None
+        #self.filter = LowPassFilter(0.2,0.1)
 
-    def control(self, error):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
+    '''
+    Params:
+    target_v - desired linear velocity
+    target_w - desired angular velocity
+    current_v - current linear velocity
+    dbw_enabled - drive by wire enabled (ignore error in this case)
+    '''
 
-        throttle = self.speed_pid.step(error, 0.1)
-        return throttle, 0., 0.
+    def control(self, target_v, target_w, current_v, dbw_enabled):
+        # Get throttle value from controller
+        if self.last_t is None or not dbw_enabled:
+            self.last_t = time.time()
+            return 0.0, 0.0, 0.0
+
+        dt = time.time() - self.last_t
+        error_v = min(target_v.x, MAX_SPEED*ONE_MPH) - current_v.x
+        throttle = self.throttle_pid.step(error_v, dt)
+        throttle = max(0.0, min(1.0, throttle))
+        if error_v < 0:
+            brake = -10.0*error_v   # Proportional braking
+            brake = max(brake, 1.0)
+            throttle = 0.0
+        else:
+            brake = 0.0
+
+        # # Special case for stopping
+        # if abs(target_v.x) < 0.1:
+        #     brake = 12.0
+
+        steer = self.yaw_control.get_steering(target_v.x, target_w.z, current_v.x)
+        # if(target_v.x <= 1.0):
+        #     brake = 6.0
+        #     throttle = 0.0
+        #     #steer = 0.0
+
+        #steer = self.filter.filt(steer)
+        self.last_t = time.time()
+        return throttle, brake, steer
