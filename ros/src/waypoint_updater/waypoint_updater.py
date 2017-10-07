a28d48c28abb552157083bfd7a42754e34ce0803
ndrplz <ndrplz@gmail.com>
2017-10-07 09:25:41 -0700

    Implement Waypoint Updater (Partial)
    Implement partially the `waypoint_updater.py` node, following Udacity suggested
    workflow.
    
    The node takes the list of all waypoints and the current car position and publishes
    the list of waypoints ahead of the car, sorted according to distance from the car.
    
    Debugging is not easy until `dbw` node is also implemented. So I did my best to debug
    singularly this node but I cannot guarantee this really works.

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,8 +3,12 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
-
 import math
+from waypoint_helper import is_ahead
+from waypoint_helper import get_distance_from_waypoint
+from waypoint_helper import compose_lane_message
+from waypoint_helper import print_waypoint
+
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -21,39 +25,84 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+VERBOSE = True       # Print debug logs
+LOOKAHEAD_WPS = 200  # Number of waypoints we will publish in /final_waypoints.
 
 
 class WaypointUpdater(object):
     def __init__(self):
         rospy.init_node('waypoint_updater')
 
-        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
-        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
+        rospy.Subscriber('/current_pose', PoseStamped, self.pose_callback)
+        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_callback)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
-
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
-        # TODO: Add other member variables you need below
+        self.frame_id       = None
+        self.current_pose   = None  # current vehicle 3D pose (position + orientation)
 
-        rospy.spin()
+        self.base_waypoints = None  # list of all waypoints of the track
 
-    def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+        self.loop()
 
-    def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+    def loop(self):
+
+        rate = rospy.Rate(10)
+
+        while not rospy.is_shutdown():
+
+            rate.sleep()
+
+            if self.base_waypoints is None or self.current_pose is None:
+                continue  # just wait
+
+            waypoints_ahead = []
+
+            for waypoint in self.base_waypoints:
+
+                if is_ahead(waypoint, self.current_pose):
+
+                    wp_distance = get_distance_from_waypoint(waypoint, self.current_pose)
+
+                    waypoints_ahead.append([waypoint, wp_distance])
+
+            # Sort by distance s.t. the first one is the closest
+            waypoints_ahead = sorted(waypoints_ahead, key=lambda x: x[1])
+
+            # Keep only the closest waypoints (also discard distances used to order waypoints)
+            waypoints_ahead = [item[0] for item in waypoints_ahead[:LOOKAHEAD_WPS]]
+
+            # Create Lane message with list of waypoints ahead
+            lane_message = compose_lane_message(self.frame_id, waypoints_ahead)
+
+            # Publish to final waypoints
+            self.final_waypoints_pub.publish(lane_message)
+
+            if VERBOSE:
+                print_waypoint(waypoints_ahead[0], msg='Next waypoint: ')
+
+    def pose_callback(self, msg):
+        """
+        Update current frame id and vehicle 3D pose (position + orientation)
+        """
+        self.frame_id = msg.header.frame_id
+        self.current_pose = msg.pose
+
+    def waypoints_callback(self, waypoints):
+        """
+        Store the list of all waypoints.
+        Notice that publisher for `/base_waypoints` publishes only once.
+        """
+        self.base_waypoints = waypoints.waypoints
 
     def traffic_cb(self, msg):
-        # TODO: Callback for /traffic_waypoint message. Implement
+        # TODO: Callback for /traffic_waypoint message. Implement it later
         pass
 
     def obstacle_cb(self, msg):
-        # TODO: Callback for /obstacle_waypoint message. We will implement it later
+        # TODO: Callback for /obstacle_waypoint message. Implement it later
         pass
 
     def get_waypoint_velocity(self, waypoint):
