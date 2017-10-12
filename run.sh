4a4e6489f497c502a3429996ae88fd1cd15e84fb
Jin <jinjiang119@gmail.com>
2017-10-12 23:25:26 +0200

    minor update run.sh: add log directory

--- run.sh
+++ run.sh
@@ -10,5 +10,5 @@ if [ "$(docker ps -a | grep ${CONTAINER_NAME})" ]; then
   echo "Attaching to running container..."
   docker exec -it ${CONTAINER_NAME} bash $@
 else
-  docker run --name ${CONTAINER_NAME} --rm -it -p 4567:4567 -v "${UDACITY_SOURCE}:/udacity" ${UDACITY_IMAGE} $@
+  docker run --name ${CONTAINER_NAME} --rm -it -p 4567:4567 -v "${UDACITY_SOURCE}:/udacity" -v "${UDACITY_SOURCE}/log:/home/udacity/.ros/log" ${UDACITY_IMAGE} $@
 fi
