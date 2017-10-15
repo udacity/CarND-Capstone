1c4f444a2b3598bd000cdf3189f39a5647d62a0b
Jin <jinjiang119@gmail.com>
2017-10-15 18:13:08 +0200

    udpate bridge: add frame_id "/world" for twist_cmd

--- ros/src/styx/bridge.py
+++ ros/src/styx/bridge.py
@@ -95,6 +95,8 @@ class Bridge(object):
 
     def create_twist(self, velocity, angular):
         tw = TwistStamped()
+        tw.header.frame_id = '/world'
+        tw.header.stamp = rospy.get_rostime()
         tw.twist.linear.x = velocity
         tw.twist.angular.z = angular
         return tw
