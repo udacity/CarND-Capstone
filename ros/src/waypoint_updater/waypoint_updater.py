e4c0681c9ccd7e05a955abe6d7d27d4c545fac7d
Brahm Windeler <github@b2brahm.com>
2017-10-18 15:08:18 -0700

    Added subscriber to /traffic_waypoint and publishers for debugging in rviz.

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,12 +3,14 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
+from std_msgs.msg import Int32
+from visualization_msgs.msg import Marker
 import math
+import copy
 from waypoint_helper import is_ahead
 from waypoint_helper import get_distance_from_waypoint
 from waypoint_helper import compose_lane_message
 from waypoint_helper import print_waypoint
-
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -35,22 +37,33 @@ class WaypointUpdater(object):
 
         rospy.init_node('waypoint_updater')
 
+        # Used to determine which waypoints lie ahead of the car
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_callback)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_callback)
 
-        # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
+        # DONE: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
+        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
+        #rospy.Subscriber('/obstacle_waypoint', Lane, self.obstacle_cb)
 
-        self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
+        # Publish the final waypoints for the pure_pursuit node
+        self.final_waypoints_pub = rospy.Publisher('/final_waypoints', Lane, queue_size=1)
+
+        # Publish waypoints for debugging
+        self.pub_car_path = rospy.Publisher('/car_path', Marker, queue_size=1)
+        self.pub_next_wp = rospy.Publisher('/next_wp', PoseStamped, queue_size=1)
+        self.pub_next_tl = rospy.Publisher('/next_tl', PoseStamped, queue_size=1)
 
         self.frame_id       = None
         self.current_pose   = None  # current vehicle 3D pose (position + orientation)
 
         self.base_waypoints = None  # list of all waypoints of the track
+        self.num_base_waypoints = 0 # number of all waypoints of the track
+
+        self.tl_index       = -1    # Index of the next traffic light from /traffic_waypoint
 
         self.loop()
 
     def loop(self):
-
 
         # Set the desired processing rate to 10Hz
         rate = rospy.Rate(10)
@@ -79,11 +92,20 @@ class WaypointUpdater(object):
             # Keep only the closest waypoints (also discard distances used to order waypoints)
             waypoints_ahead = [item[0] for item in waypoints_ahead[:LOOKAHEAD_WPS]]
 
+            # Apply deceleration as necessary
+            # TODO: [brahm] Check for traffic light and its state
+            if self.tl_index > -1:
+                self.apply_deceleration(waypoints_ahead)
+
             # Create Lane message with list of waypoints ahead
             lane_message = compose_lane_message(self.frame_id, waypoints_ahead)
 
             # Publish to final waypoints
             self.final_waypoints_pub.publish(lane_message)
+
+            # DEBUGGING
+            self.publish_car_path(waypoints_ahead)
+            self.publish_next_wp(waypoints_ahead[0])
 
             if VERBOSE:
                 if len(waypoints_ahead) > 0:
@@ -108,10 +130,62 @@ class WaypointUpdater(object):
         """
         print('waypoint_updater: p_tl: BASE WAYPOINTS UPDATES')
         self.base_waypoints = waypoints.waypoints
+        self.num_base_waypoints = len(self.base_waypoints)
 
     def traffic_cb(self, msg):
-        # TODO: Callback for /traffic_waypoint message. Implement it later
+        # DONE: Callback for /traffic_waypoint message. Implement it later
+
+        if VERBOSE:
+            print ("traffic_cb: started.")
+
+        # If we are not at the current traffic light waypoint index
+        if self.tl_index != msg.data:
+
+            # Update the traffic light index
+            self.tl_index = msg.data
+
+            if VERBOSE:
+                print ("traffic_cb: changing tl_index: {}".format(self.tl_index))
+
+            # Publish the actual traffic light waypoint for debugging purposes
+            if self.base_waypoints is not None and self.tl_index > -1:
+                self.publish_next_tl_wp(self.base_waypoints[self.tl_index])
+
+    def apply_deceleration(self, waypoints):
+        # TODO: [brahm] implement deceleration to waypoints
         pass
+
+    def publish_car_path(self, waypoints):
+        path = Marker()
+        path.header.frame_id = self.frame_id
+        path.header.stamp = rospy.Time.now()
+        path.ns = "path"
+        path.id = 0
+        path.action = Marker.ADD
+        path.type = Marker.LINE_LIST
+        path.scale.x = 0.1
+        path.color.r = 1.0
+        path.color.a = 0.5
+        path.points = []
+        for waypoint in waypoints:
+            position = copy.deepcopy(waypoint.pose.pose.position)
+            path.points.append(position)
+            position = copy.deepcopy(position)
+            position.z = 0.1 # height
+            path.points.append(position)
+        self.pub_car_path.publish(path)
+
+    def publish_next_wp(self, waypoint):
+        next_wp = copy.deepcopy(waypoint.pose)
+        next_wp.header.frame_id = self.frame_id
+        next_wp.header.stamp = rospy.Time.now()
+        self.pub_next_wp.publish(next_wp)
+
+    def publish_next_tl_wp(self, waypoint):
+        next_tl = copy.deepcopy(waypoint.pose)
+        next_tl.header.frame_id = self.frame_id
+        next_tl.header.stamp = rospy.Time.now()
+        self.pub_next_tl.publish(next_tl)
 
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. Implement it later
@@ -126,9 +200,9 @@ class WaypointUpdater(object):
     def distance(self, waypoints, wp1, wp2):
         dist = 0
         dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
-        for i in range(wp1, wp2+1):
-            dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
-            wp1 = i
+        n = len(waypoints)
+        for i in range(wp1, wp2):
+            dist += dl(waypoints[i%n].pose.pose.position, waypoints[(i+1)%n].pose.pose.position)
         return dist
 
 
