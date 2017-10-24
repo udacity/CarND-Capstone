2fb499b243648324c43541b1a0f21a354e1d8e14
jin <jinjiang119@gmail.com>
2017-10-25 00:06:34 +0200

    tl_classifier: create graph during init

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -14,7 +14,8 @@ class TLClassifier(object):
         self.image_tensor = None
         self.score_tensor = None
         self.class_tensor = None
-        self.graph_created = False
+
+        self.create_detection_graph("./frozen_inference_graph.pb")
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -26,11 +27,6 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        if not self.graph_created:
-            # TODO: Check how to safely include this model file when buliding catkin package.
-            self.create_detection_graph("./frozen_inference_graph.pb")
-            return
-
         traffic_light_index = self.infer(image)
         return TLClassifier.convert_tl_id(traffic_light_index)
 
@@ -43,11 +39,10 @@ class TLClassifier(object):
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
 
-        self.session = tf.Session(graph=self.detection_graph)
-        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-        self.score_tensor = self.detection_graph.get_tensor_by_name('detection_scores:0')
-        self.class_tensor = self.detection_graph.get_tensor_by_name('detection_classes:0')
-        self.graph_created = True
+            self.session = tf.Session(graph=self.detection_graph)
+            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+            self.score_tensor = self.detection_graph.get_tensor_by_name('detection_scores:0')
+            self.class_tensor = self.detection_graph.get_tensor_by_name('detection_classes:0')
 
     def infer(self, image):
         image_np_expanded = np.expand_dims(image, axis=0)
