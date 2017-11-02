4931d90b60713c8941b5a82e9d27073b61e4c681
Yu Shen <yubrshen@gmail.com>
2017-11-02 14:24:11 -0700

    recover LOOKAHEAD_WPS to 200

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -21,7 +21,7 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 3 # 200 # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
 
 import tf                       # This is of ROS geometry, not of TensorFlow!
 def get_yaw(orientation):
