d9001eb32df81a0a4e12a183ab0b45665b09bdb4
sf624 <shun.fujioka624@gmail.com>
2017-11-10 00:02:51 +0900

    first attempt.

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -5,6 +5,8 @@ from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
 
 import math
+import tf
+import numpy as np
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -33,7 +35,6 @@ class WaypointUpdater(object):
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
-
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
@@ -42,14 +43,52 @@ class WaypointUpdater(object):
 
     def pose_cb(self, msg):
         # TODO: Implement
+        self.pose = msg
+        ego_q = msg.orientation
+        ego_direction_3d = inverse(ego_q) * Quaternion(1,0,0,0) * ego_q
+        ego_vx = np.array([ego_direction_3d.x,ego_direction_3d,y])
+        ego_vy = np.array([[0,-1],[1,0]]) * ego_vx
+
+        try:
+            waypoints
+
+            # publish final_waypoints
+            min_distance = 1e9
+            initial_wp_index = 0
+            for i in xrange(len(waypoints)):
+                    waypoint = waypoints[i].pose.pose.position
+                    np_waypoint = np.array([waypoint.x,waypoint.y])
+                    np_ego_pose = np.array([msg.position.x,msg.position.y])
+                    np_diff = np_waypoint - np_ego_pose
+                    distance = np.linalg.norm(np_diff)
+                    param = np.linalg.inv(np.concatenate((ego_vx,ego_vy),axis=0)) * np_diff
+
+                    # If the waypoint is in front of vehicle and also the closest one,
+                    # update the index.
+                    if(param[0] > 0 and distance < min_distance):
+                        min_distance = distance
+                        initial_wp_index = i % len(waypoints)
+
+            # publish
+            final_waypoints = []
+            for i in xrange(len(LOOKAHEAD_WPS)):
+                index = (initial_wp_index + i) % len(waypoints)
+                final_waypoints.append(waypoints[index])
+            final_waypoints_pub(final_waypoints)
+
+        except NameError:
+            # do nothing
+
         pass
 
     def waypoints_cb(self, waypoints):
         # TODO: Implement
+        self.waypoints = waypoints
         pass
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
+        self.traffic = msg
         pass
 
     def obstacle_cb(self, msg):
