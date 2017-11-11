1c5efeb945d344aee021c034236e2e49d4226181
sahiljuneja <sahiljuneja17@gmail.com>
2017-11-11 06:41:14 -0800

    modify waypoint_lib directory structure

--- ros/src/waypoint_lib/setup.py
+++ ros/src/waypoint_lib/setup.py
@@ -1,5 +1,5 @@
 ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
-
+#!/usr/bin/env python
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
