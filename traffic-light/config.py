c3cd064f752bbec81ea9890abf1f7a341e833157
JC <leejchris21@gmail.com>
2017-12-08 02:06:16 -0700

    add starting traffic light notebook. add dataset helper functions

--- traffic-light/config.py
+++ traffic-light/config.py
@@ -5,5 +5,6 @@
 download_dir = './data'
 
 datasets = [
-    'https://s3-us-west-2.amazonaws.com/traffic-light-data/tl_classifier_data.zip'
+    'https://s3-us-west-2.amazonaws.com/traffic-light-data/tl_classifier_data.zip',
+    'https://s3-us-west-2.amazonaws.com/traffic-light-data/dataset_additional_rgb.zip'
 ]
