03f9f31e24880c10bb94a05ef6c0d72977979f8e
JC <leejchris21@gmail.com>
2017-12-09 15:42:20 -0700

    clear download_data to download two datasets

--- research_notebooks/traffic-light/config.py
+++ research_notebooks/traffic-light/config.py
@@ -1,8 +1,12 @@
-
+import os
 
 # change me! absolute path to store datasets
 # download_dir = 'D:/lots/of/data'
 download_dir = './data'
+
+def set_download_path(dst):
+    print('Downloading to ', os.path.abspath(dst))
+    download_dir = dst
 
 datasets = [
     'https://s3-us-west-2.amazonaws.com/traffic-light-data/just-traffic-lights.zip',
