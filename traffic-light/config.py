c359cc9e75146f7d6e99471cf3eb0ddb70ffa98e
JC <leejchris21@gmail.com>
2017-12-09 15:21:49 -0700

    clean repo for kyle merge

--- traffic-light/config.py
+++
@@ -1,9 +0,0 @@
-
-
-# change me! absolute path to store datasets
-# download_dir = 'D:/lots/of/data'
-download_dir = './data'
-
-datasets = [
-    'https://s3-us-west-2.amazonaws.com/traffic-light-data/just-traffic-lights.zip',
-]
