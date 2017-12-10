2c5796bd324ad0955e54c62537df50eefead2d05
JC <leejchris21@gmail.com>
2017-12-10 15:43:00 -0700

    add orig tl_classifier.py -- it is stub code

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,25 +1,8 @@
-#!/usr/bin/env python
-# import rospy
 from styx_msgs.msg import TrafficLight
-# from sensor_msgs.msg import CameraInfo
-
-from collections import namedtuple
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-
-    #     # get camera_info like image size from yaml_to_CameraInfo_publisher
-    #     self.num_ch = 3 # number of color channels
-    #     self.Image_Size = namedtuple('image_size', 'x y ch')
-    #     sub_cam_info = rospy.Subscriber('/camera_info_publisher', CameraInfo, self.get_cam_info_cb
-    #
-    # def get_cam_info_cb(self, cam_info):
-    #     """
-    #     """
-    #     self.image_size = self.Image(x=cam_info.height, y=cam_info.width, ch=self.num_ch)
-    #
-    #     rospy.logwarn('image_size {}'.format(self.image_size))
         pass
 
     def get_classification(self, image):
