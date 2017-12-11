1ab32266c25b1dd46d10eb9732c30202751d51bf
Boris Dayma <boris.dayma@gmail.com>
2017-12-10 16:05:34 -0800

    update detection of red lights based on probability

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,6 +1,7 @@
 from styx_msgs.msg import TrafficLight
 import label_image
 import os.path
+import rospy
 
 import tensorflow as tf
 import numpy as np
@@ -23,14 +24,24 @@ class TLClassifier(object):
         label_path = os.path.join(os.path.dirname(__file__), LABEL_FILE)
         self.graph = label_image.load_graph(model_path)
         self.labels = label_image.load_labels(label_path)
+        self.red_idx = self.get_red_idx()
         self.input_operation = self.graph.get_operation_by_name(INPUT_LAYER)
         self.output_operation = self.graph.get_operation_by_name(OUTPUT_LAYER)
 
         # Create sessions
         self.sess = tf.Session(graph=self.graph)
 
+    def get_red_idx(self):
+        ''' Find index of the red class '''
+        
+        for n, l in enumerate(self.labels):
+            if l == "red":
+                return n
+        rospy.logerr("'red' was not found as a possible label in 'classifier_labels.txt'")
+        raise
+        
     def normalize_image(self, image, model_input_size=MODEL_INPUT_SIZE,
-			            input_mean=MODEL_INPUT_MEAN, input_std=MODEL_INPUT_STD):
+                        input_mean=MODEL_INPUT_MEAN, input_std=MODEL_INPUT_STD):
         ''' Ensure we convert the image to a format compatible with the model graph '''
         
         normalized_image = cv2.resize(image, (model_input_size, model_input_size))
@@ -38,13 +49,13 @@ class TLClassifier(object):
         return normalized_image
 
     def get_classification(self, image):
-        """Determines the color of the traffic light in the image
+        """Determines the probability of having a red light
 
         Args:
             image (cv::Mat): image containing the traffic light
 
         Returns:
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+            float: probability of having a red light
         """
 
         # Normalize image
@@ -54,7 +65,6 @@ class TLClassifier(object):
         results = self.sess.run(self.output_operation.outputs[0],
                            {self.input_operation.outputs[0]: normalized_image})
         results = np.squeeze(results)
-        prediction_idx = np.argmax(results)
-        prediction = self.labels[prediction_idx]
 
-        return prediction
+        # Probability of having a red light
+        return results[self.red_idx]
