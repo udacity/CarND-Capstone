4df7e063694c88e85a0f63f6dff8df5616de4f30
Boris Dayma <boris.dayma@gmail.com>
2017-12-16 22:59:57 -0800

    set up commands in percentage and adjust pid parameters
    Command for brake control was set in percentage.
    In addition, pid parameters were fine tuned. It closes #17, closes #18, and closes #28

--- ros/src/twist_controller/pid.py
+++ ros/src/twist_controller/pid.py
@@ -24,16 +24,11 @@ class PID(object):
         self.d_effort = 0
 
     def step(self, error, sample_time):
-        # We want to cap the integral portion by using only a limited number of measurements.
-        # However it is unefficient to keep updating a list of measurements. We decide then
-        # to estimate the replace it with an estimate of the average using an EMA.
-        self.int_val = 0.2 * error * sample_time + 0.8 * self.int_val
-
         derivative = (error - self.last_error) / sample_time
         # We calculate derivative portion only if we already had at least one measurement since reset
-        if any([self.last_error, self.int_val, self.p_effort, self.i_effort, self.d_effort]):
+        if not any([self.last_error, self.int_val, self.p_effort, self.i_effort, self.d_effort]):
             derivative = 0
-
+        self.int_val += error
         self.last_error = error
         
         self.p_effort = self.kp * error
@@ -41,6 +36,10 @@ class PID(object):
         self.d_effort = self.kd * derivative
 
         y = self.p_effort + self.i_effort + self.d_effort
+        if y > self.max:
+            # we want to ensure that the integral part won't grow too much and pid remains continuous
+            self.int_val = max((self.max - self.p_effort) / self.ki, 0)
+            self.i_effort = self.ki * self.int_val
         val = max(self.min, min(y, self.max))
 	
         return val
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -18,9 +18,9 @@ class Controller(object):
         self.max_let_accel = max_let_accel
         self.wheel_base = wheel_base
 
-        self.throttle_pid = PID(.15, .00, 0, self.decel_limit, self.accel_limit)
-        self.brake_pid = PID(25, 0, 0, 0, 100)
-        self.steer_filter = LowPassFilter(0.1, DT)
+        self.throttle_pid = PID(0.1, 0.001, 0, 0, 0.4)
+        self.brake_pid = PID(60., 1, 0, 0, 100)
+        self.steer_filter = LowPassFilter(0.2, DT)
         self.last_time = 0
         self.DT = DT
         self.brakeLatch = False
@@ -32,45 +32,36 @@ class Controller(object):
                 current_linear_velocity, dbw_status, log_handle):
         '''Defines target throttle, brake and steering values'''
 
-        if dbw_status:
-            # Update DT
-            new_time = rospy.get_rostime()
-            if self.last_time:  # The first time, we are not able to calculate DT
-                self.DT = (new_time - self.last_time).to_sec()
-            self.last_time = new_time
-
+        # Update DT
+        new_time = rospy.get_rostime()
+        if self.last_time:  # The first time, we are not able to calculate DT
+            self.DT = (new_time - self.last_time).to_sec()
+        self.last_time = new_time
+            
+        if dbw_status:            
             velocity_error = target_linear_velocity - current_linear_velocity
-            pid_throttle, feedforward_throttle, decel_target = 0, 0, 0
-
-            # implement longitudinal controller
 
             # if we're going too slow, release the brakes (if they are applied)
             # This essentially adds hysterisis: the brakes are only enabled if our velocity error is negative, and only
             # released if the velocity error is positive 2 or greater.
-            if velocity_error > 2:
+            if velocity_error > 1:
                 self.brakeLatch = False
-            if self.brakeLatch is False:
-                pid_throttle = self.throttle_pid.step(velocity_error, self.DT)
-                feedforward_throttle = target_linear_velocity*.01888 #based on desired speed, predict how much throttle we need at steady state
-                throttle = pid_throttle + feedforward_throttle
-                accel_limit = 1 #mps2
-                maxThrottle = 0.1962*accel_limit+0.083 # max throttle allowed for a given acceleration limit
-                throttle = max(0,min(throttle, maxThrottle))  # saturate throttle if it exceeds acceleration limits
+            if not self.brakeLatch:
+                throttle = self.throttle_pid.step(velocity_error, self.DT)
                 brake = 0
                 self.brake_pid.reset()
-                if current_linear_velocity >= .1 and velocity_error < 0 and throttle is 0:
+                # if we go too fast and we cannot decrease throttle, we need to start braking
+                if (velocity_error < 0 and throttle is 0) or (velocity_error < -1):
                     self.brakeLatch = True
-            # we need to brake when throttle PID is saturated at 0 and velocity error is negative. In other words, when
-            # we can't slow down any more (throttle is already zero), and we want to slow down more (speed error is
-            # negative), then we need to use the brakes
             else:
+                # We are currently braking
                 throttle = 0
                 self.throttle_pid.reset()
                 brake = self.brake_pid.step(-velocity_error, self.DT)
             # If we're about to come to a stop, clamp the brake command to some value to hold the vehicle in place
             if current_linear_velocity < .1 and target_linear_velocity == 0:
                 throttle = 0
-                brake = 25
+                brake = 0.5
 
             # implement steering controller
             steer_target = self.yaw_controller.get_steering(target_linear_velocity,
@@ -84,14 +75,14 @@ class Controller(object):
             self.brake_pid.reset()
             self.steer_filter.reset()
             throttle, brake, steering = 0, 0, 0
-            pid_throttle, feedforward_throttle, velocity_error = 0, 0, 0
+            pid_throttle, velocity_error = 0, 0
 
         # Log data
         throttle_P, throttle_I, throttle_D = self.throttle_pid.get_PID()
         brake_P, brake_I, brake_D = self.brake_pid.get_PID()
         steer_P, steer_I, steer_D = 0, 0, 0
         self.log_data(log_handle, throttle_P, throttle_I, throttle_D, brake_P, brake_I, brake_D,
-                          pid_throttle, feedforward_throttle, velocity_error, self.DT, int(self.brakeLatch))
+                          velocity_error, self.DT, int(self.brakeLatch))
 
         return throttle, brake, steering
     
