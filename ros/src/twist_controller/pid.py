c5ec378bef2728d563682016c617a848a19e0e5a
Boris Dayma <boris.dayma@gmail.com>
2017-12-16 16:21:56 -0800

    Correct integral portion of PID
    There was a mistake in calculating the integral portion of the PID. Also it was replaced by an EMA to ensure it would not become too large.

--- ros/src/twist_controller/pid.py
+++ ros/src/twist_controller/pid.py
@@ -14,21 +14,27 @@ class PID(object):
         self.i_effort = 0
         self.d_effort = 0
 
-        self.int_val = self.last_int_val = self.last_error = 0.
+        self.int_val = self.last_error = 0.
 
     def reset(self):
         self.int_val = 0.0
-        self.last_int_val = 0.0
         self.last_error = 0.0
         self.p_effort = 0
         self.i_effort = 0
         self.d_effort = 0
 
     def step(self, error, sample_time):
-        self.last_int_val = self.int_val
+        # We want to cap the integral portion by using only a limited number of measurements.
+        # However it is unefficient to keep updating a list of measurements. We decide then
+        # to estimate the replace it with an estimate of the average using an EMA.
+        self.int_val = 0.2 * error * sample_time + 0.8 * self.int_val
 
-        integral = self.int_val + error * sample_time
         derivative = (error - self.last_error) / sample_time
+        # We calculate derivative portion only if we already had at least one measurement since reset
+        if any([self.last_error, self.int_val, self.p_effort, self.i_effort, self.d_effort]):
+            derivative = 0
+
+        self.last_error = error
         
         self.p_effort = self.kp * error
         self.i_effort = self.ki * self.int_val
@@ -37,14 +43,6 @@ class PID(object):
         y = self.p_effort + self.i_effort + self.d_effort
         val = max(self.min, min(y, self.max))
 	
-        if val > self.max:
-            val = self.max
-        elif val < self.min:
-            val = self.min
-        else:
-            self.int_val = integral
-        self.last_error = error
-   
         return val
 
     def get_PID(self):
