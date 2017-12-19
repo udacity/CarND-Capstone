95fbe62e748c22e1e7486950dfe3c3cff46af50e
M0J01 <moodymusicman@gmail.com>
2017-12-19 08:02:43 -0800

    Waypoint updater is publishing the nearest 50 waypoints to /final_waypoints. Still need to clean up the code a little bit

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+import math
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
 
@@ -21,7 +22,7 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 50 # Number of waypoints we will publish. You can change this number
 
 
 class WaypointUpdater(object):
@@ -36,16 +37,92 @@ class WaypointUpdater(object):
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
+        self.current_position = []
+        self.base_waypoints = []
+        self.total_waypoints = 0
         # TODO: Add other member variables you need below
 
         rospy.spin()
 
+    def convert_local(self, waypoint, current_pos):
+        x_way = waypoint.pose.pose.position.x
+        y_way = waypoint.pose.pose.position.y
+        x_car = current_pos.pose.position.x
+        y_car = current_pos.pose.position.y
+        theta_car = 2*math.acos(current_pos.pose.orientation.w)
+        # Convert Quarternion to radian angle
+        x_shift = x_way - x_car
+        y_shift = y_way - y_car
+
+        theta_waypoint = 2*math.acos(waypoint.pose.pose.orientation.w)
+        #rospy.logwarn(theta_car)
+        #rospy.logwarn(theta_waypoint)
+        x = x_shift*math.cos(0-theta_car) - y_shift*math.sin(0 - theta_car)
+        y = x_shift*math.sin(0 - theta_car) + y_shift*math.cos(0 - theta_car)
+        return x,y, theta_car, theta_waypoint
+
     def pose_cb(self, msg):
         # TODO: Implement
+        #rospy.logwarn('Position Recieved') #Trouble shooting to ensure position is being recieved
+        #Decipher current position
+        current_pos = msg
+        #x = msg.pose.position.x
+        #y = msg.pose.position.y
+        #theta = msg.pose.orientation.w
+        #theta2 = msg.pose.orientation.z
+        #rospy.logwarn("current x = %s", x)
+        #rospy.logwarn("current y = %s", y)
+        #rospy.logwarn("current theta = %s", theta)
+        #rospy.logwarn("current theta2 = %s", theta2)
+        #Determine next waypoint
+        #Construct a Lane object with LOOKAHEAD_WPS number of waypoints
+        #Where next waypoint is element[0], and following elements are waypoints after that
+        #Call final_waypoints_pub
+
+        final_waypoints_list = []
+        for i in range(len(self.base_waypoints.waypoints)):
+            waypoint = self.base_waypoints.waypoints[i]
+            #rospy.logwarn(waypoint)
+            x,y,theta_car, theta_waypoint = self.convert_local(waypoint, current_pos)
+            #rospy.logwarn("X and Y = %s & %s", x,y)
+            #rospy.logwarn(self.total_waypoints)
+            orientation_match = math.cos(theta_waypoint - theta_car)
+            if(x > 0.00 and orientation_match > 0.707 ):
+                #final_waypoints_list.append(self.base_waypoints.waypoints[i])
+                for j in range(LOOKAHEAD_WPS):
+                    j_mod = i+j%self.total_waypoints
+                    final_waypoints_list.append(self.base_waypoints.waypoints[j_mod])
+                #rospy.logwarn(len(final_waypoints_list))
+                #rospy.logwarn(final_waypoints_list[0])
+                msg = Lane()
+                msg.waypoints = final_waypoints_list
+                self.final_waypoints_pub.publish(msg)
+                #rospy.logwarn(msg)
+                return
+        '''
+        waypoint = self.base_waypoints.waypoints[0]
+        rospy.logwarn(waypoint)
+        rospy.logwarn(current_pos)
+        x, y, theta_car, theta_waypoint = self.convert_local(waypoint, current_pos)        
+        rospy.logwarn("X and Y = %s & %s", x, y)
+        '''
+
         pass
 
     def waypoints_cb(self, waypoints):
         # TODO: Implement
+        #rospy.logwarn('Waypooints Recieved')   #Trouble shooting to ensure initial waypoints are being recieved
+        #first_x = waypoints.waypoints[0].twist.twist.linear.x
+
+        #waypoints_list = []
+        #first_x = waypoints.waypoints[0]
+        #for waypoint in waypoints.waypoints:
+        #    rospy.logwarn("Waypoint is : %s", waypoint)
+        #first_x = waypoints
+        #rospy.logwarn("Received first X as %s", first_x)
+        self.base_waypoints = waypoints
+        self.total_waypoints = len(self.base_waypoints.waypoints)
+        #rospy.logwarn(self.total_waypoints)
         pass
 
     def traffic_cb(self, msg):
@@ -55,6 +132,8 @@ class WaypointUpdater(object):
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
         pass
+
+
 
     def get_waypoint_velocity(self, waypoint):
         return waypoint.twist.twist.linear.x
