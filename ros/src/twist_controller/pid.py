5d931cba4b5d193517d33cbadfe2372b8599a1fc
Jayasim Jayakumar <jayasim@gmail.com>
2017-12-21 20:03:29 +0530

    refactor: Remove extra semicolon and add missing last_error member variable in PID

--- ros/src/twist_controller/pid.py
+++ ros/src/twist_controller/pid.py
@@ -16,14 +16,15 @@ class PID(object):
     def reset(self):
         self.int_val = 0.0
         self.last_int_val = 0.0
+        self.last_error = 0.0
 
     def step(self, error, sample_time):
         self.last_int_val = self.int_val
 
-        integral = self.int_val + error * sample_time;
-        derivative = (error - self.last_error) / sample_time;
+        integral = self.int_val + error * sample_time
+        derivative = (error - self.last_error) / sample_time
 
-        y = self.kp * error + self.ki * self.int_val + self.kd * derivative;
+        y = self.kp * error + self.ki * self.int_val + self.kd * derivative
         val = max(self.min, min(y, self.max))
 
         if val > self.max:
