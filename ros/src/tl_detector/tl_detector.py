15a789d06155548878da38bec4f9356bc4661700
Aniket <aniketk@umich.edu>
2018-01-16 17:48:50 -0800

    Integrate with rest of project

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -41,7 +41,7 @@ class TLDetector(object):
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
         self.bridge = CvBridge()
-        self.light_classifier = TLClassifier()
+        self.light_classifier = TLClassifier(simulator=True)
         self.listener = tf.TransformListener()
 
         self.state = TrafficLight.UNKNOWN
