72d4b9c5b0a59059b3778d772040e10c4d89a6c0
Sujesh Jastee <sujeshkj@rediffmail.com>
2018-01-20 13:25:58 -0800

    Initial updates for updating waypoints ahead of the car

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,7 +3,6 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
-
 import math
 
 '''
@@ -32,20 +31,75 @@ class WaypointUpdater(object):
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
+#        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
+#        rospy.Subscriber('/obstacle_waypoint', Int32, self.obstacle_cb)
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
-
+        self.base_waypoints = None # Store the waypoint list as it is published for the first time
+        self.pose           = None # Store current pose
         rospy.spin()
 
     def pose_cb(self, msg):
         # TODO: Implement
+        # For partial waypoint updater, publish just a fixed number of waypoints
+        # (LOOKAHEAD_WPS)currently ahead of the vehicle
+        
+        """
+            Steps:
+                1) Find closest waypoint
+                2) Send the next LOOKAHEAD_WPS waypoints
+            """
+        
+        self.pose = msg.pose
+        frame_id  = msg.header.frame_id
+        
+        if self.base_waypoints is not None:
+            closest_waypoint_index = find_closest_waypoint_index(self.pose, self.base_waypoints)
+            # To make sure that we don't overshoot the data points
+            number_of_points       = min(len(self.base_waypoints), closest_waypoint_index + LOOKAHEAD_WPS)
+            lookahead_waypoints    = deepcopy(self.base_waypoints[closest_waypoint_index:number_of_points])
+        
+        final_waypoints = make_final_waypoints(lookahead_waypoints)
+        self.final_waypoints_pub.publish(final_waypoints)
+        
         pass
 
+    def find_closest_waypoint_index(self, pose, base_waypoints):
+        closest_index = 0
+    
+        current_position = pose.position
+        min_difference   = float('inf')
+        
+        # Loop through all waypoints and find closest index
+        
+        for index, waypoint in enumerate(waypoints):
+            index_position = waypoint.pose.pose.position
+            difference     = get_euclidean_distance(current_position, index_position)
+        
+            if(difference < min_difference):
+                min_difference = difference
+                closest_index = index
+    
+        return closest_index
+    
+    def get_euclidean_distance(self, waypoint1, waypoint2):
+        return math.sqrt((waypoint1.x-waypoint2.x)**2 + (waypoint1.y-waypoint2.y)**2)
+    
+    def make_final_waypoints(self, frame_id, waypoints):
+        # Create the Lane object and fill in waypoints
+        lane                 = Lane()
+        lane.header.frame_id = frame_id
+        lane.waypoints       = waypoints
+        lane.header.stamp    = rosepy.Time.now()
+        return lane
+    
+    
     def waypoints_cb(self, waypoints):
         # TODO: Implement
+        # Store the base waypoints for use
+        self.base_waypoints = waypoints
         pass
 
     def traffic_cb(self, msg):
