1c40df24d54bd042c5af0324a5bd0326a98d7694
Abhay <abhaycl@hotmail.com>
2018-01-21 10:57:39 +0100

    Add files via upload

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -285,7 +285,7 @@ class TLDetector(object):
         # List of positions that correspond to the line to stop in front of for a given intersection.
         #stop_line_positions = self.config['stop_line_positions']
         if(self.pose):
-            car_position = self.get_closest_waypoint(self.pose.pose)
+            car_position = self.get_closest_waypoint(self.pose.position.x, self.pose.position.y)
             
             #TODO find the closest visible traffic light (if one exists)
             light_pos = None
