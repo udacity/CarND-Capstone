d17c0f8f0345f6d9481f67347a010b187417bc45
Johan Smet <johan.smet@justcode.be>
2018-02-08 18:48:23 +0100

    twist_controller: store configuration parameters in dictionary to make it easier to pass them around

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -35,17 +35,21 @@ class DBWNode(object):
     def __init__(self):
         rospy.init_node('dbw_node')
 
-        vehicle_mass = rospy.get_param('~vehicle_mass', 1736.35)
-        fuel_capacity = rospy.get_param('~fuel_capacity', 13.5)
-        brake_deadband = rospy.get_param('~brake_deadband', .1)
-        decel_limit = rospy.get_param('~decel_limit', -5)
-        accel_limit = rospy.get_param('~accel_limit', 1.)
-        wheel_radius = rospy.get_param('~wheel_radius', 0.2413)
-        wheel_base = rospy.get_param('~wheel_base', 2.8498)
-        steer_ratio = rospy.get_param('~steer_ratio', 14.8)
-        max_lat_accel = rospy.get_param('~max_lat_accel', 3.)
-        max_steer_angle = rospy.get_param('~max_steer_angle', 8.)
+        # fetch parameters
+        self.ros_params = {
+            'vehicle_mass' : rospy.get_param('~vehicle_mass', 1736.35),
+            'fuel_capacity' : rospy.get_param('~fuel_capacity', 13.5),
+            'brake_deadband' : rospy.get_param('~brake_deadband', .1),
+            'decel_limit' : rospy.get_param('~decel_limit', -5),
+            'accel_limit' : rospy.get_param('~accel_limit', 1.),
+            'wheel_radius' : rospy.get_param('~wheel_radius', 0.2413),
+            'wheel_base' : rospy.get_param('~wheel_base', 2.8498),
+            'steer_ratio' : rospy.get_param('~steer_ratio', 14.8),
+            'max_lat_accel' : rospy.get_param('~max_lat_accel', 3.),
+            'max_steer_angle' : rospy.get_param('~max_steer_angle', 8.)
+        }
 
+        # publishers
         self.steer_pub = rospy.Publisher('/vehicle/steering_cmd',
                                          SteeringCmd, queue_size=1)
         self.throttle_pub = rospy.Publisher('/vehicle/throttle_cmd',
