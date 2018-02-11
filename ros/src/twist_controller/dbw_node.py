3a5d0923732e3288c28fdabbfd4656489e08e7dd
Johan Smet <johan.smet@justcode.be>
2018-02-11 18:42:39 +0100

    twist_controller: prepare for ROS dynamic reconfigure
    The twist_controller.py file having the same name as the ros-node causes
    problems with the cfg-submodule generated by the ROS dynamic reconfigure
    infrastructure.

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -6,7 +6,7 @@ from dbw_mkz_msgs.msg import ThrottleCmd, SteeringCmd, BrakeCmd, SteeringReport
 from geometry_msgs.msg import TwistStamped
 import math
 
-from twist_controller import Controller
+from controller import Controller
 
 '''
 You can build this node only after you have built (or partially built) the `waypoint_updater` node.
