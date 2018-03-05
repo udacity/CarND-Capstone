466ae6c021cef6687902be20b8c2801fd887ed95
Anthony Knight <tntknight@gmail.com>
2018-03-04 22:36:07 -0500

    Add basic final_waypoints updating functionality (#9)
    * Add basic final_waypoints updating functionality
    
    This code publishes the next 200 waypoints to final_waypoints topic
    after each message is received on the current_pose topic. For now a
    dummy velocity is inserted into all the waypoints.
    
    * Fix error in send_waypoints so 200 points sent
    
    Closes issue #3 for 1st pass.

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import PoseStamped, TwistStamped
 from styx_msgs.msg import Lane, Waypoint
 
 import math
@@ -22,31 +22,57 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
-
+DEFAULT_VELOCITY = 5 # just picked a number for early tests
 
 class WaypointUpdater(object):
     def __init__(self):
         rospy.init_node('waypoint_updater')
 
-        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
-        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
+        self.subs = {}
+        self.pubs = {}
+
+        self.subs['/current_pose'] = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
+
+        self.subs['/base_waypoints'] = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
+
+        self.subs['/current_velocity'] = rospy.Subscriber('/current_velocity', TwistStamped,
+                                                       self.velocity_cb)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
-
-        self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
+        self.pubs['final_waypoints'] = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
 
+
+        self.waypoints = []
+        self.pose = None
+        self.velocity = None
+        self.final_waypoints = []
+
         rospy.spin()
 
-    def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+    def velocity_cb(self, twist_msg):
+        self.velocity = twist_msg
 
-    def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+
+    def pose_cb(self, pose_msg):
+        self.pose = pose_msg.pose
+        rospy.loginfo("waypoint_updater:pose_cb pose set to  %s", self.pose)
+        self.send_waypoints()
+        #pass
+
+    def waypoints_cb(self, lane_msg):
+        rospy.loginfo("waypoint_updater:waypoints_cb loading waypoints")
+        if not self.waypoints:
+            for waypoint in lane_msg.waypoints:
+                self.waypoints.append(waypoint)
+            rospy.loginfo("waypoint_updater:waypoints_cb %d waypoints loaded", len(self.waypoints))
+        else:
+            rospy.logerr("waypoint_updater:waypoints_cb attempt to load waypoints when we have "
+                "already loaded %d waypoints", len(self.waypoints))
+        self.subs['/base_waypoints'].unregister()
+        rospy.loginfo("Unregistered from /base_waypoints topic")
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
@@ -56,17 +82,65 @@ class WaypointUpdater(object):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
         pass
 
+    def send_waypoints(self):
+        #generates the list of LOOKAHEAD_WPS waypoints based on location of car
+        #for now assume loop
+        new_wps_list = []
+        start_wps_ptr = self.closest_waypoint()
+        end_wps_ptr = (start_wps_ptr + LOOKAHEAD_WPS) % len(self.waypoints)
+        rospy.loginfo("waypoint_updater:send_waypoints start_wps_ptr = %d, end_wps_ptr = %d",
+                      start_wps_ptr, end_wps_ptr)
+        if end_wps_ptr > start_wps_ptr:
+            for w_p in self.waypoints[start_wps_ptr:end_wps_ptr]:
+                w_p.twist.twist.linear.x = DEFAULT_VELOCITY #
+                new_wps_list.append(w_p)
+            # end of for
+        else:
+            for w_p in self.waypoints[start_wps_ptr:]:
+                w_p.twist.twist.linear.x = DEFAULT_VELOCITY #
+                new_wps_list.append(w_p)
+            # end of for                
+            for w_p in self.waypoints[:end_wps_ptr]:
+                w_p.twist.twist.linear.x = DEFAULT_VELOCITY #
+                new_wps_list.append(w_p)
+            # end of for
+        # end of if 
+        rospy.loginfo("waypoint_updater:send_waypoints final_waypoints list length is %d",
+            len(new_wps_list))
+        lane = Lane()
+        lane.waypoints = list(new_wps_list)
+        lane.header.frame_id = '/world'
+        lane.header.stamp = rospy.Time()
+        self.pubs['final_waypoints'].publish(lane)
+        #pass
+
+
     def get_waypoint_velocity(self, waypoint):
         return waypoint.twist.twist.linear.x
 
-    def set_waypoint_velocity(self, waypoints, waypoint, velocity):
-        waypoints[waypoint].twist.twist.linear.x = velocity
+    def set_waypoint_velocity(self, waypointlist, waypoint, velocity):
+        waypointlist[waypoint].twist.twist.linear.x = velocity
 
-    def distance(self, waypoints, wp1, wp2):
+    def closest_waypoint(self):
+        dist = 1000000  #maybe should use max
+        closest = 0
+        distance_lambda = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2)
+        for i in range(len(self.waypoints)):
+            tmpdist = distance_lambda(self.waypoints[i].pose.pose.position, self.pose.position)
+            if tmpdist < dist:
+                closest = i
+                dist = tmpdist
+            # end of if
+        # end of for
+        return closest
+
+    #def distance(self, waypoints, wp1, wp2):
+    def distance(self, wp1, wp2):
         dist = 0
-        dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
+        distance_lambda = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
         for i in range(wp1, wp2+1):
-            dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
+            dist += distance_lambda(self.waypoints[wp1].pose.pose.position,
+                     self.waypoints[i].pose.pose.position)
             wp1 = i
         return dist
 
