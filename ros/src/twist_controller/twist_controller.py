5f60d9e23148e1f40f534a40438a5383ff324b8d
Maximilian Wenger <mwenger@uos.de>
2018-03-11 18:04:10 +0100

    First version of twist controller
    which just controls the yaw using YawController. #3

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,14 +1,18 @@
+from pid import PID
+from yaw_controller import YawController
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle):
         # TODO: Implement
-        pass
+        self.steer = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
 
-    def control(self, *args, **kwargs):
+    def control(self, dt, cmd_x, cmd_theta, actual_x):
+        steer = self.steer.get_steering(cmd_x, cmd_theta, actual_x)
+        return 0.3, 0.0, steer
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
-        return 1., 0., 0.
+        # return 1., 0., 0.
