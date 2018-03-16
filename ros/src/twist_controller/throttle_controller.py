9db9e8b50b50401aa6c55e3b9f1c6a5e53eae413
xpharry <robotpengxu@gmail.com>
2018-03-16 18:50:16 -0400

    pid para

--- ros/src/twist_controller/throttle_controller.py
+++ ros/src/twist_controller/throttle_controller.py
@@ -15,7 +15,7 @@ class ThrottleController(object):
     """ A Speed Controller class """
 
     def __init__(self, decel_limit, accel_limit):
-        self.throttle_pid = PID(kp=1.0, ki=1.0, kd=1.0, mn=decel_limit, mx=accel_limit)
+        self.throttle_pid = PID(kp=1.5, ki=0.001, kd=0.0, mn=decel_limit, mx=accel_limit)
 
     def control(self, error, dt):
         """
@@ -30,6 +30,7 @@ class ThrottleController(object):
              throttle (float) , brake (float)
         """
 
+        self.throttle_pid.reset()
         throttle = self.throttle_pid.step(error, dt)
         throttle, brake = max(0.0, min(1.0, throttle)), 0.0
 
