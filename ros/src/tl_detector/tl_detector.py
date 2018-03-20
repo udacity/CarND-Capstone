455456dbd67044c7e6df5e9eb4b6c0e00003cd4f
lajos <panka.nospam@gmail.com>
2018-03-20 17:00:51 -0400

    changed bgr8 -> rgb8 for cv bridge

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -177,7 +177,7 @@ class TLDetector(object):
             self.prev_light_loc = None
             return False
 
-        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "rgb8")
 
         #Get classification
         return self.light_classifier.get_classification(cv_image)
