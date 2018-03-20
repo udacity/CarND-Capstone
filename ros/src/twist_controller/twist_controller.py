eb7ae32f63b4d696fe91b47e74e9c3615ddf5983
Jörn Voigt <voigtjo@gmail.com>
2018-03-20 22:17:04 +0100

    slightly improved braking

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -17,21 +17,31 @@ class Controller(object):
         # Return throttle, brake, steer
 
         if not dbw_enabled:
-        	self.set_controllers()
-        	return 0.0, 0.0, 0.0
+            self.set_controllers()
+            return 0.0, 0.0, 0.0
 
         # throttle and brake controllers
-    	linear_velocity_error = linear_velocity - current_velocity
-    	throttle = self.pid_throttle.step(linear_velocity_error, time_elapsed)
-    	brake = self.pid_brake.step(-linear_velocity_error, time_elapsed)
+        linear_velocity_error = linear_velocity - current_velocity
+        #throttle = self.pid_throttle.step(linear_velocity_error, time_elapsed)
+        #brake = self.pid_brake.step(-linear_velocity_error, time_elapsed)
+        if linear_velocity_error > 0.0:
+            throttle = self.pid_throttle.step(linear_velocity_error, time_elapsed)
+            brake = 0.0
+        else:
+            throttle = 0.0
+            if linear_velocity < 0.1:
+                brake = 20.0
+            else:
+                brake = -20.0 * linear_velocity_error
+                brake = max(brake, 1.0)
 
-    	# steering controller
-    	steer = self.yawcontroller.get_steering(linear_velocity, angular_velocity, current_velocity)
-    	steer = self.lowpass_flt.filt(steer)
+        # steering controller
+        steer = self.yawcontroller.get_steering(linear_velocity, angular_velocity, current_velocity)
+        steer = self.lowpass_flt.filt(steer)
 
         return throttle, brake, steer
 
     def set_controllers(self):
-    	self.pid_throttle = PID(0.35, 0.0, 0.0, 0.0, 1.0)
-    	self.pid_brake = PID(0.30, 0.0, 0.0, 0.0, 1.0)
-    	self.lowpass_flt = LowPassFilter(0.2, 1.0)
+        self.pid_throttle = PID(0.35, 0.0, 0.0, 0.0, 1.0)
+        self.pid_brake = PID(0.30, 0.0, 0.0, 0.0, 1.0)
+        self.lowpass_flt = LowPassFilter(0.2, 1.0)
