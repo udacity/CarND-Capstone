c0499283c6e429a8e03bd89d27d249553b473fee
Shenghan Gao <shenghang@nvidia.com>
2018-03-20 22:57:24 -0700

    finished partially implement waypoinst_updater (hopefully). Start working on dbw_node

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -31,6 +31,7 @@ that we have created in the `__init__` function.
 
 '''
 
+
 class DBWNode(object):
     def __init__(self):
         rospy.init_node('dbw_node')
@@ -57,11 +58,15 @@ class DBWNode(object):
         # self.controller = Controller(<Arguments you wish to provide>)
 
         # TODO: Subscribe to all the topics you need to
-
+        rospy.Subscriber(
+            '/current_velocity', TwistStamped, self.current_velocity_cb)
+        rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cmd_cb)
+        rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_enabled_cb)
+        
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(50) # 50Hz
+        rate = rospy.Rate(50)  # 50Hz
         while not rospy.is_shutdown():
             # TODO: Get predicted throttle, brake, and steering using `twist_controller`
             # You should only publish the control commands if dbw is enabled
@@ -73,6 +78,15 @@ class DBWNode(object):
             # if <dbw is enabled>:
             #   self.publish(throttle, brake, steer)
             rate.sleep()
+    
+    def current_velocity_cb(self, curr_velocity):
+        pass
+
+    def twist_cmd_cb(self, twist_cmd):
+        pass
+
+    def dbw_enabled_cb(self, msg):
+        pass
 
     def publish(self, throttle, brake, steer):
         tcmd = ThrottleCmd()
