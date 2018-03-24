b7e0d55dc7995be52ae89077f78eb71570410ccb
Paul Michalik <paul.michalik@outlook.com>
2018-03-24 13:30:49 +0100

    Remove popd.

--- scripts/docker-run.bat
+++ scripts/docker-run.bat
@@ -5,6 +5,5 @@ call "%~dp0abspath.bat" "%~dp0.."
 call "%~dp0docker-id.bat"
 
 docker run --rm -it -p 4567:4567 --name="%DockerContainerName%" -v %AbsPath%:/capstone -v %AbsPath%/.ros:/root/.ros/ -v %AbsPath%/.ros/log:/root/.ros/log/ %DockerUserName%/%DockerImageName%:%DockerImageTag%
-popd
 
 endlocal
