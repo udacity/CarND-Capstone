f5d013ef88596dcbdf1363200bfaab8c3b66089a
gb234243 <>
2018-03-24 18:28:10 +0100

    Update comment on node

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -12,18 +12,6 @@ import sys
 This node will publish waypoints from the car's current position to some `x`
 distance ahead.
 
-As mentioned in the doc, you should ideally first implement a version which
-does not care about traffic lights or obstacles.
-
-Once you have created dbw_node, you will update this node to use the status
-of traffic lights too.
-
-Please note that our simulator also provides the exact location of traffic
-lights and their current status in `/vehicle/traffic_lights` message.
-You can use this message to build this node as well as to verify your TL
-classifier.
-
-TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 # Constants
