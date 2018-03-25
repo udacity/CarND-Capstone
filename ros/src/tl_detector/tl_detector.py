8155fa76c3b72280e8f1483981c8357207e23118
meenunat <cn.meenu@gmail.com>
2018-03-24 23:03:03 -0700

    Enable Ground truth data logging always  (#55)
    * ground truth pass-updated with reduced logging
    
    * ground truth pass-updated with reduced logging
    
    * enabled ground truth logging always -  even if classifier is enabled
    
    * moved the subscribers to the end of init

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -14,8 +14,7 @@ import math
 import time
 
 STATE_COUNT_THRESHOLD = 3
-GROUND_TRUTH_PASS = True  
-#Variable to use ground truth data until the classifier has been developed
+TLC_ENABLED = False # Global variable - Traffic Light classifier enabled 
 
 class TLDetector(object):
     def __init__(self):
@@ -26,6 +25,31 @@ class TLDetector(object):
         self.waypoints = None
         self.camera_image = None
         self.lights = []
+
+        config_string = rospy.get_param("/traffic_light_config")
+        self.config = yaml.load(config_string)
+
+        self.bridge = CvBridge()
+        self.light_classifier = TLClassifier()
+        self.listener = tf.TransformListener()
+
+        self.state = TrafficLight.UNKNOWN
+        self.last_state = TrafficLight.UNKNOWN
+        self.last_wp = -1
+        self.state_count = 0
+
+        self.L_update = False
+        rospy.loginfo('Red: %s', TrafficLight.RED)
+        rospy.loginfo('Yellow: %s', TrafficLight.YELLOW)
+        rospy.loginfo('Green: %s', TrafficLight.GREEN)
+        rospy.loginfo('Unknown: %s', TrafficLight.UNKNOWN)
+
+
+        # List of positions that correspond to the line to stop in front of for a given intersection
+        self.stop_line_positions = self.config['stop_line_positions']
+        self.stop_line_waypoints = []
+
+        self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
         #tl_detection node subscribes to:
         #/base_waypoints provides the complete list of waypoints for the course.
@@ -44,10 +68,10 @@ class TLDetector(object):
         rely on the position of the light and the camera image to predict it.
         '''
         sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb)
-
-
         sub6 = rospy.Subscriber('/image_color', Image, self.image_cb)
 
+<<<<<<< HEAD
+=======
         config_string = rospy.get_param("/traffic_light_config")
         self.config = yaml.load(config_string)
 
@@ -73,6 +97,7 @@ class TLDetector(object):
         self.stop_line_waypoints = []
 
 
+>>>>>>> eef0d3cb2ae1e4f6e2aa9b4fb142e4b13f8f5133
         rospy.spin()
 
     def pose_cb(self, msg):
@@ -101,12 +126,25 @@ class TLDetector(object):
         """
         self.has_image = True
         self.camera_image = msg
+<<<<<<< HEAD
+=======
 
         if GROUND_TRUTH_PASS:
             light_wp, state = self.process_ground_truth_lights()
         else:        
             light_wp, state = self.process_traffic_lights()
+>>>>>>> eef0d3cb2ae1e4f6e2aa9b4fb142e4b13f8f5133
 
+        #light_wp, state = self.process_traffic_lights()     
+
+        """ Added the following to confirm classifier light state - to rule out any latency issue
+        """	
+        light_wp, gt_state, tl_state = self.process_traffic_lights()
+        
+        if TLC_ENABLED:
+            state = tl_state
+        else:		
+            state = gt_state
         '''
         Publish upcoming red lights at camera frequency.
         Each predicted state has to occur `STATE_COUNT_THRESHOLD` number
@@ -116,16 +154,31 @@ class TLDetector(object):
         if self.state != state:
             self.state_count = 0
             self.state = state
+<<<<<<< HEAD
+            self.L_update = True 
+=======
             self.change = True 
+>>>>>>> eef0d3cb2ae1e4f6e2aa9b4fb142e4b13f8f5133
             
         elif self.state_count >= STATE_COUNT_THRESHOLD:
             self.last_state = self.state
             light_wp = light_wp if state == TrafficLight.RED else -1
             self.last_wp = light_wp
             self.upcoming_red_light_pub.publish(Int32(light_wp))
+<<<<<<< HEAD
+            if self.L_update:
+                self.L_update = False
+                if TLC_ENABLED:
+                    rospy.loginfo('Upcoming GT Light state: %s',gt_state)     
+                    rospy.loginfo('Upcoming Classifier Light state: %s',state)
+                else:
+                    rospy.loginfo('Upcoming GT Light state: %s',state)     
+                    rospy.loginfo('Upcoming Classifier Light state: %s',tl_state)			
+=======
             if self.change:
 		self.change = False
                 rospy.loginfo('Upcoming Light state: %s',state)
+>>>>>>> eef0d3cb2ae1e4f6e2aa9b4fb142e4b13f8f5133
                 rospy.loginfo('Upcoming Stop line: %f',light_wp)
         else:
             self.upcoming_red_light_pub.publish(Int32(self.last_wp))
@@ -166,7 +219,7 @@ class TLDetector(object):
 
 
 
-    def get_light_state(self, light):
+    def get_light_state(self, light):#Argument light may not be required
         """Determines the current color of the traffic light
 
         Args:
@@ -186,35 +239,13 @@ class TLDetector(object):
         return self.light_classifier.get_classification(cv_image)
 
     def process_traffic_lights(self):
-        """Finds closest visible traffic light, if one exists, and determines its
-            location and color
+        """Finds closest visible traffic light/ground truth data, if one exists, and determines its
+            stop line position and state of the traffic light
 
         Returns:
             int: index of waypoint closes to the upcoming stop line for a traffic light (-1 if none exists)
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
-        """
-        light = None
-
-        # List of positions that correspond to the line to stop in front of for a given intersection
-        #stop_line_positions = self.config['stop_line_positions']
-        if(self.pose):
-            car_position = self.get_closest_waypoint(self.pose.pose.position.x,self.pose.pose.position.y, self.waypoints) 
-
-        #TODO find the closest visible traffic light (if one exists)
-
-        if light:
-            state = self.get_light_state(light)
-            return light_wp, state
-        self.waypoints = None
-        return -1, TrafficLight.UNKNOWN
-
-    def process_ground_truth_lights(self):
-        """Finds closest traffic light in the ground truth data and returns the state of the light
-
-        Returns:
-            int: index of waypoint closes to the upcoming stop line for a traffic light (-1 if none exists)
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+            int: ID of traffic light color identified by TL classifier - if enabled, else ID of UNKNOWN 
 
         """
         light = None
@@ -223,7 +254,8 @@ class TLDetector(object):
         #stop_line_positions = self.config['stop_line_positions']
 	
         ntl_wp = -1
-        ntl_state =TrafficLight.UNKNOWN
+        gt_ntl_state =TrafficLight.UNKNOWN
+	ntl_state =TrafficLight.UNKNOWN
 
         if(self.pose):
             car_position = self.get_closest_waypoint(self.pose.pose.position.x,self.pose.pose.position.y, self.waypoints)
@@ -232,7 +264,7 @@ class TLDetector(object):
                 nearest_waypoint = self.get_closest_waypoint(tl.pose.pose.position.x,tl.pose.pose.position.y, self.waypoints)
                 if nearest_waypoint > car_position:
                     ntl_wp = nearest_waypoint
-                    ntl_state = tl.state
+                    gt_ntl_state = tl.state
                     break
         stop_line = -1 
         if ntl_wp > 0:
@@ -243,13 +275,23 @@ class TLDetector(object):
                     if ((ntl_wp - position) < stop_distance):
                         stop_distance = (ntl_wp - position)	
                         stop_line = position
+
         if stop_line > 0:
+<<<<<<< HEAD
+            if TLC_ENABLED:
+               ntl_state = self.get_light_state(light)
+               #Argument light may not be required
+            return stop_line,gt_ntl_state, ntl_state 
+            #return stop_line, ntl_state 
+=======
             #rospy.loginfo('Light state: %s',ntl_state)
             #rospy.loginfo('Stop line: %f',stop_line)			
             return stop_line, ntl_state 
+>>>>>>> eef0d3cb2ae1e4f6e2aa9b4fb142e4b13f8f5133
         else:
             rospy.loginfo('Light state: %s',TrafficLight.UNKNOWN)
-            return -1, TrafficLight.UNKNOWN
+            return -1, TrafficLight.UNKNOWN, TrafficLight.UNKNOWN
+            #return -1, TrafficLight.UNKNOWN
 
 if __name__ == '__main__':
     try:
