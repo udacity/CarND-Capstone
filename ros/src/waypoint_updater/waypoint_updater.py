296c91ccd1f8b3734ea9f5e6525f5241253fc419
Lorenzo <lucido.lorenzo@gmail.com>
2018-03-26 21:45:34 +0800

    [#20] [#23] Fix for full cycle + Circular toggle

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -25,11 +25,12 @@ current status in `/vehicle/traffic_lights` message. You can use this message to
 as well as to verify your TL classifier.
 '''
 
-LOOKAHEAD_WPS   = 200    # Number of waypoints we will publish.
+LOOKAHEAD_WPS   = 200   # Number of waypoints we will publish. For Test Lot, please put a value smaller than 60
+CIRCULAR_WPS    = True  # If True, assumes that the path to follow is a loop
 REFRESH_RATE_HZ = 2     # Number of times we update the final waypoints per second
 UPDATE_MAX_ITER = 50    # Max number of iterations before considering relooking for the next waypoint in full path
 WAYPOINT_INCREMENT_RATE = 5 # The number of the waypoints that is added when searching for the next one ahead
-DEBUG_MODE      = False  # Switch for whether debug messages are printed.
+DEBUG_MODE      = True  # Switch for whether debug messages are printed.
 TL_DETECTOR_ON  = False # If False, switches to direct traffic light subscription
 DECELLERATION   = 3     # Decelleration in m/s^2
 
@@ -66,7 +67,18 @@ def step_by_step_distance(waypoints, wp1, wp2):
 
 class WaypointUpdater(object):
     def __init__(self):
-        rospy.init_node('waypoint_updater', log_level=rospy.DEBUG if DEBUG_MODE else rospy.ERROR)
+        rospy.init_node('waypoint_updater', log_level=rospy.DEBUG if DEBUG_MODE else rospy.WARN)
+
+        if DEBUG_MODE:
+            rospy.logwarn('waypoint_updater: DEBUG mode enabled - Disable for submission')
+
+        if TL_DETECTOR_ON:
+            rospy.logwarn('waypoint_updater: Traffic Light Detector disabled - Enable for submission')
+
+        if WAYPOINT_INCREMENT_RATE != 1:
+            rospy.logwarn('waypoint_updater: Waypoint increment rate should be set to 1 for submission')
+
+        self.next_waypoint_indices = self.next_waypoint_indices_circular if CIRCULAR_WPS else self.next_waypoint_indices_simple
 
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
@@ -82,6 +94,7 @@ class WaypointUpdater(object):
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
+
         self.next_waypoint = 0
         self.next_red_tl_wp = 0
         self.previous_pos = None
@@ -96,12 +109,10 @@ class WaypointUpdater(object):
             self.publish_update()
 
     def waypoints_cb(self, waypoints):
-        self.static_waypoints = waypoints  # Lane message is a structure with header and waypoints
+        self.static_waypoints = waypoints.waypoints  # Lane message is a structure with header and waypoints
         self.static_velocities = [self.get_static_waypoint_id_velocity(wp)
-                                  for wp in range(len(self.static_waypoints.waypoints))]
-        rospy.logdebug('Static velocities')
-        rospy.logdebug(self.static_velocities)
-        rospy.loginfo('Received {} base waypoints'.format(len(self.static_waypoints.waypoints)))
+                                  for wp in range(len(self.static_waypoints))]
+        rospy.loginfo('Received {} base waypoints'.format(len(self.static_waypoints)))
 
     def traffic_light_array_cb(self, msg):
         if self.static_waypoints and time.time() - self.last_update > 1. / REFRESH_RATE_HZ:
@@ -113,7 +124,7 @@ class WaypointUpdater(object):
             if len(sorted_tl_array):
                 closest_tl = sorted_tl_array[0]
                 tl_x, tl_y = closest_tl.pose.pose.position.x, closest_tl.pose.pose.position.y
-                next_wps = self.next_waypoint_indices_circular()
+                next_wps = self.next_waypoint_indices_simple()
 
                 # Get corresponding stop line closest to traffic lght
                 corresponding_stop_line = sorted(self.stop_line_positions,
@@ -122,8 +133,8 @@ class WaypointUpdater(object):
                 # Get corresponding waypoint closest to stop line
                 corresponding_wp = sorted(next_wps,
                                           key=lambda x: euclidean(corresponding_stop_line,
-                                                                  (self.static_waypoints.waypoints[x].pose.pose.position.x,
-                                                                  self.static_waypoints.waypoints[x].pose.pose.position.y)))[0]
+                                                                  (self.static_waypoints[x].pose.pose.position.x,
+                                                                  self.static_waypoints[x].pose.pose.position.y)))[0]
                 self.traffic_cb(Int32(corresponding_wp))
             else:
                 self.traffic_cb(Int32(-1))
@@ -136,10 +147,10 @@ class WaypointUpdater(object):
             self.restore_all_velocities()
             # 2: gradually reduce speed in order to get to zero to next red light
             if self.next_red_tl_wp > 0:
-                # light_wps = range(self.next_red_tl_wp - 50, self.next_red_tl_wp)
-                for wp in self.next_waypoint_indices_circular():
-                    tl_wp = self.static_waypoints.waypoints[self.next_red_tl_wp]
-                    check_wp = self.static_waypoints.waypoints[wp]
+                wp_indices = self.next_waypoint_indices()
+                for wp in wp_indices:
+                    tl_wp = self.static_waypoints[self.next_red_tl_wp]
+                    check_wp = self.static_waypoints[wp]
                     if self.distance_to_previous(check_wp.pose.pose) > self.distance_to_previous(tl_wp.pose.pose) - 1:
                         self.set_waypoint_id_velocity(wp, 0.0)
                     else:
@@ -156,19 +167,19 @@ class WaypointUpdater(object):
         pass
 
     def get_static_waypoint_id_velocity(self, waypoint_id):
-        return self.static_waypoints.waypoints[waypoint_id].twist.twist.linear.x
+        return self.static_waypoints[waypoint_id].twist.twist.linear.x
 
     def set_waypoint_velocity(self, waypoint, velocity):
         waypoint.twist.twist.linear.x = velocity
 
     def set_waypoint_id_velocity(self, waypoint_id, velocity):
-        self.static_waypoints.waypoints[waypoint_id].twist.twist.linear.x = velocity
+        self.static_waypoints[waypoint_id].twist.twist.linear.x = velocity
 
     def restore_waypoint_id_velocity(self, waypoint_id):
         self.set_waypoint_id_velocity(waypoint_id, self.static_velocities[waypoint_id])
 
     def restore_all_velocities(self):
-        for wp_id in range(len(self.static_waypoints.waypoints)):
+        for wp_id in range(len(self.static_waypoints)):
             self.restore_waypoint_id_velocity(wp_id)
 
     def distance_to_previous(self, position):
@@ -184,13 +195,12 @@ class WaypointUpdater(object):
 
     def update_next_waypoint(self):
         it = 0
-        nb_waypoints = len(self.static_waypoints.waypoints)
-        while not self.waypoint_is_ahead(self.static_waypoints.waypoints[self.next_waypoint % nb_waypoints]) and \
+        nb_waypoints = len(self.static_waypoints)
+        get_wp = lambda wp: wp % nb_waypoints if CIRCULAR_WPS else min(wp, nb_waypoints-1)
+        while not self.waypoint_is_ahead(self.static_waypoints[get_wp(self.next_waypoint)]) and \
                 it < UPDATE_MAX_ITER:
             it += 1
-            self.next_waypoint += WAYPOINT_INCREMENT_RATE  # We look at the next one
-            if self.next_waypoint > len(self.static_waypoints.waypoints) - 1:
-                self.next_waypoint = 0
+            self.next_waypoint = get_wp(self.next_waypoint+WAYPOINT_INCREMENT_RATE)  # We look at the next one
 
         # Searching the next waypoint in the full path takes much longer, we want to avoid it as much as possible
         if it == UPDATE_MAX_ITER:
@@ -200,27 +210,32 @@ class WaypointUpdater(object):
         self.next_waypoint = 0
         rospy.logwarn("Initiating search for closest waypoint...")
         # We basically search among all static waypoints the closest waypoint ahead
-        for i in range(len(self.static_waypoints.waypoints)):
-            if self.waypoint_is_ahead(self.static_waypoints.waypoints[i]) and \
-                    self.distance_to_previous(self.static_waypoints.waypoints[i].pose.pose) < \
-                    self.distance_to_previous(self.static_waypoints.waypoints[self.next_waypoint].pose.pose):
+        for i in range(len(self.static_waypoints)):
+            if self.waypoint_is_ahead(self.static_waypoints[i]) and \
+                    self.distance_to_previous(self.static_waypoints[i].pose.pose) < \
+                    self.distance_to_previous(self.static_waypoints[self.next_waypoint].pose.pose):
                 self.next_waypoint = i
         rospy.logwarn('Found next closest waypoint: {}'.format(self.next_waypoint))
 
+    # --- Next waypoint indices functions ---
     def next_waypoint_indices_circular(self):
-        cyc = cycle(range(len(self.static_waypoints.waypoints)))
+        cyc = cycle(range(len(self.static_waypoints)))
         return list(islice(cyc, self.next_waypoint, self.next_waypoint + LOOKAHEAD_WPS))
 
-    def next_waypoints_circular(self):
-        indices = self.next_waypoint_indices_circular()
-        return [self.static_waypoints.waypoints[i] for i in indices]
+    def next_waypoint_indices_simple(self):
+        return list(islice(range(len(self.static_waypoints)), self.next_waypoint, self.next_waypoint+LOOKAHEAD_WPS))
+    # ----------------------------------------
+
+    def next_waypoints(self):
+        indices = self.next_waypoint_indices()
+        return [self.static_waypoints[i] for i in indices]
 
     def publish_update(self):
         # Emits the new waypoints, but only if we have received the base waypoints
         if self.static_waypoints:
             self.update_next_waypoint()
             rospy.logdebug("Next waypoint: {}".format(self.next_waypoint))
-            self.final_waypoints_pub.publish(Lane(waypoints=self.next_waypoints_circular()))
+            self.final_waypoints_pub.publish(Lane(waypoints=self.next_waypoints()))
 
         self.last_update = time.time()
 
