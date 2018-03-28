8132f62d7050ecf1fb5a0d9e4113c03acde5a682
scs50 <scottcschnelle@gmail.com>
2018-03-28 16:23:52 -0400

    More changes

--- ros/src/twist_controller/yaw_controller.py
+++ ros/src/twist_controller/yaw_controller.py
@@ -16,12 +16,10 @@ class YawController(object):
         return max(self.min_angle, min(self.max_angle, angle))
 
     def get_steering(self, linear_velocity, angular_velocity, current_velocity):
-        # angular_velocity = current_velocity * angular_velocity / linear_velocity if abs(linear_velocity) > 0. else 0.
+        angular_velocity = current_velocity * angular_velocity / linear_velocity if abs(linear_velocity) > 0. else 0.
 
         if abs(current_velocity) > 0.1:
             max_yaw_rate = abs(self.max_lat_accel / current_velocity);
             angular_velocity = max(-max_yaw_rate, min(max_yaw_rate, angular_velocity))
-        if current_velocity < self.min_speed:
-            return 0.0
-        else:
-            return self.get_angle(max(current_velocity*0.8, self.min_speed) / angular_velocity) if abs(angular_velocity) > 0. else 0.0;
+
+        return self.get_angle(max(current_velocity, self.min_speed) / angular_velocity) if abs(angular_velocity) > 0. else 0.0;
