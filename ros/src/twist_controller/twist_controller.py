32ceed475273675aaaca78eaccf84219792ff59f
Shenghan Gao <shenghang@nvidia.com>
2018-03-28 22:08:29 -0700

    the car can follow the line and control speed
    but turned a little bit too much

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -11,15 +11,20 @@ class Controller(object):
         min_speed, max_lat_accel, max_steer_angle
     ):
         # TODO: Implement
-        self.throttle_controller = PID(1, 1, 1)
+        self.throttle_controller = PID(0.03, 1000, 10, -5, 1)
         self.yaw_controller = YawController(
             wheel_base, steer_ratio, min_speed, max_lat_accel,
             max_steer_angle)
-    
+
     def control(self, linear_velocity, angular_velocity, current_velocity):
         steer = self.yaw_controller.get_steering(
             linear_velocity,
             angular_velocity,
             current_velocity)
-        # Return throttle, brake, steer
-        return 0.5, 0., steer
+        activation = self.throttle_controller.step(
+            linear_velocity - current_velocity, 0.02)
+        if activation > 0:
+            throttle, brake = activation, 0
+        else:
+            throttle, brake = 0, activation
+        return throttle, brake, steer
