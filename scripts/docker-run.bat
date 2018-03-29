8164fa9d0e452891d9eba7ee30fb4d51c2a152cf
Paul Michalik <paul.michalik@outlook.com>
2018-03-29 16:37:40 +0200

    Do not mount volumes recursively

--- scripts/docker-run.bat
+++ scripts/docker-run.bat
@@ -4,6 +4,6 @@ setlocal
 call "%~dp0abspath.bat" "%~dp0.."
 call "%~dp0docker-id.bat"
 
-docker run --rm -it -p 4567:4567 --name="%DockerContainerName%" -v %AbsPath%:/capstone -v %AbsPath%/.ros:/root/.ros/ -v %AbsPath%/.ros/log:/root/.ros/log/ %DockerUserName%/%DockerImageName%:%DockerImageTag%
+docker run --rm -it -p 4567:4567 --name="%DockerContainerName%" -v %AbsPath%:/capstone -v %AbsPath%/.ros:/root/.ros/ %DockerUserName%/%DockerImageName%:%DockerImageTag%
 
 endlocal
