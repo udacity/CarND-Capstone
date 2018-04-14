fca8f2b84069bd5e7d2a165ccf9711ee52bb98f5
Guillaume Granie <granie.guillaume@gmail.com>
2018-04-14 19:11:01 +0100

    Attempt to improve waypoint_updater

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -7,6 +7,8 @@ import tf
 from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Int32
 from styx_msgs.msg import Lane
+from scipy.spatial import KDTree
+import numpy as np
 
 '''
 This node will publish waypoints from the car's current position
@@ -26,7 +28,7 @@ verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 100  # Number of waypoints we publish
+LOOKAHEAD_WPS = 45  # Number of waypoints we publish
 
 class WaypointUpdater(object):
     def __init__(self):
@@ -43,31 +45,50 @@ class WaypointUpdater(object):
         # ROS publishers
         self.pub = rospy.Publisher('/final_waypoints', Lane, queue_size=1)
 
-        self.waypoints = None
-        self.n_waypoints = 0
+        self.base_waypoints = None
+        self.waypoints_2d = None
+        self.waypoints_tree = None
         self.ego = None
         self.next_idx = -1
 
+        self.publishing_loop()
+
+    def publishing_loop(self):
+        '''
+        Main loop finding the next LOOKAHEAD_WPS waypoints and publishing
+        them in the final_waypoints topic.
+        :return: None
+        '''
         rate = rospy.Rate(30)
         while not rospy.is_shutdown():
-            self.publish()
+            if self.ego and self.base_waypoints:
+                closest_waypoint_idx = self.find_next_waypoint()
+                rospy.loginfo("Current position ({}, {}), next waypoint position: ({}, {})"
+                              .format(self.ego.pose.position.x,
+                                      self.ego.pose.position.y,
+                                      self.waypoints_2d[closest_waypoint_idx][0],
+                                      self.waypoints_2d[closest_waypoint_idx][1],))
+                self.publish(closest_waypoint_idx)
             rate.sleep()
 
     def pose_cb(self, pose):
         #Get current pose
-        if self.ego is None or self.ego.header.seq < pose.header.seq:
-            self.ego = pose
+        self.ego = pose
 
-    def waypoints_cb(self, lane):
-        #Get list of waypoints from lane
-        if self.waypoints is None and lane.waypoints is not None:
-            self.waypoints = lane.waypoints
-            self.n_waypoints = len(lane.waypoints)
+    def waypoints_cb(self, waypoints):
+        '''
+        Get list of waypoints along our lane and build a KD Tree
+        :param waypoints:
+        '''
+        self.base_waypoints = waypoints
+        if not self.waypoints_2d:
+            self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
+            self.waypoints_tree = KDTree(self.waypoints_2d)
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
         pass
-    
+
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message.
         # We will implement it later
@@ -98,30 +119,16 @@ class WaypointUpdater(object):
         return math.sqrt((pt1.x - pt2.x) ** 2 + (pt1.y - pt2.y) ** 2 +
                          (pt1.z - pt2.z) ** 2)
 
-    def publish(self):
-        #Publish list of next waypoints to lane
-        self.next_idx = self.find_next_waypoint()
-        if -1 < self.next_idx and not rospy.is_shutdown():
-
-            rospy.loginfo("Current position ({}, {}), next waypoint: {}"
-                          .format(self.ego.pose.position.x,
-                                  self.ego.pose.position.y,
-                                  self.next_idx))
-
-            waypoints = list(self.get_lookahead(self.next_idx))
-
-            lane = Lane()
-            lane.header.frame_id = '/world'
-            lane.header.stamp = rospy.Time.now()
-            lane.waypoints = waypoints
-            self.pub.publish(lane)
-
-    def get_lookahead(self, start_idx):
-        #Get LOOKAHEAD_WPS number of waypoints from waypoints list
-        for i in range(LOOKAHEAD_WPS):
-            idx = (start_idx + i + self.n_waypoints) % self.n_waypoints
-            yield copy.deepcopy(self.waypoints[idx])
-
+    def publish(self, closest_waypoint):
+        '''
+        Publish list of next set of waypoints the car will be to reach
+        :return: None
+        '''
+        lane = Lane()
+        lane.header.frame_id = '/world'
+        lane.header.stamp = rospy.Time.now()
+        lane.waypoints = self.base_waypoints.waypoints[closest_waypoint:closest_waypoint + LOOKAHEAD_WPS]
+        self.pub.publish(lane)
 
     def vector_from_quaternion(self, q):
         #Used to convert to vector coord from quaternion for ego vehicle coord
@@ -133,33 +140,32 @@ class WaypointUpdater(object):
         return x, y, z
 
     def find_next_waypoint(self):
-        #Finds the next closest waypoint in front of the vehicle
-        min_dist = 1e10
-        min_idx = -1
-        if self.ego and self.waypoints:
-            ego_pose = self.ego.pose
-            # Find the closest waypoint
-            for i in range(self.next_idx, self.next_idx + self.n_waypoints):
-                idx = (i + self.n_waypoints) % self.n_waypoints
-                wp_pos = self.waypoints[idx].pose.pose.position
-                dl = self.euclidean_dist(ego_pose.position, wp_pos)
-                if dl < min_dist:
-                    min_dist = dl
-                    min_idx = idx
-                if min_dist < 10 and dl > min_dist:
-                    break
+        '''
+        Finds the next closest waypoint in front of the vehicle
+        :return: Index of the closest waypoint in the waypoint_2d array
+        '''
+        x = self.ego.pose.position.x
+        y = self.ego.pose.position.y
+        # This returned index is matched onto the indexes of self.waypoints_2d
+        closest_waypoint_index = self.waypoints_tree.query([x,y], 1)[1]
 
-            # Only get waypoints in front of us, so check relitive pos to closest waypoint
-            wp_pos = self.waypoints[min_idx].pose.pose.position
-            pos = copy.deepcopy(ego_pose.position)
-            x, y, z = self.vector_from_quaternion(ego_pose.orientation)
-            pos.x += x * .1
-            pos.y += y * .1
-            pos.z += z * .1
-            if self.euclidean_dist(wp_pos, pos) > min_dist:
-                min_idx = (min_idx + 1) % self.n_waypoints
-        return min_idx
+        # Since we want to only return waypoints ahead of the vehicle
+        # We test whether the waypoint is ahead or behind of the current
+        # position of the car.
+        closest_coord = self.waypoints_2d[closest_waypoint_index]
+        previous_closest_coord = self.waypoints_2d[closest_waypoint_index-1]
 
+        closest_coord_vec = np.array(closest_coord)
+        prev_vec = np.array(previous_closest_coord)
+        position_vec = np.array([x, y])
+
+        val = np.dot(closest_coord_vec-prev_vec, position_vec-closest_coord_vec)
+
+        # If the dot product is positive, the car is ahead of the closest waypoint found.
+        # Then we take the next one in the list.
+        if val > 0:
+            closest_waypoint_index = (closest_waypoint_index + 1) % len(self.waypoints_2d)
+        return closest_waypoint_index
 
 if __name__ == '__main__':
     try:
