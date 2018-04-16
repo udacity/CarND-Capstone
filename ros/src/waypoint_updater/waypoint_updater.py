24c1fb90b115e0117303c39a46af08bbe58b61da
Luis <luis.paradela@gmail.com>
2018-04-16 00:11:03 -0300

    Update waypoint_updater.py

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -52,7 +52,7 @@ class WaypointUpdater(object):
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(2)
+        rate = rospy.Rate(30)
         while not rospy.is_shutdown():
 
             if self.pose and self.base_waypoints:
