6518cc59a8a4ac99908062a410ed6155948aa5e3
Louis Pienaar <louis.pienaar@momentum.co.za>
2018-04-21 19:54:37 +0200

    light state fixed

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -150,11 +150,13 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        if (not self.has_image):
-            self.prev_light_loc = None
-            return False
 
-        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        return light.state
+        #if (not self.has_image):
+        #    self.prev_light_loc = None
+        #    return False
+
+        #cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
         # Generate sample images
         # import time
@@ -164,7 +166,7 @@ class TLDetector(object):
         # rospy.logerr('Sample image saved: %s', filename)
 
         # Get classification
-        return self.light_classifier.get_classification(cv_image)
+        #return self.light_classifier.get_classification(cv_image)
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
