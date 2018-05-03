a7e70cbe17b6dda8c43d18d32569e4952ce1851c
Emmanuel Onzon <emmanuel.onzon@algolux.com>
2018-05-03 16:47:41 +0200

    tl_detector: fix bgr -> rgb

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -180,7 +180,7 @@ class TLDetector(object):
         elif self.get_light == 'detect':
             if self.has_image:
                 self.prev_light_loc = None
-                cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+                cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "rgb8")
                 self.has_image = False
                 #Get classification
                 return self.light_classifier.get_classification(cv_image)
