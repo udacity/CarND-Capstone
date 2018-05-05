e27fb636ee85c577200478e34f2bc7cdca09ed6a
Kevin La Ra <kglara@yahoo.com>
2018-05-04 19:10:50 -0700

    Update dbw_node.py

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -66,7 +66,7 @@ class DBWNode(object):
                                      max_steer_angle = max_steer_angle)
 
         # TODO: Subscribe to all the topics you need to
-        rospy.Subscriber('/vehicle/dbw_enabeld', Bool, self.dbw_enabled_cb)
+        rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_enabled_cb)
         rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cb)
         rospy.Subscriber('/current_velocity', TwistStamped, self.velocity_cb)
 
