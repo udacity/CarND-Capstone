b0e680a38fe54f50613ac8adc3c7de9f0a66d9f7
Kevin La Ra <kglara@yahoo.com>
2018-05-04 19:12:56 -0700

    Update twist_controller.py

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -52,7 +52,7 @@ class Controller(object):
         sample_time = current_time - self.last_time
         self.last_time = current_time
 
-        throttle - self.throttle_controller.step(vel_error, sample_time)
+        throttle = self.throttle_controller.step(vel_error, sample_time)
         brake = 0
 
         if linear_vel == 0.0 and current_vel < 0.1:
