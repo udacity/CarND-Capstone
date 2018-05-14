eb267bd45efbdfe486134b5c77831209bbd6cdcc
Ridgebeck <beck.niklas@gmail.com>
2018-05-14 01:32:32 -0700

    add pid control for steering and optimize predictive control

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -11,7 +11,7 @@ class Controller(object):
     def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit,
     	accel_limit, wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
         # TODO: Implement
-        self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
+        self.yaw_controller = YawController(wheel_base, steer_ratio, 1.5, max_lat_accel, max_steer_angle)
 
         kp = 0.3
         ki = 0.1
@@ -20,9 +20,15 @@ class Controller(object):
         mx = 0.5  # max throttle value
         self.throttle_controller = PID(kp, ki, kd, mn, mx)
 
-        tau = 0.5  # 1 / (2*pi* tau) = cuttoff freq
+        self.steer_controller = PID(0.2, 0.10, 0.45, -max_steer_angle, max_steer_angle)
+
+        tau = 0.2  # 1 / (2*pi* tau) = cuttoff freq
         ts = 0.02  # sample time
         self.vel_lpf = LowPassFilter(tau, ts)
+
+        tau_cte = 0.2  # 1 / (2*pi* tau) = cuttoff freq
+        ts_cte = 0.02  # sample time, f
+        self.cte_lpf = LowPassFilter(tau_cte, ts_cte)
 
         self.vehicle_mass = vehicle_mass
         self.fuel_capacity = fuel_capacity
@@ -33,17 +39,18 @@ class Controller(object):
 
         self.last_time = rospy.get_time()
 
-    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
+    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel, cte):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
 
         if not dbw_enabled:
         	self.throttle_controller.reset()
+        	self.steer_controller.reset()
         	return 0.0, 0.0, 0.0
 
         current_vel = self.vel_lpf.filt(current_vel)
 
-        steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+        predictive_steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
 
         vel_error = linear_vel - current_vel
         self.last_vel = current_vel
@@ -52,7 +59,13 @@ class Controller(object):
         sample_time = current_time - self.last_time
         self.last_time = current_time
 
+        cte_lpf = self.cte_lpf.filt(cte)
+        pid_steering = self.steer_controller.step(cte_lpf, sample_time)
+
+        steering = 0.4 * predictive_steering + 0.6 * pid_steering
+
         throttle = self.throttle_controller.step(vel_error, sample_time)
+
         brake = 0
 
         if linear_vel == 0.0 and current_vel < 0.1:
