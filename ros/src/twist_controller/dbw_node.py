fae0689d486d4c6e5ff43e54391cf6fb87a35aae
satoshi <satoshi.kumano@kii.com>
2018-05-16 22:11:55 +0900

    Adding log..

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -94,13 +94,13 @@ class DBWNode(object):
                 dbw_enabled=self.dbw_enabled,
                 linear_vel=self.linear_vel,
                 angular_vel=self.angular_vel)
-            rospy.loginfo('t: {} b: {} s: {}'.format(throttle, brake, steering))
+            rospy.loginfo('loop thr: {} brk: {} steer: {}'.format(throttle, brake, steering))
             if self.dbw_enabled:
                 self.publish(throttle, brake, steering)
             rate.sleep()
 
     def twist_cb(self, msg):
-        rospy.loginfo('twist_cb')
+        rospy.loginfo('twist_cb linear_vel: {}, angular_vel: {}'.format(msg.twist.linear.x, msg.twist.angular.z))
         self.linear_vel = msg.twist.linear.x
         self.angular_vel = msg.twist.angular.z
 
