23ef1af91c43f895fee3fb5bff11cd249d138401
Rekon <arindambaidya168@gmail.com>
2018-06-01 21:33:23 +0530

    Changed PID parameters

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -13,8 +13,8 @@ class Controller(object):
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
         
         kp = 0.3
-        ki = 0.1
-        kd = 0.005
+        ki = 0.0003
+        kd = 0.03
         mn = 0.0 #Minimum throttle value
         mx = 0.2 #Maximum throttle value
         self.throttle_controller = PID(kp, ki, kd, mn, mx)
