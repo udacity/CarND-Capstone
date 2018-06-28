f11dae804903e36526204242090c083df5168a27
Markus Isaksson <markus.isaksson@gmail.com>
2018-06-28 03:09:02 -0700

    Waypoint search
    Extract code for waypoint search to it's own class to simplify reuse.

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -2,7 +2,7 @@
 
 import rospy
 from geometry_msgs.msg import PoseStamped
-from styx_msgs.msg import Lane, Waypoint
+from styx_msgs.msg import Lane
 from scipy.spatial import KDTree
 import numpy as np
 import math
@@ -30,69 +30,42 @@ class WaypointUpdater(object):
         rospy.init_node('waypoint_updater')
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
-        # TODO: Add other member variables you need below
         self.pose = None
-        self.base_waypoints = None
         self.max_velocity = None
-        self.waypoints_2d = None
-        self.waypoints_tree = None
-
+        self.base_waypoints_msg = None
+        self.wp_search = None
         self.rate = rospy.Rate(50)
         self.waitUntilInit()
         self.loopForEver()
 
     def waitUntilInit(self):
-        """Wait until all subscriptions has provided at least one value."""
+        """Wait until all subscriptions has provided at least one msg."""
         while not rospy.is_shutdown():
-            if None not in (self.pose, self.base_waypoints):
+            if None not in (self.pose, self.base_waypoints_msg):
                 # All base waypoints should have the same velocity (in this project).
-                self.max_velocity = self.get_waypoint_velocity(self.base_waypoints.waypoints[0])
+                self.max_velocity = self.get_waypoint_velocity(self.base_waypoints_msg.waypoints[0])
                 # Just make sure the assumption above is correct.
                 assert(all(self.max_velocity == self.get_waypoint_velocity(waypoint)
-                           for waypoint in self.base_waypoints.waypoints))
-
-                # Preprocess to make it faster to search for closest waypoint using the k-d tree algorithm.
-                self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y]
-                                     for waypoint in self.base_waypoints.waypoints]
-                self.waypoint_tree = KDTree(self.waypoints_2d)
+                           for waypoint in self.base_waypoints_msg.waypoints))
+                self.wp_search = WaypointSearch(self.base_waypoints_msg.waypoints)
                 break
             self.rate.sleep()
 
     def loopForEver(self):
         while not rospy.is_shutdown():
             # Get closest waypoint
-            closest_waypoint_idx = self.get_closest_waypoint_ahead_idx()
+            closest_waypoint_idx = self.wp_search.get_closest_waypoint_ahead_idx(self.pose)
             self.publish_waypoints(closest_waypoint_idx)
             self.rate.sleep()
 
-    def get_closest_waypoint_ahead_idx(self):
-        x = self.pose.pose.position.x
-        y = self.pose.pose.position.y
-        closest_idx = self.waypoint_tree.query([x, y], 1)[1]
-
-        # Check if closest is ahead or behind vehicle
-        closest_coord = self.waypoints_2d[closest_idx]
-        prev_coord = self.waypoints_2d[closest_idx - 1]
-
-        # Equation for hyperplane through closest_coord
-        cl_vect = np.array(closest_coord)
-        prev_vect = np.array(prev_coord)
-        pos_vect = np.array([x, y])
-
-        val = np.dot(cl_vect - prev_vect, pos_vect - cl_vect)
-        if val > 0:
-            closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
-        return closest_idx
-
     def publish_waypoints(self, closest_idx):
         final_waypoints = Lane()
-        final_waypoints.header = self.base_waypoints.header
-        final_waypoints.waypoints = self.base_waypoints.waypoints[closest_idx:closest_idx + LOOKAHEAD_WPS]
+        final_waypoints.header = self.base_waypoints_msg.header
+        final_waypoints.waypoints = self.base_waypoints_msg.waypoints[closest_idx:closest_idx + LOOKAHEAD_WPS]
         self.keep_speed_limit(final_waypoints)
         self.final_waypoints_pub.publish(final_waypoints)
 
@@ -111,7 +84,7 @@ class WaypointUpdater(object):
         pass
 
     def waypoints_cb(self, waypoints):
-        self.base_waypoints = waypoints
+        self.base_waypoints_msg = waypoints
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
@@ -137,6 +110,33 @@ class WaypointUpdater(object):
             dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
             wp1 = i
         return dist
+
+
+class WaypointSearch(object):
+    """Organize waypoints to make it fast to search for the closest to a position"""
+    def __init__(self, waypoints):
+        # Preprocess waypoints using the k-d tree algorithm
+        self.waypoints_2d = [[wp.pose.pose.position.x, wp.pose.pose.position.y] for wp in waypoints]
+        self.waypoints_tree = KDTree(self.waypoints_2d)
+
+    def get_closest_waypoint_ahead_idx(self, pose):
+        x = pose.pose.position.x
+        y = pose.pose.position.y
+        closest_idx = self.waypoints_tree.query([x, y], 1)[1]
+
+        # Check if closest is ahead or behind vehicle
+        closest_coord = self.waypoints_2d[closest_idx]
+        prev_coord = self.waypoints_2d[closest_idx - 1]
+
+        # Equation for hyperplane through closest_coord
+        cl_vect = np.array(closest_coord)
+        prev_vect = np.array(prev_coord)
+        pos_vect = np.array([x, y])
+
+        val = np.dot(cl_vect - prev_vect, pos_vect - cl_vect)
+        if val > 0:
+            closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
+        return closest_idx
 
 
 if __name__ == '__main__':
