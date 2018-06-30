bdb35cd05c124cf9e6dc048e592b03717141452a
Glyn Matthews <glyn.matthews@gmail.com>
2018-06-30 21:34:04 +0200

    Added MAX_DECEL definition.

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -24,6 +24,7 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 100 # Number of waypoints we will publish. You can change this number
+MAX_DECEL = 0.5
 
 
 class WaypointUpdater(object):
