b49857d580e080fe007e6fac575dd7f2a5c56603
Stefan Larsson <stefan@larsson.nu>
2018-07-05 01:56:15 +0200

    Don't try to inline base_waypoints (sorry!), it's used twice.

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -88,12 +88,12 @@ class WaypointUpdater(object):
 
         closest_idx = self.get_closest_waypoint_idx()
         farthest_idx = closest_idx + LOOKAHEAD_WPS
-
+        base_waypoints = self.base_lane.waypoints[closest_idx: closest_idx + LOOKAHEAD_WPS]
         if self.stopline_wp_idx == -1 or (self.stopline_wp_idx >= farthest_idx):
             # No buffer overrun, end is auto-truncated at end in Python
-            lane.waypoints = self.base_lane.waypoints[closest_idx: closest_idx + LOOKAHEAD_WPS]
+            lane.waypoints = base_waypoints
         else:
-            lane.waypoints = self.decelerate_waypoints(self.base_lane, closest_idx)
+            lane.waypoints = self.decelerate_waypoints(base_waypoints, closest_idx)
 
         return lane
 
