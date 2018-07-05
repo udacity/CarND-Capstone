3a2d1f582e4575ba82a1070d2811f7eb05596e39
Markus Isaksson <markus.isaksson@gmail.com>
2018-07-05 14:22:15 -0700

    SpeedCalculator also calculates acceleration
    - Implements a method to get the acceleration at a specific distance.
    - Simplifies the implementation by using linear interpolation between
      the distances already calculated for each time-step, instead of
      calculating a LUT from these values.

--- ros/src/waypoint_updater/speed_calculator.py
+++ ros/src/waypoint_updater/speed_calculator.py
@@ -17,11 +17,10 @@ class SpeedCalculator(object):
                               target_acceleration, current_accleration,
                               acceleration_limit, jerk_limit)
         else:
-            # No acceleration. Current speed will be returned regardless of distance.
-            self.distance_speed_lut = [current_speed]
-
-        # The list of distances represented in the LUT
-        self.distances = [i * self.delta_d for i in range(len(self.distance_speed_lut))]
+            # No acceleration. Current speed/acceleration will be returned regardless of distance.
+            self.distances = [0.0]
+            self.velocities = [current_speed]
+            self.accelerations = [current_accleration]
 
     def __accelerate(self, target_speed, current_speed,
                      target_acceleration, current_accleration,
@@ -29,48 +28,32 @@ class SpeedCalculator(object):
         # Calculate the velocity at each time-step when applying acceleration and jerk according to their max limits.
         # Instead of continuously calculate when the jerk must change sign such that the acceleration can be reduced to
         # zero at target speed, the iteration is started from both ends to find the speed overlap in the middle.
-        v0 = current_speed
-        a0 = current_accleration
-        v1 = target_speed
-        a1 = target_acceleration
-        j0 = jerk_limit
-        j1 = -jerk_limit
-        delta_t = 0.001
+        delta_t = 0.01
+        vs_current = [current_speed]
+        vs_target = [target_speed]
+        as_current = [current_accleration]
+        as_target = [target_acceleration]
 
-        velocity_from_start_time = []
-        velocity_from_end_time = []
+        while vs_current[-1] < vs_target[-1]:
+            vs_current.append(vs_current[-1] + as_current[-1] * delta_t)
+            vs_target.append(vs_target[-1] - as_target[-1] * delta_t)
+            as_current.append(min(acceleration_limit, as_current[-1] + jerk_limit * delta_t))
+            as_target.append(min(acceleration_limit, as_target[-1] + jerk_limit * delta_t))
 
-        while v1 > v0:
-            velocity_from_start_time.append(v0)
-            velocity_from_end_time.append(v1)
-            v0 += a0 * delta_t
-            v1 -= a1 * delta_t
-            a0 = min(acceleration_limit, a0 + j0 * delta_t)
-            a1 = min(acceleration_limit, a1 - j1 * delta_t)
+        # Calculate the middle time-step
+        middle_delta_t = (vs_target[-1] - vs_current[-1]) / as_current[-1]
+        delta_ts = [delta_t] * len(vs_current) + [middle_delta_t] + [delta_t] * len(vs_target)
 
-        # Calculate if the approximation is better with or without the middle point.
-        if (v0 - v1) < (a0 / 2.0):
-            velocity_from_start_time.append((v0 + v1) / 2.0)
-
-        # Concatenate the both lists into one common list from the start.
-        velocity_from_start_time += reversed(velocity_from_end_time)
+        # Concatenate the list starting from current with the reversed one starting from target.
+        self.velocities = vs_current + vs_target[::-1]
+        self.accelerations = as_current + as_target[::-1]
 
         # Calculate the distance at each time-step.
-        distance_from_start_time = []
+        self.distances = []
         d = 0.0
-        for v in velocity_from_start_time:
+        for delta_t, v in zip(delta_ts, self.velocities):
             d += v * delta_t
-            distance_from_start_time.append(d)
-
-        # Calculate a look-up table for the velocity at a given distance.
-        self.distance_speed_lut = []
-        d = 0.0
-        for i, d1 in enumerate(distance_from_start_time):
-            while d <= d1:
-                self.distance_speed_lut.append(velocity_from_start_time[i])
-                d += self.delta_d
-        # Make sure target velocity not is omitted (due to resolution of delta_d)
-        self.distance_speed_lut.append(velocity_from_start_time[-1])
+            self.distances.append(d)
 
     def __decelerate(self, target_speed, current_speed,
                      target_acceleration, current_accleration,
@@ -79,7 +62,12 @@ class SpeedCalculator(object):
         self.__accelerate(target_speed=current_speed, current_speed=target_speed,
                           target_acceleration=current_accleration, current_accleration=target_acceleration,
                           acceleration_limit=acceleration_limit, jerk_limit=jerk_limit)
-        self.distance_speed_lut.reverse()
+        self.velocities.reverse()
+        self.accelerations.reverse()
+        self.distances = [self.distances[-1] - distance for distance in self.distances[::-1]]
 
     def get_speed_at_distance(self, distance):
-        return np.interp(distance, self.distances, self.distance_speed_lut)
+        return np.interp(distance, self.distances, self.velocities)
+
+    def get_acceleration_at_distance(self, distance):
+        return np.interp(distance, self.distances, self.accelerations)
