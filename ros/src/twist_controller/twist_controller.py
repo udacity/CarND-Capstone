5de76535d35de3eca18b9d00d0725683d8745465
mav <maverick.udacity@gmail.com>
2018-09-20 13:34:01 -0700

    getting parameters for tunning pids

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -6,7 +6,24 @@ ONE_MPH = 0.44704
 class Controller(object):
     def __init__(self, *args, **kwargs):
         # TODO: Implement
-        pass
+        
+        rospy.loginfo('TwistController: Initializing...')
+
+        # get parameters for tunning pids
+        self.sampling_rate = kwargs["sampling_rate"]
+        self.decel_limit = kwargs["decel_limit"]
+        self.accel_limit = kwargs["accel_limit"]
+        self.brake_deadband = kwargs["brake_deadband"]
+        self.vehicle_mass = kwargs["vehicle_mass"]
+        self.fuel_capacity = kwargs["fuel_capacity"]
+        self.wheel_radius = kwargs["wheel_radius"]
+
+        # for the steering pid
+        self.wheel_base = kwargs["wheel_base"]
+        self.steer_ratio = kwargs["steer_ratio"]
+        self.max_lat_accel = kwargs["max_lat_accel"]
+        self.max_steer_angle = kwargs["max_steer_angle"]
+        
 
     def control(self, *args, **kwargs):
         # TODO: Change the arg, kwarg list to suit your needs
