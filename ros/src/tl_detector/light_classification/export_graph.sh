b4784ce55cc81da69240cd868445e73e6b423f1b
sreakdgeek <srikanth.vid@gmail.com>
2018-10-05 03:26:11 +0000

    TL Detector using DL

---
+++ ros/src/tl_detector/light_classification/export_graph.sh
@@ -0,0 +1,2 @@
+python ./export_inference_graph.py --pipeline_config_path=/data2/TrafficLight
+/ssd_mobilenet_v2_coco_2018_03_29/pipeline.config --trained_checkpoint_prefix=/data2/TrafficLight/ssd_mobilenet_v2_coco_2018_03_29/train/model.ckpt-10000 --output_directory=/data2/TrafficLight/ssd_mobilenet_v2_coco_2018_03_29/frozen`
---
+++ ros/src/tl_detector/light_classification/training.sh
@@ -0,0 +1,1 @@
+python train.py --logtostderr --traindir=/data2/TrafficLight/ssd_mobilenet_v2_coco_2018_03_29/train --pipeline_config_path=/data2/TrafficLight/ssd_mobilenet_v2_coco_2018_03_29/pipeline.config
