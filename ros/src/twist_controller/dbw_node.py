dfbb6f2b14d4ea9c3edbbc2db67cc043834e2d9b
Katsumi Inoue <katsusea528@gmail.com>
2018-10-15 21:03:55 +0200

    bug fix on twist_cb

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -106,7 +106,7 @@ class DBWNode(object):
 
     def twist_cb(self, msg):
         self.linear_vel = msg.twist.linear.x
-        self.angular_vel = msg.twist.angular.
+        self.angular_vel = msg.twist.angular.z
 
     def dbw_cb(self, msg):
         self.dbw = msg
