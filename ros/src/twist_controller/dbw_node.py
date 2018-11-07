53768ea637e8abc9e4d6b6231c5dcc99dc3349a1
Yuxiao Tan <yuxiaota@gmail.com>
2018-11-07 22:14:26 +0000

    implemented dbw node and twist controller

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -55,9 +55,28 @@ class DBWNode(object):
 
         # TODO: Create `Controller` object
         # self.controller = Controller(<Arguments you wish to provide>)
-
+        self.controller = Controller(vehicle_mass = vehicle_mass, \
+                                     fuel_capacity = fuel_capacity,\
+                                     brake_deadband = brake_deadband,\
+                                     decel_limit = decel_limit,\
+                                     accel_limit = accel_limit,\
+                                     wheel_radius = wheel_radius,\
+                                     wheel_base = wheel_base,\
+                                     steer_ratio = steer_ratio,\
+                                     max_lat_accel = max_lat_accel,\
+                                     max_steer_angle = max_steer_angle)
         # TODO: Subscribe to all the topics you need to
-
+        rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_enabled_cb)
+        rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cb)
+        rospy.Subscriber('/current_velocity', TwistStamped, self.velocity_cb)
+        
+        self.current_vel = None
+        self.curr_ang_vel = None
+        self.dbw_enabled = None
+        self.linear_vel = None
+        self.angular_vel = None
+        self.throttle = self.steering = self.brake = 0
+        
         self.loop()
 
     def loop(self):
@@ -65,15 +84,25 @@ class DBWNode(object):
         while not rospy.is_shutdown():
             # TODO: Get predicted throttle, brake, and steering using `twist_controller`
             # You should only publish the control commands if dbw is enabled
-            # throttle, brake, steering = self.controller.control(<proposed linear velocity>,
-            #                                                     <proposed angular velocity>,
-            #                                                     <current linear velocity>,
-            #                                                     <dbw status>,
-            #                                                     <any other argument you need>)
-            # if <dbw is enabled>:
-            #   self.publish(throttle, brake, steer)
+            if not None in (self.current_vel, self.linear_vel, self.angular_vel):
+                self.throttle, self.brake, self.steering = self.controller.control(self.current_vel,
+                                                                                  self.dbw_enabled,
+                                                                                  self.linear_vel,
+                                                                                  self.angular_vel)
+            if self.dbw_enabled:
+                self.publish(self.throttle, self.brake, self.steering)
             rate.sleep()
-
+            
+    def dbw_enabled_cb(self, msg):
+        self.dbw_enabled = msg
+        
+    def twist_cb(self, msg):
+        self.linear_vel = msg.twist.linear.x
+        self.angular_vel = msg.twist.angular.z
+        
+    def velocity_cb(self, msg):
+        self.current_vel = msg.twist.linear.x
+        
     def publish(self, throttle, brake, steer):
         tcmd = ThrottleCmd()
         tcmd.enable = True
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,14 +1,69 @@
-
+from yaw_controller import YawController
+from pid import PID
+from lowpass import LowPassFilter
+import rospy
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
-        # TODO: Implement
-        pass
+    def __init__(self, vehicle_mass, \
+                 fuel_capacity, \
+                 brake_deadband, \
+                 decel_limit, \
+                 accel_limit, \
+                 wheel_radius, \
+                 wheel_base, \
+                 steer_ratio, \
+                 max_lat_accel, \
+                 max_steer_angle):
+            self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
+            
+            kp = 0.3
+            ki = 0.1
+            kd = 0.
+            mn = 0.
+            mx = 0.2
+            self.throttle_controller = PID(kp, ki, kd, mn, mx)
+            
+            tau = 0.5
+            ts = .02
+            self.vel_lpf = LowPassFilter(tau, ts)
+            
+            self.vehicle_mass = vehicle_mass
+            self.fuel_capacity = fuel_capacity
+            self.brake_deadband = brake_deadband
+            self.decel_limit = decel_limit
+            self.accel_limit = accel_limit
+            self.wheel_radius = wheel_radius
+            
+            self.last_time = rospy.get_time()
 
-    def control(self, *args, **kwargs):
+    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
-        return 1., 0., 0.
+        if not dbw_enabled:
+            self.throttle_controller.reset()
+            return 0., 0., 0.
+        current_vel = self.vel_lpf.filt(current_vel)
+        steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+        
+        vel_error = linear_vel -  current_vel
+        self.last_vel = current_vel
+ 
+        current_time = rospy.get_time()
+        sample_time = current_time - self.last_time
+        self.last_time = current_time
+        throttle = self.throttle_controller.step(vel_error, sample_time)
+        brake = 0
+       
+        if linear_vel == 0. and current_vel < 0.1:
+            throttle = 0
+            brake = 400
+            
+        elif throttle < .1 and vel_error < 0:
+            throttle = 0
+            decel = max(vel_error, self.decel_limit)
+            brake = abs(decel) * self.vehicle_mass * self.wheel_radius
+            
+        return throttle, brake, steering    
