cc84cf6ec1edbfb7c805d41367565bb694c3207a
Ryan Jonesford <ryanjonesford@gmail.com>
2018-11-08 21:35:31 -0500

    update

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -34,7 +34,7 @@ class WaypointUpdater(object):
 
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-        rospy.Subscriber('/traffic_waypoint', Lane, self.traffic_cb)
+        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
         
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
