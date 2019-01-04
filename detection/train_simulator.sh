e3967df847c4bf537fa0c00dfc546a4f3e22b646
Ivo van Dongen <info@ivovandongen.nl>
2019-01-04 16:41:02 +0000

    use train.py

--- detection/train_simulator.sh
+++ detection/train_simulator.sh
@@ -4,11 +4,8 @@ echo "Training for simulator"
 
 PIPELINE_CONFIG_PATH=models/faster_rcnn_resnet101_coco.config
 MODEL_DIR=out
-NUM_TRAIN_STEPS=50000
-SAMPLE_1_OF_N_EVAL_EXAMPLES=1
-python deps/tensorflow-models/research/object_detection/model_main.py \
+
+python object_detection/train.py \
+    --logtostderr \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
-    --model_dir=${MODEL_DIR} \
-    --num_train_steps=${NUM_TRAIN_STEPS} \
-    --sample_1_of_n_eval_examples=${SAMPLE_1_OF_N_EVAL_EXAMPLES} \
-    --alsologtostderr
+    --train_dir=${MODEL_DIR}
