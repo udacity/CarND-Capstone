07c30b0d2c57f687813afbc16f41640a20dd5ba2
Feng Wang <fwa785@gmail.com>
2019-01-30 19:35:02 -0800

    Only find the closest traffic stopline if the waypoint tree is constructed

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -150,7 +150,7 @@ class TLDetector(object):
 
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
-        if(self.pose):
+        if(self.pose and self.waypoint_tree):
             car_wp_idx = self.get_closest_waypoint(self.pose.pose.position.x, self.pose.pose.position.y)
 
             #TODO find the closest visible traffic light (if one exists)
