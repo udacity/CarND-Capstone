77ed43434bf59734b42bb00f2d7b196575116205
Ying Li <liyingcmu@fb.com>
2019-05-04 00:35:38 -0700

    dbw

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -16,7 +16,7 @@ class Controller(object):
         ki=0.1
         mn=0. # min throttle value
         mx=0.2 # max throttle value
-        self.throttle_pid = PID(kp, ki, kd, mn=MIN_NUM, mx=MAX_NUM)
+        self.throttle_pid = PID(kp, ki, kd, mn, mx)
 
         self.throttle_controller=PID(kp,ki,kd,mn,mx)
         tau=0.5 # cut off frequency
