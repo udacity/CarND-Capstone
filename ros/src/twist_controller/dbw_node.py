79c3ad9ae6c55fae53f124ee6b69926bf9d1c092
Marshall Ma 马骁 <marshall.ma@nio.com>
2019-05-08 19:54:13 +0800

    Add controller

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -89,10 +89,6 @@ class DBWNode(object):
                                                                                     self.dbw_enabled,
                                                                                     self.linear_vel,
                                                                                     self.angular_vel)
-            #                                                     <proposed angular velocity>,
-            #                                                     <current linear velocity>,
-            #                                                     <dbw status>,
-            #                                                     <any other argument you need>)
             if self.dbw_enabled:
                 self.publish(self.throttle, self.brake, self.steering)
             rate.sleep()
