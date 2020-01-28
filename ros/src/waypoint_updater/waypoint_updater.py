6de3761ee5f0524e504f79fd9790723169931fc8
Dragos Rugescu <dragos.rugescu@yahoo.com>
2020-01-28 13:09:55 +0000

    Changed Waypoint updater rate and number of lookahead points to ease load and ensure the workspace version runs smoothly.

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -23,7 +23,7 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 100 # Number of waypoints we will publish. You can change this number
 
 
 class WaypointUpdater(object):
@@ -47,7 +47,7 @@ class WaypointUpdater(object):
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(50)
+        rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             if self.pose and self.waypoint_tree:
                 # Get closest waypoint
