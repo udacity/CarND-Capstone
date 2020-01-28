9e34e6a9456d7e4877b21223ccb05203f51c9e3b
Dragos Rugescu <dragos.rugescu@yahoo.com>
2020-01-28 13:10:50 +0000

    PID implemented for speed, P controller placeholder for steering at the moment until yaw control is properly implemented.

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -3,7 +3,7 @@
 import rospy
 from std_msgs.msg import Bool
 from dbw_mkz_msgs.msg import ThrottleCmd, SteeringCmd, BrakeCmd, SteeringReport
-from geometry_msgs.msg import TwistStamped
+from geometry_msgs.msg import TwistStamped, PoseStamped
 import math
 
 from twist_controller import Controller
@@ -64,19 +64,31 @@ class DBWNode(object):
         self.proposed_ang_vel = 0.0
         self.current_lin_vel  = 0.0
         self.counter = 0
+        
+        # Subscribe to current velocity and intended linear/angular velocities
         rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cmd_cb)
         rospy.Subscriber('/current_velocity', TwistStamped, self.current_lin_vel_cb)
         
+        # Subscribe to drive-by-wire
         self.dbw_is_enabled = False
         rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_enabled_cb)
+        
+        # Subscribe to current pose for current angle
+        self.pose = None
+        self.pose_z = 0
+        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
 
         self.loop()
 
-    # DBW essage callback
+    # Pose message callback to get angle for steering controller
+    def pose_cb(self, msg):
+        self.pose = msg
+        self.pose_z = self.pose.pose.orientation.z
+        
+    # DBW message callback
     def dbw_enabled_cb(self, msg):
       # Get message and update dbw state
       self.dbw_is_enabled = msg.data
-      rospy.logwarn("[ LOG ] : Got a DBW state message = %s/n", self.dbw_is_enabled)
       
     # TwistStamped message callback on "/twist_cmd" topic
     # Get a TwistCmd message with fields:
@@ -91,18 +103,10 @@ class DBWNode(object):
     def twist_cmd_cb(self, msg):
       self.proposed_lin_vel = msg.twist.linear.x
       self.proposed_ang_vel = msg.twist.angular.z
-      #rospy.logwarn("[ LOG ] : Got a twist_cmd message with lin_vel = %s, ang_vel = %s/n", 
-      #              self.proposed_lin_vel, self.proposed_ang_vel)
                     
     # Callback on current speed message "/current_velocity" topic, message type TwistStamped, as <twist_cmd_cb()>
     def current_lin_vel_cb(self, msg):
       self.current_lin_vel = msg.twist.linear.x
-      if self.counter < 100:
-        self.counter += 1
-      else:
-        self.counter = 0
-        rospy.logwarn("[ LOG ] : Got a current_lin_vel message with lin_vel = %s/n", 
-                      self.current_lin_vel)
 
     def loop(self):
         rate = rospy.Rate(50) # 50Hz
@@ -120,20 +124,9 @@ class DBWNode(object):
             throttle, brake, steering = self.controller.control(self.proposed_lin_vel,
                                                                 self.proposed_ang_vel,
                                                                 self.current_lin_vel,
-                                                                self.dbw_is_enabled)
+                                                                self.dbw_is_enabled,
+                                                                self.pose_z)
             
-            #if (self.dbw_is_enabled == True):
-            #  self.publish(throttle, brake, steer)
-            output_speed = 0.0
-            if (self.proposed_lin_vel < 1.0):
-              output_speed = 0.0
-              brake = 1
-            else:
-              output_speed = self.proposed_lin_vel
-              brake = 0
-              
-            throttle = output_speed
-            steering = self.proposed_ang_vel
             if (self.dbw_is_enabled == True):
               self.publish(throttle, brake, steering)
             
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -3,12 +3,52 @@ GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
 
+from pid import PID
+from lowpass import LowPassFilter
+from yaw_controller import YawController
+import rospy
+
 class Controller(object):
     def __init__(self, *args, **kwargs):
         # TODO: Implement
-        pass
+        self.kp = 0.001
+        self.kd = 0.100
+        self.ki = 0.000
+        self.pid_thr = PID(self.kp, self.ki, self.kd, 1.0, 0.0)
+        self.dt = rospy.get_time()
 
-    def control(self, *args, **kwargs):
+    def control(self, prop_lin_vel, prop_ang_vel, curr_lin_vel, dbw_enabled, pose_z):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
-        return 1., 0., 0.
+        # Get dt
+        sample_time = self.dt - rospy.get_time()
+        dt = rospy.get_time()
+
+        # Get linear error
+        rospy.logwarn("prop_lin_vel = %s, curr_lin_vel = %s, prop_ang_vel = %s, pose_z = %s\n", prop_lin_vel, curr_lin_vel, prop_ang_vel, pose_z)
+        lin_err = curr_lin_vel - prop_lin_vel
+        
+        # Must obtain proposed angle from proposed angular velocity
+        # angular velocity omega = (th1 - th0) / (t1-t0) = dtheta / dt # grc.nasa.gov/www/k-12/airplane/angdva.html
+        # Therefore angular proposed position th1 = omega*dt + th0 --> prop_ang = prop_ang_vel * dt + pose_z
+        prop_ang = prop_ang_vel * (dt / 10000000000.0) + pose_z
+        rospy.logwarn("prop_ang = %s, pose_z = %s\n", prop_ang, pose_z)
+        ang_err = pose_z - prop_ang
+        
+        # Reset PID if needed
+        if not dbw_enabled:
+           self.pid_thr.reset()
+           return 0.0, 0.0, 0.0
+
+        # Advance PID
+        thr = self.pid_thr.step(lin_err, dt)
+        
+        # Low pass should be here somewhere or before pid control
+        
+        # Break - hipass
+        brk = 0
+        
+        # Steering, P controller for the time being - should be replaced with YawController
+        ster = -ang_err * 20
+        
+        return thr, brk, ster
