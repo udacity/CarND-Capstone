f01dec7ded0b5a160641350e588ce12bdd0009ac
Muhsin Sahin <sahinmuhsin2018@gmail.com>
2020-02-20 17:30:51 +0100

    waypoint_update_step-1

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -86,7 +86,7 @@ class WaypointUpdater(object):
         self.base_waypoints = waypoints
         if not self.waypoints_2d:
             self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
-            self.waypoint_tree = KDTree(self.waypoints_2d)
+            self.waypoint_tree = KDTree( self.waypoints_2d )
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
