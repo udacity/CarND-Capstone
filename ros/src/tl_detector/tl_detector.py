4ff6acf6f781a4516c61c72c66262cd1250709d7
xiao <chenxiao199519@foxmail.com>
2020-03-23 00:12:56 +0100

    integrated with traffic light classifier

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -24,6 +24,18 @@ class TLDetector(object):
         self.camera_image = None
         self.lights = []
 
+        self.state = TrafficLight.UNKNOWN
+        self.last_state = TrafficLight.UNKNOWN
+        self.last_wp = -1
+        self.state_count = 0
+
+        self.waypoints_tree=None
+        self.waypoints_2d=None
+
+        self.bridge = CvBridge()
+        self.light_classifier = TLClassifier()
+        self.listener = tf.TransformListener()
+
         sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
@@ -42,17 +54,9 @@ class TLDetector(object):
 
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
-        self.bridge = CvBridge()
-        self.light_classifier = TLClassifier()
-        self.listener = tf.TransformListener()
 
-        self.state = TrafficLight.UNKNOWN
-        self.last_state = TrafficLight.UNKNOWN
-        self.last_wp = -1
-        self.state_count = 0
 
-        self.waypoints_tree=None
-        self.waypoints_2d=None
+
 
         rospy.spin()
 
@@ -113,6 +117,8 @@ class TLDetector(object):
         return closest_idx
 
     def get_light_state(self, light):
+
+        #return light.state
         """Determines the current color of the traffic light
 
         Args:
@@ -122,7 +128,7 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        '''
+
         if(not self.has_image):
             self.prev_light_loc = None
             return False
@@ -130,9 +136,10 @@ class TLDetector(object):
         cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
         #Get classification
+        print self.light_classifier.get_classification(cv_image)
         return self.light_classifier.get_classification(cv_image)
-        '''
-        return light.state
+        
+
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
