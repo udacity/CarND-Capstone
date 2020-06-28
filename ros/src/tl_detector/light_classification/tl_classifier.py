7db1c97879148256f1741f559ac4158bde442ab9
leo.borgnino <leo.borgnino@gmail.com>
2020-06-28 20:04:52 +0000

    Added tl classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,46 @@
+import os
+
+import cv2
+import numpy as np
+import rospy
+import tensorflow as tf
 from styx_msgs.msg import TrafficLight
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+        self.current_light = TrafficLight.UNKNOWN
+        #model = 'frozen_inference_graph.pb'
+        model = 'frozen_inference_graph_sim_tf_v1.4.pb'
+        model_path = 'light_classification/ssd_mobilenet_v1_coco_11_06_2017/' + model
+        print(os.getcwd())
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(model_path, 'rb') as fid:
+               serialized_graph = fid.read()
+               od_graph_def.ParseFromString(serialized_graph)
+               tf.import_graph_def(od_graph_def, name='')
+            
+        self.category_index = {1: {'id': 1, 'name': 'Green'}, 2: {'id': 2, 'name': 'Red'},
+                               3: {'id': 3, 'name': 'Yellow'}, 4: {'id': 4, 'name': 'off'}}
+
+        # create tensorflow session for detection
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        # end
+        self.sess = tf.Session(graph=self.graph, config=config)
+
+        # Definite input and output Tensors for detection_graph
+        self.image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
+
+        # Each box represents a part of the image where a particular object was detected.
+        self.detection_boxes = self.graph.get_tensor_by_name('detection_boxes:0')
+
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        self.detection_scores = self.graph.get_tensor_by_name('detection_scores:0')
+        self.detection_classes = self.graph.get_tensor_by_name('detection_classes:0')
+        self.num_detections = self.graph.get_tensor_by_name('num_detections:0')
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -15,5 +52,39 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        (im_width, im_height, _) = image_rgb.shape
+        image_np = np.expand_dims(image_rgb, axis=0)
+
+        # Actual detection.
+        with self.graph.as_default():
+            (boxes, scores, classes, num) = self.sess.run(
+                [self.detection_boxes, self.detection_scores,
+                 self.detection_classes, self.num_detections],
+                feed_dict={self.image_tensor: image_np})
+
+        boxes = np.squeeze(boxes)
+        scores = np.squeeze(scores)
+        classes = np.squeeze(classes).astype(np.int32)
+
+        min_score_thresh = .5
+        count = 0
+        count1 = 0
+        # print(scores)
+
+        for i in range(boxes.shape[0]):
+            if scores is None or scores[i] > min_score_thresh:
+                count1 += 1
+                class_name = self.category_index[classes[i]]['name']
+
+                # Traffic light thing
+                if class_name == 'Red':
+                    count += 1
+
+        # print(count)
+        if count < count1 - count:
+            self.current_light = TrafficLight.GREEN
+        else:
+            self.current_light = TrafficLight.RED
+
+        return self.current_light
--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -98,6 +98,7 @@ class TLDetector(object):
             self.upcoming_red_light_pub.publish(Int32(self.last_wp))
         self.state_count += 1
 
+
     def get_closest_waypoint(self, x, y):
         """Identifies the closest path waypoint to the given position
             https://en.wikipedia.org/wiki/Closest_pair_of_points_problem
@@ -129,8 +130,8 @@ class TLDetector(object):
         cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
 
         #Get classification
-        #return self.light_classifier.get_classification(cv_image)
-        return light.state
+        return self.light_classifier.get_classification(cv_image)
+        #return light.state
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
@@ -147,19 +148,19 @@ class TLDetector(object):
         stop_line_positions = self.config['stop_line_positions']
         if(self.pose):
             car_position = self.get_closest_waypoint( self.pose.pose.position.x, self.pose.pose.position.y)
-
+            
             diff = len(self.waypoints.waypoints)
             for i, light in enumerate(self.lights):
                 # Get stop line waypoint index
                 line = stop_line_positions[i]
                 temp_wp_idx = self.get_closest_waypoint(line[0], line[1])
                 # Closest stop line waypoint index
-                d = temp_wp_idx - car_wp_idx
+                d = temp_wp_idx - car_position
                 if d >= 0 and d < diff:
                     diff = d
                     closest_light = light
                     line_wp_idx = temp_wp_idx
-                    
+
         if closest_light:
             state = self.get_light_state(closest_light)
             return line_wp_idx, state
