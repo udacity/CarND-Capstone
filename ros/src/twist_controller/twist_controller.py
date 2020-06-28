dfd3326b5120522c74c55f515f963b63ff01d418
leo.borgnino <leo.borgnino@gmail.com>
2020-06-28 21:57:03 +0000

    Removed model and added placeholder to the folder

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -14,7 +14,7 @@ class Controller(object):
         
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
         
-        kp = 0.3
+        kp = 0.2
         ki = 0.1
         kd = 0.
         mn = 0.
