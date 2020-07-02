130f0f5d16125a7cd42847187d43490847b7e59f
stecasta <stefanocastagnetta@gmail.com>
2020-07-02 16:03:52 +0000

    fixed steering issue

--- ros/src/waypoint_follower/src/pure_pursuit_core.cpp
+++ ros/src/waypoint_follower/src/pure_pursuit_core.cpp
@@ -252,22 +252,22 @@ bool PurePursuit::verifyFollowing() const
 geometry_msgs::Twist PurePursuit::calcTwist(double curvature, double cmd_velocity) const
 {
   // verify whether vehicle is following the path
-  bool following_flag = verifyFollowing();
-  static double prev_angular_velocity = 0;
+//   bool following_flag = verifyFollowing();
+//   static double prev_angular_velocity = 0;
 
   geometry_msgs::Twist twist;
   twist.linear.x = cmd_velocity;
-  if (!following_flag)
-  {
+//   if (!following_flag)
+//   {
     //ROS_ERROR_STREAM("Not following");
     twist.angular.z = current_velocity_.twist.linear.x * curvature;
-  }
-  else
-  {
-    twist.angular.z = prev_angular_velocity;
-  }
+//   }
+//   else
+//   {
+//     twist.angular.z = prev_angular_velocity;
+//   }
 
-  prev_angular_velocity = twist.angular.z;
+//   prev_angular_velocity = twist.angular.z;
   return twist;
 }
 
