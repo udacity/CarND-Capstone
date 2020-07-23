09b34feb2f4ce23abd3c332a5bc5a30a62980878
CaoFM <fangmingcao@yahoo.com>
2020-07-23 03:37:01 +0000

    Change parameter in detection range to 300; Add several trained model file

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -16,7 +16,7 @@ from datetime import datetime
 import os
 
 
-STATE_COUNT_THRESHOLD = 3
+STATE_COUNT_THRESHOLD = 5
 
 # configuration for saving training data from simulator
 SAVE_TRAINING_IMAGE = False
@@ -62,7 +62,7 @@ class TLDetector(object):
 
         self.bridge = CvBridge()
 
-        self.light_classifier = TLClassifier('./light_classification/sim_model.h5')
+        self.light_classifier = TLClassifier('sim_raw.h5')
 
         self.listener = tf.TransformListener()
 
@@ -206,7 +206,7 @@ class TLDetector(object):
                 line_wp_idx = temp_wp_idx
 
         # if found a closest light and is 300 waypoints in front of us
-        if closest_light and diff < 200:
+        if closest_light and diff < 300:
             # get state of traffic light
             state = self.get_light_state(closest_light)
             rospy.loginfo("Next light is state %d at %d waypoints ahead" % (state,diff))
@@ -225,7 +225,7 @@ class TLDetector(object):
 
         else:
             # no upcoming traffic light was found
-            rospy.loginfo("No light in next 200 waypoint")
+            rospy.loginfo("No light in next 300 waypoint")
 
             if SAVE_TRAINING_IMAGE and not stop_line_immediate_behind:
                 state_truth = 3 # new class 3 'no traffic ligh present
