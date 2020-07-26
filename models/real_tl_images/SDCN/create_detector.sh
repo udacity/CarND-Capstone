9cddc5837fe5578c3610a50fc15f63ff650b10fb
Yaboo Oyabu <yaboo@pop-os.localdomain>
2020-07-27 03:11:09 +0900

    update

--- models/real_tl_images/SDCN/create_detector.sh
+++ models/real_tl_images/SDCN/create_detector.sh
@@ -18,20 +18,21 @@ if [ ! -e ${ROOT_DIR}/pretrained_models/${MODEL_NAME} ]; then
 fi
 
 # Start training
-# python3 ${ROOT_DIR}/models/research/object_detection/train.py \
-#     --logtostderr \
-#     --train_dir=${ROOT_DIR}/detector_training \
-#     --pipeline_config_path=${ROOT_DIR}/pipeline_config/${MODEL_NAME}.config
+python3 ${ROOT_DIR}/models/research/object_detection/train.py \
+    --logtostderr \
+    --train_dir=${ROOT_DIR}/detector_training \
+    --pipeline_config_path=${ROOT_DIR}/pipeline_config/${MODEL_NAME}.config
 
+# # Evaluation
 # python3 ${ROOT_DIR}/models/research/object_detection/eval.py \
 #     --logtostderr \
 #     --checkpoint_dir ${ROOT_DIR}/detector_training \
 #     --eval_dir=${ROOT_DIR}/detector_evaluation \
 #     --pipeline_config_path ${ROOT_DIR}/pipeline_config/${MODEL_NAME}.config
 
-# https://github.com/tensorflow/tensorflow/issues/16268
-python3 ${ROOT_DIR}/models/research/object_detection/export_inference_graph.py \
-    --input_type image_tensor \
-    --pipeline_config_path ${ROOT_DIR}/pipeline_config/${MODEL_NAME}.config \
-    --trained_checkpoint_prefix ${ROOT_DIR}/detector_training/model.ckpt-2806 \
-    --output_directory ${ROOT_DIR}/ssd_inception_v2_retrained_2806
+# # https://github.com/tensorflow/tensorflow/issues/16268
+# python3 ${ROOT_DIR}/models/research/object_detection/export_inference_graph.py \
+#     --input_type image_tensor \
+#     --pipeline_config_path ${ROOT_DIR}/pipeline_config/${MODEL_NAME}.config \
+#     --trained_checkpoint_prefix ${ROOT_DIR}/detector_training/model.ckpt-2806 \
+#     --output_directory ${ROOT_DIR}/ssd_inception_v2_retrained_2806
