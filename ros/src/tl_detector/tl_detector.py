761f3be27dcd50ded8df4b4cf0e681efaf0377dd
Erik Rosen <erosen@palantir.com>
2020-11-01 18:28:13 +0100

    Receiving camera images from simulator without error - need to test wether tl_detector actually outputs correct values

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -145,10 +145,11 @@ class TLDetector(object):
 
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
-        if(self.pose):
+        if(self.pose and self.waypoint_tree):
             car_wp_idx = self.get_closest_waypoint(self.pose.pose.position.x,self.pose.pose.position.y)
 
             #TODO find the closest visible traffic light (if one exists)
+            diff = len(self.waypoints.waypoints)
             for i, light in enumerate(self.lights):
                 # Get stop line waypoint index
                 line = stop_line_positions[i]
@@ -164,7 +165,7 @@ class TLDetector(object):
 
         if closest_light:
             state = self.get_light_state(closest_light)
-            return light_wp_idx, state
+            return line_wp_idx, state
         
         return -1, TrafficLight.UNKNOWN
 
