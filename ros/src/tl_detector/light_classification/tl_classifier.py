75a01185f04a5ed81bed9deecac2c654fa74453f
Bulat Yapparov <byapparov@gmail.com>
2020-11-08 18:36:21 +0000

    Migrating the changes for a clean PR

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,92 @@
 from styx_msgs.msg import TrafficLight
+import tensorflow as tf
+import os
+import numpy as np
+
+def select_boxes(boxes, classes, scores, score_threshold=0, target_class=10):
+    """
+    :param boxes:
+    :param classes:
+    :param scores:
+    :param target_class: default traffic light id in COCO dataset is 10
+    :return:
+    """
+    sq_scores = np.squeeze(scores)
+    sq_classes = np.squeeze(classes)
+    sq_boxes = np.squeeze(boxes)
+
+    sel_id = np.logical_and(sq_classes == target_class, sq_scores > score_threshold)
+    return sq_boxes[sel_id]
+
+
+def crop_roi_image(image_np, sel_box):
+    im_height, im_width, _ = image_np.shape
+    (left, right, top, bottom) = (sel_box[1] * im_width, sel_box[3] * im_width,
+                                  sel_box[0] * im_height, sel_box[2] * im_height)
+    cropped_image = image_np[int(top):int(bottom), int(left):int(right), :]
+    return cropped_image
+
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
+        self.load_graph()
+        self.extract_graph_components()
+        self.sess = tf.Session(graph=self.detection_graph)
+
         pass
+
+    def extract_graph_components(self):
+        # Definite input and output Tensors for detection_graph
+        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+        self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+
+    def detect_multi_object(self, image_np, score_threshold):
+        """
+        Return detection boxes in a image
+
+        :param image_np:
+        :param score_threshold:
+        :return:
+        """
+
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        # Actual detection.
+
+        (boxes, scores, classes, num) = self.sess.run(
+            [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
+            feed_dict={self.image_tensor: image_np_expanded})
+
+        sel_boxes = select_boxes(boxes=boxes, classes=classes, scores=scores,
+                                 score_threshold=score_threshold, target_class=10)
+
+        return sel_boxes
+
+    def load_graph(self):
+
+        # To read more about the model zoo see:
+        # https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/tf1_detection_zoo.md
+        PATH_TO_CKPT = "./light_classification/models/ssd_mobilenet_v1_coco_11_06_2017/frozen_inference_graph.pb"
+
+        cwd = os.getcwd()
+        print(cwd)
+
+        detection_graph = tf.Graph()
+        with detection_graph.as_default():
+            # Line below is different from tensorflow 2 version used in Jupyter
+            # https://stackoverflow.com/questions/57614436/od-graph-def-tf-graphdef-attributeerror-module-tensorflow-has-no-attribut
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+        self.detection_graph = detection_graph
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -15,5 +98,13 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
+        # TODO implement light color prediction
         return TrafficLight.UNKNOWN
+
+    def detect_traffic_light(self, image):
+        boxes = self.detect_multi_object(image, score_threshold=0.2)
+        if len(boxes) == 0:
+            return np.array([])
+
+        cropped_image = crop_roi_image(image, boxes[0])
+        return cropped_image
