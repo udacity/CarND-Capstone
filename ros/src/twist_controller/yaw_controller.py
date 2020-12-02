b9c4d8a95913ca3a2b5f0dfa3aebcfedb2ba6ab6
YANGYi <71188696+YANGYi0112@users.noreply.github.com>
2020-12-02 19:53:48 +0100

    Update of DBW node

--- ros/src/twist_controller/yaw_controller.py
+++ ros/src/twist_controller/yaw_controller.py
@@ -1,4 +1,5 @@
 from math import atan
+import rospy
 
 class YawController(object):
     def __init__(self, wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle):
@@ -13,13 +14,17 @@ class YawController(object):
 
     def get_angle(self, radius):
         angle = atan(self.wheel_base / radius) * self.steer_ratio
+        #rospy.logwarn(" Angle: {0}".format(angle))
         return max(self.min_angle, min(self.max_angle, angle))
 
     def get_steering(self, linear_velocity, angular_velocity, current_velocity):
         angular_velocity = current_velocity * angular_velocity / linear_velocity if abs(linear_velocity) > 0. else 0.
+        #rospy.logwarn(" Angular_velocity1: {0}".format(angular_velocity))
 
         if abs(current_velocity) > 0.1:
             max_yaw_rate = abs(self.max_lat_accel / current_velocity);
+            #rospy.logwarn(" Max_ywa_rate: {0}".format(max_yaw_rate))
             angular_velocity = max(-max_yaw_rate, min(max_yaw_rate, angular_velocity))
-
+            #rospy.logwarn(" Angular_velocity2: {0}".format(angular_velocity))
+            
         return self.get_angle(max(current_velocity, self.min_speed) / angular_velocity) if abs(angular_velocity) > 0. else 0.0;
