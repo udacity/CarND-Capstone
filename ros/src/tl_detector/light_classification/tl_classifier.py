8ce7c94dd5cbe504bbcdd36625762006c5344128
aadeshnpn <neupaneaadesh@gmail.com>
2020-12-05 20:01:39 -0500

    code running with traffic light detection but too slow and inaccurate

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -24,15 +24,15 @@ class TLClassifier(object):
         in_features = self.model.roi_heads.box_predictor.cls_score.in_features
         # replace the pre-trained head with a new one
         self.model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
         # Load model from saved pt file
         self.model.load_state_dict(
             torch.load(model_path, map_location=torch.device('cpu')))
+        self.model.eval()
         self.tl_catogeries = {
-                1: TrafficLight.UNKNOWN,
-                2: TrafficLight.GREEN,
-                3: TrafficLight.YELLOW,
-                4: TrafficLight.RED
+                0: TrafficLight.UNKNOWN,
+                1: TrafficLight.GREEN,
+                2: TrafficLight.YELLOW,
+                3: TrafficLight.RED
             }
 
     def get_classification(self, image):
@@ -51,19 +51,29 @@ class TLClassifier(object):
         #     [TrafficLight.UNKNOWN, TrafficLight.RED,
         #     TrafficLight.GREEN, TrafficLight.YELLOW]
         #     )
+        current_light = TrafficLight.UNKNOWN
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         (im_width, im_height, _) = image_rgb.shape
-        image_np = np.expand_dims(image_rgb, axis=0)
+        image_np = np.expand_dims(image_rgb, axis=0)/255.0
         rospy.logwarn(image_np.shape)
-        outputs = self.model(image_np)
-        boxes = np.squeeze(outputs['boxes'])
-        scores = np.squeeze(outputs['scores'])
-        classes = np.squeeze(outputs['labels']).astype(np.int32)
+        image_np = [torch.tensor(image_np, dtype=torch.float).reshape(
+                3, image_np.shape[1], image_np.shape[2])]
 
+        # rospy.logwarn("image shape={}".format(image_np[0].shape))
+        # rospy.logwarn("image val={}".format(image_np[0]))
+        outputs = self.model(image_np)
+        boxes = outputs[0]['boxes'].detach().numpy()
+        scores = outputs[0]['scores'].detach().numpy()
+        classes = outputs[0]['labels'].detach().numpy().astype(np.int32)
+        rospy.logwarn("image val={},{},{}".format(boxes, scores, classes))
         min_treshold = 0.6
-        filtclasses = [
-            classes[i] for i in range(
-                boxes.shape[0]) if scores[i] > min_treshold]
-        filtclasses, counts = np.unique(filtclasses, return_counts=True)
-        current_light = self.tl_catogeries[filtclasses[np.argmax(counts)[0]]]
+        if boxes.shape[0] >=1:
+            try:
+                filtclasses = [
+                    classes[i] for i in range(
+                        boxes.shape[0]) if scores[i] > min_treshold]
+                filtclasses, counts = np.unique(filtclasses, return_counts=True)
+                current_light = self.tl_catogeries[filtclasses[np.argmax(counts)]]
+            except (KeyError, ValueError):
+                current_light = TrafficLight.UNKNOWN
         return current_light
--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -23,27 +23,9 @@ class TLDetector(object):
         self.base_waypoints = None
         self.waypoints_2d = None
         self.waypoint_tree = None
-        self.waypoints = None
+        # self.waypoints = None
         self.camera_image = None
         self.lights = []
-
-        sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
-        sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-
-        '''
-        /vehicle/traffic_lights provides you with the location of the traffic light in 3D map space and
-        helps you acquire an accurate ground truth data source for the traffic light
-        classifier by sending the current color state of all traffic lights in the
-        simulator. When testing on the vehicle, the color state will not be available. You'll need to
-        rely on the position of the light and the camera image to predict it.
-        '''
-        sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb)
-        sub6 = rospy.Subscriber('/image_color', Image, self.image_cb)
-
-        config_string = rospy.get_param("/traffic_light_config")
-        self.config = yaml.load(config_string)
-
-        self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
         self.bridge = CvBridge()
         self.light_classifier = TLClassifier()
@@ -54,13 +36,29 @@ class TLDetector(object):
         self.last_wp = -1
         self.state_count = 0
 
+        config_string = rospy.get_param("/traffic_light_config")
+        self.config = yaml.load(config_string)
+
+        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb, queue_size=2)
+        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb, queue_size=8)
+        rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb, queue_size=2)
+        rospy.Subscriber('/image_color', Image, self.image_cb, queue_size=1)
+        self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
+
+        '''
+        /vehicle/traffic_lights provides you with the location of the traffic light in 3D map space and
+        helps you acquire an accurate ground truth data source for the traffic light
+        classifier by sending the current color state of all traffic lights in the
+        simulator. When testing on the vehicle, the color state will not be available. You'll need to
+        rely on the position of the light and the camera image to predict it.
+        '''
         rospy.spin()
 
     def pose_cb(self, msg):
         self.pose = msg
 
     def waypoints_cb(self, waypoints):
-        self.waypoints = waypoints
+        self.base_waypoints = waypoints
         if not self.waypoints_2d:
             self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in
                                  waypoints.waypoints]
