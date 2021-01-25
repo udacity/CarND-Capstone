7620cc427d6a452d06e66903af90652d278f2896
btiegelmann <>
2021-01-25 21:32:49 +0100

    [m] First working traffic light detector

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,12 @@
+import cv2
+from tl_classifier_cv import detect_red_lights_basic as classify
+from tl_classifier_hough import LightDetector
 from styx_msgs.msg import TrafficLight
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+        self.light_detector = LightDetector()
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -16,4 +19,22 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+
+        #bgr = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        bgr = image[:,:,::-1]
+        images, output_image = self.light_detector.drawCirclesAndGetImages(bgr, True)
+
+        color = self.light_detector.getLightColor(images)
+        
+        if color == 'red':
+            print("color:", color)
+            return TrafficLight.RED
+        else:
+            print("color:", color)
+            return TrafficLight.GREEN
+
+        # if classify(image):
+        #     return TrafficLight.RED
+        # else:
+        #     return TrafficLight.GREEN
+        #return TrafficLight.UNKNOWN
