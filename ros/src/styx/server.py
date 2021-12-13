081a43a4cd365fc65271818735caa52b9074f503
Kun Lin <kun.lin.596@gmail.com>
2021-12-13 22:47:03 +0900

    Format styx

--- ros/src/styx/server.py
+++ ros/src/styx/server.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
@@ -9,21 +9,25 @@ import time
 from bridge import Bridge
 from conf import conf
 
-sio = socketio.Server(async_mode='gevent')
+sio = socketio.Server(async_mode="gevent")
 msgs = []
 
 dbw_enable = False
 
-@sio.on('connect')
+
+@sio.on("connect")
 def connect(sid, environ):
     print("connect ", sid)
+
 
 def send(topic, data):
     sio.emit(topic, data=data, skip_sid=True)
 
+
 bridge = Bridge(conf, send)
 
-@sio.on('telemetry')
+
+@sio.on("telemetry")
 def telemetry(sid, data):
     global dbw_enable
     if data["dbw_enable"] != dbw_enable:
@@ -31,30 +35,36 @@ def telemetry(sid, data):
         bridge.publish_dbw_status(dbw_enable)
     bridge.publish_odometry(data)
 
-@sio.on('control')
+
+@sio.on("control")
 def control(sid, data):
     bridge.publish_controls(data)
 
-@sio.on('obstacle')
+
+@sio.on("obstacle")
 def obstacle(sid, data):
     bridge.publish_obstacles(data)
 
-@sio.on('lidar')
+
+@sio.on("lidar")
 def obstacle(sid, data):
     bridge.publish_lidar(data)
 
-@sio.on('trafficlights')
+
+@sio.on("trafficlights")
 def trafficlights(sid, data):
     bridge.publish_traffic(data)
 
-@sio.on('image')
+
+@sio.on("image")
 def image(sid, data):
     bridge.publish_camera(data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     # Create socketio WSGI application
     app = socketio.WSGIApp(sio)
 
     # deploy as an gevent WSGI server
-    pywsgi.WSGIServer(('', 4567), app, handler_class=WebSocketHandler).serve_forever()
+    pywsgi.WSGIServer(("", 4567), app, handler_class=WebSocketHandler).serve_forever()
