d42bb11d5336e517c235599e7a0f87325ccf7c03
Kun Lin <kun.lin.596@gmail.com>
2021-12-13 22:51:55 +0900

    Format waypoint_loader.py

--- ros/src/waypoint_loader/waypoint_loader.py
+++ ros/src/waypoint_loader/waypoint_loader.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import csv
@@ -11,34 +11,33 @@ from styx_msgs.msg import Lane, Waypoint
 import tf
 import rospy
 
-CSV_HEADER = ['x', 'y', 'z', 'yaw']
+CSV_HEADER = ["x", "y", "z", "yaw"]
 MAX_DECEL = 1.0
 
 
-class WaypointLoader(object):
-
+class WaypointLoader:
     def __init__(self):
-        rospy.init_node('waypoint_loader', log_level=rospy.DEBUG)
+        rospy.init_node("waypoint_loader", log_level=rospy.DEBUG)
 
-        self.pub = rospy.Publisher('/base_waypoints', Lane, queue_size=1, latch=True)
+        self.pub = rospy.Publisher("/base_waypoints", Lane, queue_size=1, latch=True)
 
-        self.velocity = self.kmph2mps(rospy.get_param('~velocity'))
-        self.new_waypoint_loader(rospy.get_param('~path'))
+        self.velocity = self.kmph2mps(rospy.get_param("~velocity"))
+        self.new_waypoint_loader(rospy.get_param("~path"))
         rospy.spin()
 
     def new_waypoint_loader(self, path):
         if os.path.isfile(path):
             waypoints = self.load_waypoints(path)
             self.publish(waypoints)
-            rospy.loginfo('Waypoint Loded')
+            rospy.loginfo("Waypoint Loded")
         else:
-            rospy.logerr('%s is not a file', path)
+            rospy.logerr("%s is not a file", path)
 
     def quaternion_from_yaw(self, yaw):
-        return tf.transformations.quaternion_from_euler(0., 0., yaw)
+        return tf.transformations.quaternion_from_euler(0.0, 0.0, yaw)
 
     def kmph2mps(self, velocity_kmph):
-        return (velocity_kmph * 1000.) / (60. * 60.)
+        return (velocity_kmph * 1000.0) / (60.0 * 60.0)
 
     def load_waypoints(self, fname):
         waypoints = []
@@ -46,10 +45,10 @@ class WaypointLoader(object):
             reader = csv.DictReader(wfile, CSV_HEADER)
             for wp in reader:
                 p = Waypoint()
-                p.pose.pose.position.x = float(wp['x'])
-                p.pose.pose.position.y = float(wp['y'])
-                p.pose.pose.position.z = float(wp['z'])
-                q = self.quaternion_from_yaw(float(wp['yaw']))
+                p.pose.pose.position.x = float(wp["x"])
+                p.pose.pose.position.y = float(wp["y"])
+                p.pose.pose.position.z = float(wp["z"])
+                q = self.quaternion_from_yaw(float(wp["yaw"]))
                 p.pose.pose.orientation = Quaternion(*q)
                 p.twist.twist.linear.x = float(self.velocity)
 
@@ -58,29 +57,29 @@ class WaypointLoader(object):
 
     def distance(self, p1, p2):
         x, y, z = p1.x - p2.x, p1.y - p2.y, p1.z - p2.z
-        return math.sqrt(x*x + y*y + z*z)
+        return math.sqrt(x * x + y * y + z * z)
 
     def decelerate(self, waypoints):
         last = waypoints[-1]
-        last.twist.twist.linear.x = 0.
+        last.twist.twist.linear.x = 0.0
         for wp in waypoints[:-1][::-1]:
             dist = self.distance(wp.pose.pose.position, last.pose.pose.position)
             vel = math.sqrt(2 * MAX_DECEL * dist)
-            if vel < 1.:
-                vel = 0.
+            if vel < 1.0:
+                vel = 0.0
             wp.twist.twist.linear.x = min(vel, wp.twist.twist.linear.x)
         return waypoints
 
     def publish(self, waypoints):
         lane = Lane()
-        lane.header.frame_id = '/world'
+        lane.header.frame_id = "/world"
         lane.header.stamp = rospy.Time(0)
         lane.waypoints = waypoints
         self.pub.publish(lane)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         WaypointLoader()
     except rospy.ROSInterruptException:
-        rospy.logerr('Could not start waypoint node.')
+        rospy.logerr("Could not start waypoint node.")
